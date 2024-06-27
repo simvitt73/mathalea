d01132b0f14aeca6f1091b6ae970d832b643d985
Florian Picard <florian.picard.fr@gmail.com>
2024-06-27 20:18:13 +0200

    feat: nouveaux type de questions

--- src/exercices/1e/1G21.ts
+++ src/exercices/1e/1G21.ts
@@ -1,62 +1,166 @@
 import Exercice from '../Exercice'
-import { listeQuestionsToContenu } from '../../modules/outils.js'
+import { listeQuestionsToContenu, randint } from '../../modules/outils.js'
+import { toTex } from '../../modules/outilsMathjs'
+import { Vecteur } from '../../lib/2d/segmentsVecteurs'
+import { Point, milieu } from '../../lib/2d/points'
 import { combinaisonListes } from '../../lib/outils/arrayOutils'
+import { reduireAxPlusByPlusC } from '../../lib/outils/ecritures'
 
 export const titre = 'Équations cartésiennes de droites'
 export const dateDePublication = '24/06/2024'
 
 /**
  * Calcul d'équations cartésiennes de droites
+ * Beaucoup trop de fonctions... Comment les intégrer à la base
+ * de code déjà existante ?
  * @florianpicard
-*/
+ */
+
 export const uuid = '8472c'
 export const ref = '1G21'
 export const refs = {
-  'fr-fr': ['1G21'],
-  'fr-ch': []
+    'fr-fr': ['1G21'],
+    'fr-ch': []
+}
+
+function pointVersTex(A: Point): string {
+    return `${A.nom}\\left(${A.x} ; ${A.y}\\right)`
+}
+
+function vecteurcoordVersTex(u: Vecteur): string {
+    return `\\begin{pmatrix} ${u.x} \\\\ ${u.y} \\end{pmatrix}`
+}
+
+function vecteurVersTex(u: Vecteur): string {
+    let fonctionLatex = u.nom.length >= 2 ? 'overrightarrow' : 'vec';
+    return `\\${fonctionLatex}{${u.nom}} = \\begin{pmatrix} ${u.x} \\\\ ${u.y} \\end{pmatrix}`
+}
+
+function vecteurNormal(u: Vecteur, nom: string = 'n'): Vecteur {
+    return new Vecteur(-u.y, u.x, nom)
+}
+
+function equaDroite(A: Point, {B = false, u = false, n = false}) : [string, string] {
+    if (B instanceof Point) {
+	let u = new Vecteur(A, B, `${A.nom}${B.nom}`)
+	let [equa, details] = equaDroite(A, {u: u})
+	return [equa,
+		`Un vecteur directeur de la droite $(${A.nom}${B.nom})$ est $${vecteurVersTex(u)}$. ` + details]
+    }
+    if (u instanceof Vecteur) {
+	let n = vecteurNormal(u)
+	let [equa, details] = equaDroite(A, {n: n})
+	return [equa,
+		`Un vecteur normal à la droite $(d)$ est $${vecteurVersTex(n)}$. ` + details]
+    }
+    if (n instanceof Vecteur) {
+	let c = -(A.x*n.x + A.y*n.y)
+	let equa =  `${reduireAxPlusByPlusC(n.x, n.y, c)} = 0`
+	return [equa,
+		`La droite de vecteur normal $${vecteurVersTex(n)}$ admet pour équation cartésienne $${n.x}x + ${n.y}y + c = 0$, pour un certain nombre réel $c$. Comme $${A.nom}$ appartient à la droite, $${n.x}\\times${A.x} + ${n.y}\\times ${A.y} + c = 0$. Ainsi, $c = ${c}$, l'équation cartésienne de la droite est donc $${equa}$.`]
+    }
+    return ""
+}
+
+function hauteur(A: Point, B: Point, C: Point): [string, string] {
+    let n = new Vecteur(B, C, 'BC')
+    let [equa, details] = equaDroite(A, {n: n})
+
+    return [
+	equa,
+	`La hauteur $(h)$ issue de $${A.nom}$ dans le triangle $${A.nom}${B.nom}${C.nom}$ passe par le point $${A.nom}$ et est perpendiculaire à la droite $(${B.nom}${C.nom}$). Ainsi le vecteur $${vecteurVersTex(n)}$ est un vecteur normal à la droite $(h)$. ` + details
+    ]
+}
+
+function mediatrice(A: Point, B: Point): [string, string] {
+    let I = milieu(A, B, 'I')
+    let n = new Vecteur(A, B, 'AB')
+    let [equa, details] = equaDroite(I, {n: n})
+
+    return [
+	equa,
+	`La médiatrice du segment $[${A.nom}${B.nom}]$ coupe perpendiculairement $[${A.nom}${B.nom}]$ en son milieu $${pointVersTex(I)}$. Ainsi, $${vecteurVersTex(n)}$ est un vecteur normal à la médiatrice du segment $[${A.nom}${B.nom}]$. ` + details
+    ]
+
 }
 
 
-export default class equationsCartesiennes extends Exercice {
-  constructor () {
-    super()
-    this.consigne = 'Consigne'
-    this.nbQuestions = 10
-  }
+function questionDeuxPoints(i: number = 0): [string, string] {
+    let A = new Point(randint(-5, 5), randint(-5, 5), 'A')
+    let B = new Point(randint(-5, 5, [A.x]), randint(-5, 5, [A.y]), 'B')
+    let [equa, details] = equaDroite(A, {B: B})
 
-  nouvelleVersion () {
-    this.listeQuestions = []
-    this.listeCorrections = []
-    this.autoCorrection = []
+    return [
+	`Soient les points $${pointVersTex(A)}$, et $${pointVersTex(B)}$. Donner l'équation de la droite $(AB)$.`,
+	details
+    ]
+} 
 
-    const typeQuestionsDisponibles = ['type1', 'type2', 'type3']
+function questionPointvDir(i: number = 0): [string, string] {
+    let A = new Point(randint(-5, 5), randint(-5, 5), 'A')
+    let u = new Vecteur(randint(-5, 5), randint(-5, 5), 'u')
+    let [equa, details] = equaDroite(A, {u: u})
 
-    const listeTypeQuestions =typeQuestionsDisponibles;
-    // combinaisonListes(typeQuestionsDisponibles, this.nbQuestions)
-    for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50;) {
-      let texte = ''
-      let texteCorr = ''
-      switch (listeTypeQuestions[i]) {
-        case 'type1':
-          texte = `Question ${i + 1} de type 1`
-          texteCorr = `Correction ${i + 1} de type 1`
-          break
-        case 'type2':
-          texte = `Question ${i + 1} de type 2`
-          texteCorr = `Correction ${i + 1} de type 2`
-          break
-        case 'type3':
-          texte = `Question ${i + 1} de type 3`
-          texteCorr = `Correction ${i + 1} de type 3`
-          break
-      }
-      if (this.questionJamaisPosee(i, texte)) {
-        this.listeQuestions.push(texte)
-        this.listeCorrections.push(texteCorr)
-        i++
-      }
-      cpt++
+    return [
+	`Soient le point $${pointVersTex(A)}$, et le vecteur $${vecteurVersTex(u)}$. Donner l'équation de la droite $(d)$ passant par le point $${A.nom}$ et de vecteur directeur $\\vec{${u.nom}}$.`,
+	details
+    ]
+}
+
+function questionPointvNorm(i: number = 0): [string, string] {
+    let A = new Point(randint(-5, 5), randint(-5, 5), 'A')
+    let n = new Vecteur(randint(-5, 5), randint(-5, 5), 'n')
+    let [equa, details] = equaDroite(A, {n: n})
+    return [
+	`Soient le point $${pointVersTex(A)}$, et le vecteur $${vecteurVersTex(n)}$. Donner l'équation de la droite $(d)$ passant par le point $${A.nom}$ et de vecteur normal $\\vec{${n.nom}}$.`,
+	details
+    ]
+}
+
+function questionHauteur(i: number = 0): [string, string] {
+    let A = new Point(randint(-5, 5), randint(-5, 5), 'A')
+    let B = new Point(randint(-5, 5, [A.x]), randint(-5, 5, [A.y]), 'B')
+    let C = new Point(randint(-5, 5, [A.x, B.x]), randint(-5, 5, [A.y, B.y]), 'C')
+    let [equa, details] = hauteur(A, B, C)
+    return [
+	`Soient les trois points $${pointVersTex(A)}$, $${pointVersTex(B)}$, et $${pointVersTex(C)}$. Déterminer l'équation cartésienne de la hauteur issue de $${A.nom}$ dans le triangle $${A.nom}${B.nom}${C.nom}$.`,
+	details
+    ]
+}
+
+function questionMediatrice(i: number = 0): [string, string] {
+    let A = new Point(randint(-5, 5), randint(-5, 5), 'A')
+    let B = new Point(randint(-5, 5, [A.x]), randint(-5, 5, [A.y]), 'B')
+    let [equa, details] = mediatrice(A, B)
+    return [
+	`Soient les points $${pointVersTex(A)}$ et $${pointVersTex(B)}$. Déterminer une équation cartésienne de la médiatrice du segment $[${A.nom}${B.nom}$].`,
+	details
+    ]
+}
+
+export default class nomExercice extends Exercice {
+    constructor () {
+	super()
+	this.consigne = ""
+	this.nbQuestions = 10
     }
-    listeQuestionsToContenu(this)
-  }
+
+    nouvelleVersion () {
+	this.listeQuestions = []
+	this.listeCorrections = []
+	this.autoCorrection = []
+
+	const questions = [questionPointvNorm, questionPointvDir, questionDeuxPoints, questionHauteur, questionMediatrice]
+
+	for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50;) {
+	    let [texte, texteCorr] = questions[i](i)
+	    if (this.questionJamaisPosee(i, texte)) {
+		this.listeQuestions.push(texte)
+		this.listeCorrections.push(texteCorr)
+		i++
+	    }
+	    cpt++
+	}
+	listeQuestionsToContenu(this)
+    }
 }
