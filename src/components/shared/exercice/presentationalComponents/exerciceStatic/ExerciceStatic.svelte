eba57c06d179275727fb73b7d16958e1bc202767
Sylvain Chambon <chambon.sylvain@gmail.com>
2024-07-17 02:25:41 +0200

    feat: Indice layout

--- src/components/shared/exercice/presentationalComponents/exerciceStatic/ExerciceStatic.svelte
+++ src/components/shared/exercice/presentationalComponents/exerciceStatic/ExerciceStatic.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
   import HeaderExerciceVueProf from '../../shared/headerExerciceVueProf/HeaderExerciceVueProf.svelte'
   import { retrieveResourceFromUuid } from '../../../../../lib/components/refUtils'
-  import { resourceHasPlace, isStaticType, type JSONReferentielObject, isCrpeType } from '../../../../../lib/types/referentiels'
+  import {
+    resourceHasPlace,
+    isStaticType,
+    type JSONReferentielObject,
+    isCrpeType
+  } from '../../../../../lib/types/referentiels'
   /**
    * Gestion du référentiel pour la recherche de l'uuid
-  */
+   */
   import referentielStaticFR from '../../../../../json/referentielStaticFR.json'
   import referentielStaticCH from '../../../../../json/referentielStaticCH.json'
 
   import referentielBibliotheque from '../../../../../json/referentielBibliotheque.json'
   import type { HeaderProps } from '../../../../../lib/types/ui'
+  import { globalOptions } from '../../../../../lib/stores/generalStore'
   // on rassemble les deux référentiel statique
   const allStaticReferentiels: JSONReferentielObject = {
     ...referentielBibliotheque,
@@ -30,15 +36,20 @@
   export let zoomFactor: string
   export let isSolutionAccessible: boolean
   const foundResource = retrieveResourceFromUuid(allStaticReferentiels, uuid)
-  const resourceToDisplay = isStaticType(foundResource) || isCrpeType(foundResource)
-    ? { ...foundResource }
-    : null
+  const resourceToDisplay =
+    isStaticType(foundResource) || isCrpeType(foundResource) ? { ...foundResource } : null
   const exercice =
     resourceToDisplay === null
       ? null
       : {
-          png: typeof resourceToDisplay.png === 'string' ? [resourceToDisplay.png] : resourceToDisplay.png,
-          pngCor: typeof resourceToDisplay.pngCor === 'string' ? [resourceToDisplay.pngCor] : resourceToDisplay.pngCor
+          png:
+            typeof resourceToDisplay.png === 'string'
+              ? [resourceToDisplay.png]
+              : resourceToDisplay.png,
+          pngCor:
+            typeof resourceToDisplay.pngCor === 'string'
+              ? [resourceToDisplay.pngCor]
+              : resourceToDisplay.pngCor
         }
   let isCorrectionVisible = false
   let isContentVisible = true
@@ -74,7 +85,9 @@
 </script>
 
 <HeaderExerciceVueProf
-  {...headerExerciceProps}{indiceExercice}{indiceLastExercice}
+  {...headerExerciceProps}
+  {indiceExercice}
+  {indiceLastExercice}
   on:clickCorrection={(event) => {
     isCorrectionVisible = event.detail.isCorrectionVisible
   }}
@@ -94,6 +107,29 @@
     {/if}
   {/if}
 
+  {#if $globalOptions.staticDisplayStyle.hint}
+    {#if resourceToDisplay && Object.keys(resourceToDisplay).includes('pngIndice')}
+      <div
+        class="relative border-l-coopmaths-warn-800 dark:border-l-coopmathsdark-warn border-l-[3px] text-coopmaths-corpus dark:text-coopmathsdark-corpus mt-6 lg:mt-2 mb-6 py-2 pl-4"
+        id="correction{indiceExercice}"
+      >
+        <img
+          src={resourceToDisplay.pngIndice}
+          style="width: calc(100% * {zoomFactor}"
+          alt="indice"
+        />
+        <div
+        class="absolute flex flex-row py-[1.5px] px-3 rounded-t-md justify-center items-center -left-[3px] -top-[15px] bg-coopmaths-warn-800 dark:bg-coopmathsdark-warn font-semibold text-xs text-coopmaths-canvas dark:text-coopmathsdark-canvas"
+      >
+        Indice
+      </div>
+      <div
+        class="absolute border-coopmaths-warn-800 dark:border-coopmathsdark-warn bottom-0 left-0 border-b-[3px] w-4"
+      />
+      </div>
+    {/if}
+  {/if}
+
   {#if isCorrectionVisible}
     <div
       class="relative border-l-coopmaths-struct dark:border-l-coopmathsdark-struct border-l-[3px] text-coopmaths-corpus dark:text-coopmathsdark-corpus mt-6 lg:mt-2 mb-6 py-2 pl-4"
@@ -106,13 +142,13 @@
               <p class="text-red-500">Aucune correction disponible</p>
             {:else}
               <img
-              src={url}
-              class="p-2"
-              style="width: calc(100% * {zoomFactor}"
-              alt="correction"
-              on:error={handleNoCorrectionAvailable}
+                src={url}
+                class="p-2"
+                style="width: calc(100% * {zoomFactor}"
+                alt="correction"
+                on:error={handleNoCorrectionAvailable}
               />
-              {/if}
+            {/if}
           {/each}
         {/if}
       </div>
