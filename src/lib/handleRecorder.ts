055f578be305cf6edf7ee9b80fda89bea599829c
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2024-10-06 17:27:11 +0200

    Update handleRecorder.ts

--- src/lib/handleRecorder.ts
+++ src/lib/handleRecorder.ts
@@ -1,13 +1,41 @@
-import { exercicesParams, globalOptions } from './stores/generalStore'
+import { exercicesParams } from './stores/generalStore'
 import { get } from 'svelte/store'
+import { mathaleaGetExercicesFromParams } from './mathalea'
 
-export function sendActivityParams () {
-  console.log('toto')
-  window.parent.postMessage({ toto: 2 }, '*')
+export async function sendActivityParams () {
+  const exercices = []
+  const exoParams = get(exercicesParams)
+  const exo = await mathaleaGetExercicesFromParams(exoParams)
+  let i = 0
+  for (const param of exoParams) {
+    let paramUrl = ''
+    for (const key of Object.keys(param)) {
+      if (key === 'sup') {
+        paramUrl += `s=${param[key]}&`
+      } else if (key === 'sup2') {
+        paramUrl += `s2=${param[key]}&`
+      } else if (key === 'sup3') {
+        paramUrl += `s3=${param[key]}&`
+      } else if (key === 'sup4') {
+        paramUrl += `s4=${param[key]}&`
+      } else if (key === 'sup5') {
+        paramUrl += `s5=${param[key]}&`
+      } else if (key === 'nbQuestions') {
+        paramUrl += `n=${param[key]}&`
+      } else if (key !== 'alea' && key !== 'id') {
+        paramUrl += `${key}=${param[key]}&`
+      }
+    }
+    paramUrl = paramUrl.slice(0, -1)
+    exercices.push({
+      titre: exo[i].titre,
+      url: paramUrl
+    })
+    i++
+  }
   window.parent.postMessage(
     {
-      exercicesParams: get(exercicesParams),
-      globalOptions: get(globalOptions),
+      exercices,
       action: 'mathalea:activityParams'
     },
     '*'
