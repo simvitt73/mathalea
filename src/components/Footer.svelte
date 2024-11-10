83a177387d04dcfea215134036ccd7e4d996bfb7
Rémi Angot <remi.angot@gmail.com>
2024-11-10 09:08:41 +0100

    fix #1632

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,7 +4,7 @@
 
 <footer class="{$keyboardState.isVisible ? 'pt-48 md:pt-60' : 'pt-4'} w-full bg-coopmaths-canvas dark:bg-coopmathsdark-canvas h-12 flex flex-row-reverse justify-center items-center opacity-100 pb-4 px-4">
 <a href="https://coopmaths.fr/www/mentions_legales/" target="_blank">
-  <img class="hidden md:block px-6" style="height: 20px" src='./assets/images/logo2.png' alt="Logo Coopmaths 2" />
+  <img class="hidden md:block px-6" style="height: 20px" src='assets/images/logo2.png' alt="Logo Coopmaths 2" />
 </a>
   <a
     class="flex flex-row items-center justify-start pr-6 font-thin text-coopmaths-action hover:text-coopmaths-action-lightest dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
--- src/components/display/eleve/Footer2.svelte
+++ src/components/display/eleve/Footer2.svelte
@@ -3,6 +3,6 @@
 
 <footer class="flex-1 w-full bg-coopmaths-canvas dark:bg-coopmathsdark-canvas h-12 flex justify-center bottom-0 opacity-100">
   <a href="/" target="_blank">
-    <img class="px-6" style="height: 30px" src="/assets/images/logo2.png" alt="Logo Coopmaths 2" />
+    <img class="px-6" style="height: 30px" src="assets/images/logo2.png" alt="Logo Coopmaths 2" />
   </a>
 </footer>
