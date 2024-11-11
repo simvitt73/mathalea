9fe6a95ddf9647dce57eaab9df24fcf925dec4c0
Matthieu Devillers <matthieu.devillers@ac-rennes.fr>
2024-11-11 18:51:30 +0000

    Correction d'un bug (ticket #1651)

--- src/exercices/5e/5S21-1.js
+++ src/exercices/5e/5S21-1.js
@@ -8,6 +8,7 @@ import { listeQuestionsToContenu, randint } from '../../modules/outils.js'
 import { ajouteChampTexteMathLive } from '../../lib/interactif/questionMathLive'
 import { handleAnswers } from '../../lib/interactif/gestionInteractif'
 import { fonctionComparaison } from '../../lib/interactif/comparisonFunctions'
+import { miseEnEvidence } from '../../lib/outils/embellissements'
 export const titre = 'Trouver des probabilités simples'
 export const dateDePublication = '01/05/2021'
 export const interactifReady = true
@@ -40,26 +41,26 @@ export default function ProbabilitesSimples () {
       // const pP = 100 - pG - pN
       const sport = choice(['hand-ball', 'football', 'rugby', 'basket', 'volley-ball', 'water-polo', 'baseball'])
       texte = `Lors d'un match de ${sport}, l'équipe qui reçoit un adversaire a une probabilité de $ ${texNombre(pG / 100)}$ de gagner son match`
-      texte += ` et $${texNombre(pN / 100)}$ de faire un match nul.<br>`
+      texte += ` et $${texNombre(pN / 100)}$ de faire un match nul.<br><br>`
 
       texte += `${numAlpha(0)}Quelle est la probabilité, pour cette équipe, de ne pas perdre le match ?` + ajouteChampTexteMathLive(this, 2 * i, '')
-      let correction1 = 'Ne pas perdre un match, c\'est, soit le gagner, soit faire un match nul. La probabilité est donc : <br> <br>'
+      let correction1 = `${numAlpha(0)} Ne pas perdre un match, c'est, soit le gagner, soit faire un match nul. La probabilité est donc : <br> <br>`
       correction1 += `P(«${sp(1)}Ne pas perdre le match${sp(1)}») $=$ P(«${sp(1)}Gagner le match${sp(1)}») + P(«${sp(1)}Match nul${sp(1)}») <br>`
       correction1 += `P(«${sp(1)}Ne pas perdre le match${sp(1)}») $= ${texNombre(pG / 100)} + ${texNombre(pN / 100)}$ <br> `
       const reponse1 = texNombre((pG + pN) / 100)
-      correction1 += `P(«${sp(1)}Ne pas perdre le match${sp(1)}») $= ${reponse1}$  <br>`
+      correction1 += `P(«${sp(1)}Ne pas perdre le match${sp(1)}») $= ${miseEnEvidence(`${reponse1}`)} $  <br>`
       texte += `<br><br>${numAlpha(1)}Quelle est la probabilité, pour cette équipe, de perdre le match ?` + ajouteChampTexteMathLive(this, 2 * i + 1, '')
-      let correction2 = `L'évènement  «${sp(1)}Perdre le match${sp(1)}» est l'évènement contraire de  «${sp(1)}Ne pas perdre le match${sp(1)}». On peut donc affirmer que : <br> <br>`
-      correction2 += `P(«${sp(1)}Perdre le match${sp(1)}») $+$ P(«${sp(1)}Ne pas perdre le match${sp(1)}») $= 1$ <br>`
-      correction2 += `P(«${sp(1)}Perdre le match${sp(1)}») $=1-$ P(«${sp(1)}Ne pas perdre le match${sp(1)}»)<br>`
-      correction2 += `P(«${sp(1)}Perdre le match${sp(1)}») $=1-${texNombre((pG + pN) / 100)}$<br>`
+      correction1 += `<br><br>${numAlpha(1)} L'évènement  «${sp(1)}Perdre le match${sp(1)}» est l'évènement contraire de  «${sp(1)}Ne pas perdre le match${sp(1)}». On peut donc affirmer que : <br> <br>`
+      correction1 += `P(«${sp(1)}Perdre le match${sp(1)}») $+$ P(«${sp(1)}Ne pas perdre le match${sp(1)}») $= 1$ <br>`
+      correction1 += `P(«${sp(1)}Perdre le match${sp(1)}») $=1-$ P(«${sp(1)}Ne pas perdre le match${sp(1)}»)<br>`
+      correction1 += `P(«${sp(1)}Perdre le match${sp(1)}») $=1-${texNombre((pG + pN) / 100)}$<br>`
       const reponse2 = texNombre(1 - (pG + pN) / 100)
-      correction2 += `P(«${sp(1)}Perdre le match${sp(1)}») $=${reponse2}$<br>`
+      correction1 += `P(«${sp(1)}Perdre le match${sp(1)}») $=${miseEnEvidence(`${reponse2}`)} $<br>`
       if (this.questionJamaisPosee(i, pG)) {
         handleAnswers(this, 2 * i, { reponse: { value: reponse1, compare: fonctionComparaison } })
         handleAnswers(this, 2 * i + 1, { reponse: { value: reponse2, compare: fonctionComparaison } })
         this.listeQuestions.push(texte)
-        this.listeCorrections.push(correction1, correction2)
+        this.listeCorrections.push(correction1)
       }
     }
     listeQuestionsToContenu(this)
