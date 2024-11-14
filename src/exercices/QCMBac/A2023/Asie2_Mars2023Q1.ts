769a5921f701e87f7b6c94890afdef60c340b7f9
PierreChs <pierre.cauchois@ac-normandie.fr>
2024-11-14 22:33:34 +0100

    mise à jour asie 2 Q1 et Q2

--- src/exercices/QCMBac/A2023/Asie2_Mars2023Q1.ts
+++ src/exercices/QCMBac/A2023/Asie2_Mars2023Q1.ts
@@ -1,4 +1,6 @@
-import ExerciceQcm from '../../ExerciceQcm'
+import { randint } from '../../../modules/outils'
+import { nombreElementsDifferents } from '../../ExerciceQcm'
+import ExerciceQcmA from '../../ExerciceQcmA'
 
 export const uuid = 'A2032023Q1'
 export const refs = {
@@ -17,30 +19,47 @@ export const dateDePublication = '05/11/2024'
  * Ce moule à exo dispose d'une méthode qcmCamExport qui permet de récupérer le JSON de la question et de la reponse pour qcmCam.
  * Il est interactif et dispose d'un export AMC d'office
  */
-export default class SujetZero2024Ex5Q5 extends ExerciceQcm {
-  versionOriginale: () => void = () => {
+export default class SujetZero2024Ex5Q5 extends ExerciceQcmA {
+  private appliquerLesValeurs (u0:number, r:number, k:number): void {
+    /*
+    */
+    const un = u0 + r * k
     this.reponses = [
-      '$673$',
-      '$2023$',
-      '$672$',
-      '$2016$'
+      `$${k + 1}$`,
+      `$${un}$`,
+      `$${k}$`,
+      `$${un - u0}$`
     ]
 
     this.enonce = this.sup3
-      ? `On considère L une liste de nombres constituée de termes consécutifs d'une suite arithmétique de premier terme $7$ et de raison $3$,<br> le dernier nombre de la liste est $2023$ soit: 
-L = [7, 10, \\ldots , 2023].<br>`
+      ? `On considère L une liste de nombres constituée de termes consécutifs d'une suite arithmétique de premier terme ${u0} et de raison ${r},<br> le dernier nombre de la liste est ${un} soit: 
+L = [${u0}, ${u0 + r}, \\ldots , ${un}].<br>`
       : ''
-    this.enonce += 'Le nombre de termes de cette liste est :'
-    this.correction = 'Le nombre de termes de cette liste est 673. <br>'
-    this.correction += 'En effet, le $n$-ième terme de la liste est $u_{n-1}=7 + 3(n - 1)$ ;<br>'
+    this.enonce += 'Le nombre de terme de cette liste est:'
+    this.correction = `Le nombre de termes de cette liste est : ${k + 1}.<br>`
+    this.correction += `En effet, le $n$-ième terme de la liste est $u_{n-1}=${u0} + ${r}(n - 1)$ ;<br>`
     this.correction += 'il faut donc résoudre l\'équation :<br>'
-    this.correction += '$2023 = 7+3(n - 1) \\iff 2016 = 3(n - 1) \\iff n - 1=672 \\iff n=673$.<br>'
+    this.correction += `$${un} = ${u0} + ${r}(n - 1) \\iff ${un - u0} = ${r}(n - 1) \\iff n - 1=${(un - u0) / r}\\iff n=${k + 1}$.<br>`
+  }
+
+  versionOriginale: () => void = () => {
+    this.appliquerLesValeurs(7, 3, 672)
+  }
+
+  versionAleatoire: () => void = () => {
+    const n = 4
+    do {
+      const u0 = randint(2, 10)
+      const r = 2 * randint(1, 5) + 1
+      const k = randint(8, 20)
+      this.appliquerLesValeurs(u0, r, k)
+    } while (nombreElementsDifferents(this.reponses) < n)
   }
 
   constructor () {
     super()
     this.options = { vertical: true, ordered: false }
-    this.versionOriginale()
+    this.versionAleatoire()
     this.besoinFormulaire3CaseACocher = ['Avec le préambule de l\'énoncé', true]
     this.sup3 = true
   }
--- src/exercices/QCMBac/A2023/Asie2_Mars2023Q2.ts
+++ src/exercices/QCMBac/A2023/Asie2_Mars2023Q2.ts
@@ -1,15 +1,17 @@
-import ExerciceQcm from '../../ExerciceQcm'
+import { randint } from '../../../modules/outils'
+import { nombreElementsDifferents } from '../../ExerciceQcm'
+import ExerciceQcmA from '../../ExerciceQcmA'
 
-export const uuid = 'A2032023Q2'
+export const uuid = 'A062023Q1'
 export const refs = {
-  'fr-fr': ['TSA1-QCM06'],
+  'fr-fr': ['TSA1-06'],
   'fr-ch': []
 }
 export const interactifReady = true
 export const interactifType = 'qcm'
 export const amcReady = 'true'
 export const amcType = 'qcmMono'
-export const titre = 'QCM Bac Asie mars 2023 : Suites'
+export const titre = 'QCM Bac Asie mars 2023 : Python'
 export const dateDePublication = '05/11/2024'
 /**
  * Ceci est un exo construit à partir d'une question de qcm de Bac.
@@ -17,30 +19,43 @@ export const dateDePublication = '05/11/2024'
  * Ce moule à exo dispose d'une méthode qcmCamExport qui permet de récupérer le JSON de la question et de la reponse pour qcmCam.
  * Il est interactif et dispose d'un export AMC d'office
  */
-export default class SujetZero2024Ex5Q5 extends ExerciceQcm {
-  versionOriginale: () => void = () => {
+export default class SujetZero2024Ex5Q5 extends ExerciceQcmA {
+  private appliquerLesValeurs (u0:number, r:number, k:number): void {
+    /*
+    */
+    const un = u0 + r * k
     this.reponses = [
-      '$\\dfrac{336}{673}$',
+      `$\\dfrac{${Math.floor(k / 2)}}{${k + 1}}$`,
+      `$\\dfrac{${Math.floor(1 + k / 2)}}{${k + 1}}$`,
       '$\\dfrac{1}{2}$',
-      '$\\dfrac{34}{673}$',
-      '$\\dfrac{337}{673}$'
+      `$\\dfrac{${(2 * u0 + r) * 2}}{${k + 1}}$`
     ]
 
-    this.enonce = this.sup3
-      ? `On considère L une liste de nombres constituée de termes consécutifs d'une suite arithmétique de premier terme $7$ et de raison $3$,<br> le dernier nombre de la liste est $2023$ soit: 
-L = [7, 10, \\ldots , 2023].<br>`
-      : ''
-    this.enonce += 'On choisit un nombre au hasard dans cette liste.<br>'
-    this.enonce += 'La probabilité de tirer un nombre pair est :'
-    this.correction = 'Sur les 673 nombres de la liste le premier et le dernier nombre de la liste sont impairs ; il y a donc 336 nombres pairs.<br>'
-    this.correction += 'La probabilité de tirer un nombre pair est donc $\\dfrac{336}{673}$ ;<br>'
+    this.enonce = `On considère L une liste de nombres constituée de termes consécutifs d'une suite arithmétique de premier terme ${u0} et de raison ${r},<br> le dernier nombre de la liste est ${un} soit: 
+L = [${u0}, ${u0 + r}, \\ldots , ${un}].<br>
+On choisit au hasard un nombre dans cette liste. La probabilité de tirer un nombre pair est:`
+    this.correction = `Sur les ${k + 1} nombres de la liste le premier et le dernier nombre de la liste sont impairs.<br>`
+    this.correction += `Il y a donc ${Math.floor(k / 2)} nombres pairs.<br>`
+    this.correction += `La probabilité de tirer un nombre pair est donc $\\dfrac{${Math.floor(k / 2)}}{${k + 1}}$ ;<br>`
+  }
+
+  versionOriginale: () => void = () => {
+    this.appliquerLesValeurs(7, 3, 672)
+  }
+
+  versionAleatoire: () => void = () => {
+    const n = 4
+    do {
+      const u0 = randint(2, 10)
+      const r = 2 * randint(1, 5) + 1
+      const k = randint(8, 20)
+      this.appliquerLesValeurs(u0, r, k)
+    } while (nombreElementsDifferents(this.reponses) < n)
   }
 
   constructor () {
     super()
     this.options = { vertical: true, ordered: false }
-    this.versionOriginale()
-    this.besoinFormulaire3CaseACocher = ['Avec le préambule de l\'énoncé', true]
-    this.sup3 = true
+    this.versionAleatoire()
   }
 }
