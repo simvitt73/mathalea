5ea47fa8887bb8c4953595a6f297c11bc1b7156d
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2024-11-16 17:29:33 +0100

    brouillon avancé

--- src/exercices/6e/6G1.ts
+++ src/exercices/6e/6G1.ts
@@ -1,5 +1,5 @@
 import Exercice from '../Exercice'
-import { shuffle } from '../../lib/outils/arrayOutils'
+import { choice, shuffle } from '../../lib/outils/arrayOutils'
 import { choisitLettresDifferentes } from '../../lib/outils/aleatoires'
 import { context } from '../../modules/context'
 import Figure from 'apigeom'
@@ -7,10 +7,14 @@ import figureApigeom from '../../lib/figureApigeom'
 import type SuperFigure from 'apigeom'
 import type Line from 'apigeom/src/elements/lines/Line'
 import { createList } from '../../lib/format/lists'
-import { randint } from '../../modules/outils'
 import { wrapperApigeomToMathalea } from '../../lib/apigeom/apigeomZoom'
+import { rangeMinMax } from '../../lib/outils/nombres'
+import { point } from '../../lib/2d/points'
+import { Polygone, polygone } from '../../lib/2d/polygones'
+import { aireTriangle } from '../../lib/2d/triangle'
+import type PointApiGeom from 'apigeom/src/elements/points/Point'
 
-export const titre = 'Construire des symétriques de points par rapport à un point'
+export const titre = 'Situer et nommer des points d\'intersections'
 export const dateDePublication = '27/09/2024'
 export const interactifReady = true
 export const interactifType = 'custom'
@@ -43,42 +47,59 @@ export default class PointsIntersectionDroites extends Exercice {
   labels!: string[][]
   exoCustomResultat: boolean
   figuresApiGeom!: SuperFigure[]
+  listePoints: {intersection: PointApiGeom, droites: [Line, Line]}[][]
   constructor () {
     super()
     this.exoCustomResultat = true
     this.nbQuestions = 1
     this.spacingCorr = 1
+    this.listePoints = []
   }
 
   nouvelleVersion () {
-    const colors: string[] = context.isHtml ? ['red', 'green', 'purple', 'blue', 'gray', 'yellow'] : ['gray', 'gray', 'gray', 'gray', 'gray', 'gray']
+    const colors: string[] = context.isHtml ? ['red', 'green', 'purple', 'blue', 'gray', 'pink'] : ['gray', 'gray', 'gray', 'gray', 'gray', 'gray']
+    const frenchColors = {
+      red: 'rouge',
+      green: 'verte',
+      purple: 'violette',
+      blue: 'bleue',
+      gray: 'grise',
+      pink: 'rose'
+    }
     this.answers = {}
     this.listeQuestions = []
     this.listeCorrections = []
     this.autoCorrection = []
     this.figuresApiGeom = []
+    this.listePoints = []
     const nbPoints = 4
     this.labels = Array.from({ length: this.nbQuestions }, () => [])
     for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 20;) {
       let enonce = ''
       let nuage: {x: number, y:number}[] = []
       // On construit les points
+      const listeY: number[] = rangeMinMax(-2, 2).map(el => el * 2)
+      let triangles: Polygone[]
       do {
         nuage = []
-        for (let x = -8; x < 8; x += 1) {
-          if (x !== 0) nuage.push({ y: (randint(2, 8) + 1) * (-1) ** x, x })
+        for (let x = -6; x < 6; x += 3) {
+          nuage.push({ x, y: choice(listeY, nuage.map(el => el.y)) })
         }
         nuage = shuffle(nuage)
+        triangles = nuage.map(el => point(el.x, el.y)).map(el => {
+          const troisAutresPoints = nuage.filter(val => !(Math.abs(val.x - el.x) < 0.1 && Math.abs(val.y - el.y) < 0.1)).map(val => point(val.x, val.y))
+          return polygone(...troisAutresPoints)
+        })
         nuage = nuage.slice(0, nbPoints)
-      } while (!checkDistance(nuage))
+      } while (!checkDistance(nuage) || triangles.some(el => Number(aireTriangle(el)) < 3))
       // Les antécédents sont des points nommés
       this.labels[i] = Array.from(choisitLettresDifferentes(15, 'Q', true))
       const options = {}
-      this.figuresApiGeom[i] = new Figure(Object.assign(options, { xMin: -10, yMin: -10, width: 600, height: 600 }))
+      this.figuresApiGeom[i] = new Figure(Object.assign(options, { xMin: -8, yMin: -8, width: 480, height: 480 }))
       this.figuresApiGeom[i].scale = 1
       this.figuresApiGeom[i].setToolbar({ tools: ['NAME_POINT', 'POINT_INTERSECTION', 'UNDO', 'REDO', 'REMOVE'], position: 'top' })
       const fig = this.figuresApiGeom[i]
-      const pointsPossibles = shuffle(nuage).map((el, k) => fig.create('Point', { x: el.x, y: el.y, label: this.labels[i][k] }))
+      const pointsPossibles = shuffle(nuage).map(el => fig.create('Point', { x: el.x, y: el.y, isVisible: false, isSelectable: false }))
       const droites: Line[] = []
       let indiceDroite = 0
       for (let k = 0; k < nbPoints; k++) {
@@ -87,7 +108,7 @@ export default class PointsIntersectionDroites extends Exercice {
           indiceDroite++
         }
       }
-      const listeDeuxDroites = [
+      const listeDeuxDroites: [Line, Line][] = [
         [droites[0], droites[1]],
         [droites[0], droites[2]],
         [droites[0], droites[3]],
@@ -105,20 +126,26 @@ export default class PointsIntersectionDroites extends Exercice {
         [droites[4], droites[5]]
       ]
       const allIntersections = listeDeuxDroites
-        .map((droites: Line[], k) => Object.assign({}, {
-          intersection: fig.create('PointIntersectionLL', { line1: droites[0], line2: droites[1], label: this.labels[i][k] }),
+        .map(droites => Object.assign({}, {
+          intersection: fig.create('PointIntersectionLL', { line1: droites[0], line2: droites[1], isVisible: false, isSelectable: false }),
           deuxDroites: droites
         }))
-      const intersections = shuffle(allIntersections
+      const intersectionsADistanceRespectables = allIntersections
         .filter(val => checkDistance([{ x: val.intersection.x, y: val.intersection.y }]))
-        .filter((val, k) => allIntersections.slice(k + 1, allIntersections.length)
-          .every((val2) => Math.abs(val2.intersection.x - val.intersection.x) < 0.1 && Math.abs(val2.intersection.y - val.intersection.y) < 0.1)))
-
+        // On nettoie les intersections trop proches
+      const intersections = shuffle(intersectionsADistanceRespectables
+        .filter((val, k, tab) => {
+          for (let j = 0; j < k; j++) {
+            if (Math.abs(val.intersection.x - tab[j].intersection.x) < 0.5 && Math.abs(val.intersection.y - tab[j].intersection.y) < 0.5) return false
+          }
+          return true
+        }))
       const lesTroisPoints = intersections.slice(0, 3)
-      enonce = 'Nommer le point d\'intersection des droites : '
+      this.listePoints.push(...lesTroisPoints)
+      enonce = 'Nommer  : '
       const listeQuestions = createList({
-        items: lesTroisPoints.map((val) => {
-          return `(${val.deuxDroites[0].point1.label + val.deuxDroites[0].point2.label}) et (${val.deuxDroites[1].point1.label + val.deuxDroites[1].point2.label})`
+        items: lesTroisPoints.map((val, k) => {
+          return `$${this.labels[i][k]}$, le point d'intersection de la droite ${frenchColors[val.deuxDroites[0].color as keyof typeof frenchColors]} et de la droite ${frenchColors[val.deuxDroites[1].color as keyof typeof frenchColors]}`
         }),
         style: 'alpha'
 
@@ -140,5 +167,29 @@ export default class PointsIntersectionDroites extends Exercice {
       }
       cpt++
     }
+  }
+
+  correctionInteractive (i) {
+    if (this.answers === undefined) this.answers = {}
+    // Sauvegarde de la réponse pour Capytale
+    this.answers[this.figuresApiGeom[i].id] = this.figuresApiGeom[i].json
+    const divFeedback = document.querySelector(`#feedbackEx${this.numeroExercice}Q${i}`) as HTMLDivElement
+    for (let k = 0; k < 3; k++) {
+      const p = this.listePoints[i][k]
+    }
+
+    const fig = this.figuresApiGeom[i]
+    const points = fig.getPoints()
+    const droites = fig.getLines()
+    const intersections = fig.getIntersections()
+    const lesTroisPoints = intersections.slice(0, 3)
+    const lesTroisPointsLabels = lesTroisPoints.map(val => val.label)
+    const lesTroisPointsLabelsReponses = lesTroisPoints.map(val => val.label)
+    const lesTroisPointsLabelsReponsesTriees = lesTroisPointsLabelsReponses.slice().sort()
+    const lesTroisPointsLabelsTriees = lesTroisPointsLabels.slice().sort()
+    const reponse = lesTroisPointsLabelsTriees.join('')
+    const reponseAttendue = lesTroisPointsLabelsReponsesTriees.join('')
+    this.listeCorrections.push(`Les points d'intersection sont : ${reponseAttendue}`)
+    this.autoCorrection.push(reponse === reponseAttendue)
   }
 }
