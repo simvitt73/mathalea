eac13e631092402795e9b7f1ea4d775341d8980a
moragi <gilles.mora@ac-normandie.fr>
2024-11-17 11:34:57 +0100

    canTNY-2025

---
+++ src/exercices/can/can6NY-2025/canTNY-2025-Q11.ts
@@ -0,0 +1,48 @@
+import Exercice from '../../Exercice'
+import { miseEnEvidence } from '../../../lib/outils/embellissements'
+import { texNombre } from '../../../lib/outils/texNombre'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
+import { randint } from '../../../modules/outils'
+import { rienSi1 } from '../../../lib/outils/ecritures'
+import FractionEtendue from '../../../modules/FractionEtendue'
+import { fonctionComparaison } from '../../../lib/interactif/comparisonFunctions'
+export const titre = 'Calculer un antécédent'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const uuid = 'cbaad'
+export const refs = {
+  'fr-fr': [],
+  'fr-ch': []
+}
+/**
+ * Modèle d'exercice très simple pour la course aux nombres
+ * @author Eric Elter - Gilles Mora
+ * Référence
+*/
+export default class calcAntecedent extends Exercice {
+  constructor () {
+    super()
+    this.titre = titre
+    this.typeExercice = 'simple' // Cette ligne est très importante pour faire faire un exercice simple !
+    this.nbQuestions = 1
+    this.formatInteractif = 'calcul'
+    this.formatChampTexte = ''
+    this.formatChampTexte = KeyboardType.clavierDeBaseAvecFraction
+    this.compare = fonctionComparaison
+    this.optionsDeComparaison = { fractionEgale: true }
+  }
+
+  nouvelleVersion () {
+    this.listeCanEnonces = []
+    this.listeCanReponsesACompleter = []
+    const a = randint(-5, 5, 0)
+    const ant = randint(2020, 2030)
+    this.question = `Déterminer l'antécédent de $${texNombre(ant)}$ par la fonction $f$ définie par : $f(x)=${rienSi1(a)}x+${texNombre(2025, 0)}$.`
+    this.reponse = new FractionEtendue(ant - 2025, a).simplifie().texFSD
+    this.correction = `L'antécédent est la solution de l'équation  $${rienSi1(a)}x+${texNombre(2025, 0)}=${texNombre(ant)}$.<br>
+    Il s'agit  de $${miseEnEvidence(this.reponse)}$.`
+    if (this.interactif) { this.question += '<br>' }
+    this.canEnonce = this.question
+    this.canReponseACompleter = '$f(x)=\\ldots$'
+  }
+}
---
+++ src/exercices/can/can6NY-2025/canTNY-2025-Q12.ts
@@ -0,0 +1,66 @@
+import Exercice from '../../Exercice'
+import { miseEnEvidence } from '../../../lib/outils/embellissements'
+import { texNombre } from '../../../lib/outils/texNombre'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
+import { randint } from '../../../modules/outils'
+import { handleAnswers } from '../../../lib/interactif/gestionInteractif'
+import { creerNomDePolygone } from '../../../lib/outils/outilString'
+import { arrondi } from '../../../lib/outils/nombres'
+import { calculCompare, fonctionComparaison } from '../../../lib/interactif/comparisonFunctions'
+import { toutPourUnPoint } from '../../../lib/interactif/mathLive'
+import FractionEtendue from '../../../modules/FractionEtendue'
+export const titre = 'Calculer les coordonnées d\'un milieu'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const uuid = '2ecf9'
+export const refs = {
+  'fr-fr': [],
+  'fr-ch': []
+}
+/**
+ * Modèle d'exercice très simple pour la course aux nombres
+ * @author Eric Elter - Gilles Mora
+ * Référence
+*/
+export default class coordMilieu extends Exercice {
+  constructor () {
+    super()
+    this.titre = titre
+    this.typeExercice = 'simple' // Cette ligne est très importante pour faire faire un exercice simple !
+    this.nbQuestions = 1
+    this.formatInteractif = 'calcul'
+    this.formatChampTexte = ''
+    this.compare = fonctionComparaison
+    this.optionsDeComparaison = { fractionEgale: true }
+    this.formatChampTexte = KeyboardType.clavierDeBaseAvecFraction
+  }
+
+  nouvelleVersion () {
+    this.listeCanEnonces = []
+    this.listeCanReponsesACompleter = []
+    const nom = creerNomDePolygone(2, ['PQDO'])
+    const b = randint(-5, 5) * 2 + 1
+    const c = randint(-5, 5) * 2 + 1
+    const reponse1 = new FractionEtendue(2025 + b, 2).texFraction
+    const reponse2 = new FractionEtendue(2025 + c, 2).texFraction
+    this.consigne = `Dans un repère du plan, on donne $${nom[0]}(${texNombre(2025, 0)}\\,;\\,${c})$ et $${nom[1]}(${b}\\,;\\,${texNombre(2025, 0)})$.<br>
+        Déterminer les coordonnées du milieu de $[${nom[0] + nom[1]}]$.`
+    this.question = '(%{champ1}\\,;\\,%{champ2})'
+    this.correction = `Les coordonnées du milieu sont données par :
+        $\\left(\\dfrac{${texNombre(2025, 0)}+${b}}{2};\\dfrac{${c}+${texNombre(2025, 0)}}{2}\\right)=
+        \\left(\\dfrac{${texNombre(2025 + b, 0)}}{2};\\dfrac{${texNombre(c + 2025, 0)}}{2}\\right)=
+        ${miseEnEvidence(`(${texNombre((2025 + b) / 2, 1)};${texNombre((c + 2025) / 2, 1)})`)}$.<br>`
+    this.reponse = `(${arrondi((2025 + b) / 2, 1)};${arrondi((c + 2025) / 2, 1)})`
+
+    handleAnswers(this, 0, {
+      bareme: (listePoints) => [Math.min(listePoints[0], listePoints[1]), 1],
+      champ1: { value: reponse1, compare: calculCompare },
+      champ2: { value: reponse2, compare: calculCompare }
+    }
+    )
+    this.reponse = { bareme: toutPourUnPoint, champ1: { value: reponse1, compare: fonctionComparaison }, champ2: { value: reponse2, compare: fonctionComparaison } }
+
+    this.canReponseACompleter = '$(\\,\\ldots\\,;\\,\\ldots\\,)$'
+    this.canEnonce = this.consigne
+  }
+}
