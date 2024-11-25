51f448ff5ef6b0b7666f626694aab0d0fe9d2b0a
Rémi Angot <remi.angot@gmail.com>
2024-11-25 06:20:47 +0100

    3A10DNB-0 => 3A10DNB

--- src/exercices/3e/3A10DNB.ts
+++ src/exercices/3e/3A10DNB.ts
@@ -7,7 +7,7 @@ import ExerciceBrevetA from '../ExerciceBrevetA'
 
 export const uuid = '9a8e7'
 export const refs = {
-  'fr-fr': ['3A10DNB-0'],
+  'fr-fr': ['3A10DNB'],
   'fr-ch': []
 }
 export const titre = 'Problème de partage'
