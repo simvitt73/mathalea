2db5d8d2621ab6729a8e57042ec2bb7b8ea6e848
Gilles Mora <gilles.mora@ac-normandie.fr>
2024-11-27 19:40:16 +0000

    Gilles can5 c30

---
+++ src/exercices/can/5e/can5C30.ts
@@ -0,0 +1,74 @@
+import Exercice from '../../Exercice'
+import { miseEnEvidence } from '../../../lib/outils/embellissements'
+import { listeQuestionsToContenu, randint } from '../../../modules/outils'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
+import { fonctionComparaison } from '../../../lib/interactif/comparisonFunctions'
+import { choice } from '../../../lib/outils/arrayOutils'
+import { texNombre } from '../../../lib/outils/texNombre'
+import Decimal from 'decimal.js'
+import FractionEtendue from '../../../modules/FractionEtendue'
+import { handleAnswers } from '../../../lib/interactif/gestionInteractif'
+import { remplisLesBlancs } from '../../../lib/interactif/questionMathLive'
+export const titre = 'Compléter une somme contenant une fraction avec un décimal'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const dateDePublication = '24/11/2024'
+export const uuid = '779eb'
+export const refs = {
+  'fr-fr': ['can5C30'],
+  'fr-ch': []
+}
+/**
+ * Modèle d'exercice très simple pour la course aux nombres
+ * @author Gilles Mora
+ * Référence
+*/
+export default class fractionsDecimaux extends Exercice {
+  constructor () {
+    super()
+    this.nbQuestions = 1
+    this.formatChampTexte = KeyboardType.clavierDeBase
+    // this.formatInteractif = 'calcul'
+    this.compare = fonctionComparaison
+  }
+
+  nouvelleVersion () {
+    this.listeQuestions = []
+    this.listeCorrections = []
+    this.autoCorrection = []
+    this.listeCanEnonces = []
+    this.listeCanReponsesACompleter = []
+    for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50;) {
+      let texte = ''
+      let texteCorr = ''
+      const listeFractions = [[1, 2], [1, 4], [1, 5], [2, 5], [3, 5], [3, 4]]
+      const fraction = choice(listeFractions)
+      const laFraction = new FractionEtendue(fraction[0], fraction[1])
+      const laFractionDecimale = new Decimal(fraction[0]).div(fraction[1])
+      const decimal = new Decimal(randint(1, 19)).div(100)
+      const somme = new Decimal(fraction[0]).div(fraction[1]).add(decimal)
+
+      const reponse = texNombre(decimal, 2)
+
+      texte = `Compléter l'égalité avec un nombre décimal.<br>
+            `
+      handleAnswers(this, i, { champ1: { value: reponse } })
+      texte += remplisLesBlancs(this, i, `${laFraction.texFraction} +~ %{champ1} =${texNombre(somme, 2)}`, 'fillInTheBlank', '\\ldots')
+      texteCorr = `Comme $${laFraction.texFraction}=${texNombre(laFractionDecimale, 2)}$, alors : <br>
+             $${laFraction.texFraction}+ ${miseEnEvidence(reponse)} =${texNombre(somme, 2)}$`
+      this.canEnonce = 'Compléter l\'égalité avec un nombre décimal.'
+      this.canReponseACompleter = `$${laFraction.texFraction} +~ \\ldots ~ =${texNombre(somme, 2)}$`
+
+      if (this.questionJamaisPosee(i, texte)) {
+        this.listeCorrections.push(texteCorr)
+        this.listeQuestions.push(texte)
+        this.listeCanEnonces.push(this.canEnonce)
+        this.listeCanReponsesACompleter.push(this.canReponseACompleter)
+
+        i++
+      }
+      cpt++
+    }
+    listeQuestionsToContenu(this)
+  }
+}
