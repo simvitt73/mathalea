e857f5a580406eb9d258ac6398f3163ec8a80097
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2024-12-01 22:28:25 +0000

    Jc 6 n31 interactif

--- src/exercices/6e/6N31.js
+++ src/exercices/6e/6N31.js
@@ -1,13 +1,15 @@
 import { choice, combinaisonListes } from '../../lib/outils/arrayOutils'
-import { texPrix } from '../../lib/format/style'
 import { sp } from '../../lib/outils/outilString.js'
 import { texNombre } from '../../lib/outils/texNombre'
-import Exercice from '../deprecatedExercice.js'
-import { listeQuestionsToContenu, randint, calculANePlusJamaisUtiliser } from '../../modules/outils.js'
+import Exercice from '../Exercice'
+import { listeQuestionsToContenu, randint } from '../../modules/outils.js'
 import { context } from '../../modules/context.js'
+import { propositionsQcm } from '../../lib/interactif/qcm'
 export const titre = 'Comparer des nombres décimaux'
 export const amcReady = true
 export const amcType = 'AMCOpen'
+export const interactifType = 'qcm'
+export const interactifReady = true
 
 /**
  * Comparer deux nombres décimaux
@@ -27,6 +29,7 @@ export const amcType = 'AMCOpen'
  * @author Rémi Angot
  * 6N31
  * Ajout AMC : Janvier 2022 par EE
+ * Ajout interactivité : 1/12/2024 par Jean-Claude Lhote
  */
 export const uuid = 'be1e4'
 export const ref = '6N31'
@@ -34,14 +37,16 @@ export const refs = {
   'fr-fr': ['6N31'],
   'fr-ch': ['9NO7-4']
 }
-export default function ComparerDecimaux () {
-  Exercice.call(this)
-  this.consigne = 'Compléter avec le signe < , > ou =.'
-  this.nbQuestions = 8
-  this.nbCols = 2
-  this.nbColsCorr = 2
+export default class ComparerDecimaux extends Exercice {
+  constructor () {
+    super()
+    this.consigne = 'Compléter avec le signe < , > ou =.'
+    this.nbQuestions = 8
+    this.nbCols = 2
+    this.nbColsCorr = 2
+  }
 
-  this.nouvelleVersion = function () {
+  nouvelleVersion () {
     this.autoCorrection = []
 
     const typesDeQuestionsDisponibles = [
@@ -82,27 +87,27 @@ export default function ComparerDecimaux () {
           a = randint(1, 99)
           b = randint(11, 99)
           c = randint(11, 99)
-          x = calculANePlusJamaisUtiliser(a + b / 100)
-          y = calculANePlusJamaisUtiliser(a + c / 100)
+          x = a + b / 100
+          y = a + c / 100
           break
         case 3: // a,b  a,cc avec b>c
           a = randint(1, 99)
           b = randint(1, 8)
           c = randint(1, b * 10)
-          x = calculANePlusJamaisUtiliser(a + b / 10)
-          y = calculANePlusJamaisUtiliser(a + c / 100)
+          x = a + b / 10
+          y = a + c / 100
           break
         case 4: // 0,ab 0,ba
           a = randint(1, 9)
           b = randint(1, 9, a)
-          x = calculANePlusJamaisUtiliser((a * 10 + b) / 100)
-          y = calculANePlusJamaisUtiliser((b * 10 + a) / 100)
+          x = (a * 10 + b) / 100
+          y = (b * 10 + a) / 100
           break
         case 5: // 0,a0b 0,b0a
           a = randint(1, 9)
           b = randint(1, 9, a)
-          x = calculANePlusJamaisUtiliser((a * 100 + b) / 1000)
-          y = calculANePlusJamaisUtiliser((b * 100 + a) / 1000)
+          x = (a * 100 + b) / 1000
+          y = (b * 100 + a) / 1000
           break
         case 6: // a,b a,b0
           a = randint(11, 999)
@@ -110,22 +115,22 @@ export default function ComparerDecimaux () {
             // pas de nombre divisible par 10
             a = randint(11, 999)
           }
-          x = calculANePlusJamaisUtiliser(a / 10)
+          x = a / 10
           y = x
           zeroInutile = true
           break
         case 7: // 0,0ab 0,0a0b
           a = randint(1, 9)
           b = randint(1, 9)
-          x = calculANePlusJamaisUtiliser(a / 100 + b / 1000)
-          y = calculANePlusJamaisUtiliser(a / 100 + b / 10000)
+          x = a / 100 + b / 1000
+          y = a / 100 + b / 10000
           break
         case 8: // a,bb  a,ccc avec b>c
           a = randint(11, 99)
           b = randint(11, 99)
           c = randint(100, b * 10)
-          x = calculANePlusJamaisUtiliser(a + b / 100)
-          y = calculANePlusJamaisUtiliser(a + c / 1000)
+          x = a + b / 100
+          y = a + c / 1000
           if (randint(1, 2) === 1) {
             [x, y] = [y, x]
           }
@@ -134,41 +139,57 @@ export default function ComparerDecimaux () {
           a = randint(11, 98)
           b = randint(11, 99)
           c = randint(b * 100, 10000)
-          x = calculANePlusJamaisUtiliser(a + 1 + b / 100)
-          y = calculANePlusJamaisUtiliser(a + c / 10000)
+          x = a + 1 + b / 100
+          y = a + c / 10000
           if (randint(1, 2) === 1) {
             [x, y] = [y, x]
           }
           break
       }
-
-      texte = `$${texNombre(x)}${sp(3)}\\ldots\\ldots${sp(3)}${texNombre(y)}$`
+      let signe = '>'
+      let prop1, prop2, prop3
+      texte = `$${texNombre(x, 6)}${sp(3)}\\ldots\\ldots${sp(3)}${texNombre(y, 6)}$`
+      prop1 = `$${texNombre(x, 6)} > ${texNombre(y, 6)}$`
+      prop2 = `$${texNombre(x, 6)} < ${texNombre(y, 6)}$`
+      prop3 = `$${texNombre(x, 6)} = ${texNombre(y, 6)}$`
       if (parseFloat(x) > parseFloat(y)) {
-        texteCorr = `$${texNombre(x)} > ${texNombre(y)}$`
+        texteCorr = `$${texNombre(x, 6)} > ${texNombre(y, 6)}$`
       } else if (parseFloat(x) < parseFloat(y)) {
-        texteCorr = `$${texNombre(x)} < ${texNombre(y)}$`
+        signe = '<'
+        texteCorr = `$${texNombre(x, 6)} < ${texNombre(y, 6)}$`
       } else {
-        texteCorr = `$${texNombre(x)} = ${texNombre(y)}$`
+        signe = '='
+        texteCorr = `$${texNombre(x, 6)} = ${texNombre(y, 6)}$`
       }
 
       if (zeroInutile) {
         if (randint(1, 2) === 1) {
-          texte = `$${texPrix(x)}${sp(3)}\\ldots\\ldots${sp(3)}${texNombre(y)}$`
+          texte = `$${texNombre(x, 2, true)}${sp(3)}\\ldots\\ldots${sp(3)}${texNombre(y, 6)}$`
+          prop1 = `$${texNombre(x, 2, true)} > ${texNombre(y, 6)}$`
+          prop2 = `$${texNombre(x, 2, true)} < ${texNombre(y, 6)}$`
+          prop3 = `$${texNombre(x, 2, true)} = ${texNombre(y, 6)}$`
           if (parseFloat(x) > parseFloat(y)) {
-            texteCorr = `$${texPrix(x)} > ${texNombre(y)}$`
+            texteCorr = `$${texNombre(x, 2, true)} > ${texNombre(y, 6)}$`
           } else if (parseFloat(x) < parseFloat(y)) {
-            texteCorr = `$${texPrix(x)} < ${texNombre(y)}$`
+            signe = '<'
+            texteCorr = `$${texNombre(x, 2, true)} < ${texNombre(y, 6)}$`
           } else {
-            texteCorr = `$${texPrix(x)} = ${texNombre(y)}$`
+            signe = '='
+            texteCorr = `$${texNombre(x, 2, true)} = ${texNombre(y, 6)}$`
           }
         } else {
-          texte = `$${texNombre(x)}${sp(3)}\\ldots\\ldots${sp(3)}${texPrix(y)}$`
+          texte = `$${texNombre(x, 6)}${sp(3)}\\ldots\\ldots${sp(3)}${texNombre(y, 2, true)}$`
+          prop1 = `$${texNombre(x, 6)} > ${texNombre(y, 2, true)}$`
+          prop2 = `$${texNombre(x, 6)} < ${texNombre(y, 2, true)}$`
+          prop3 = `$${texNombre(x, 6)} = ${texNombre(y, 2, true)}$`
           if (parseFloat(x) > parseFloat(y)) {
-            texteCorr = `$${texNombre(x)} > ${texPrix(y)}$`
+            texteCorr = `$${texNombre(x, 6)} > ${texNombre(y, 2, true)}$`
           } else if (parseFloat(x) < parseFloat(y)) {
-            texteCorr = `$${texNombre(x)} < ${texPrix(y)}$`
+            texteCorr = `$${texNombre(x, 6)} < ${texNombre(y, 2, true)}$`
+            signe = '<'
           } else {
-            texteCorr = `$${texNombre(x)} = ${texPrix(y)}$`
+            texteCorr = `$${texNombre(x, 6)} = ${texNombre(y, 2, true)}$`
+            signe = '='
           }
         }
       }
@@ -177,8 +198,32 @@ export default function ComparerDecimaux () {
           enonce: texte,
           propositions: [{ texte: texteCorr, statut: 3, feedback: '', sanscadre: true }]
         }
+      } else if (this.interactif) {
+        this.autoCorrection[i] = {
+          enonce: texte,
+          propositions: [
+            {
+              texte: prop1,
+              statut: signe === '>',
+            },
+            {
+              texte: prop2,
+              statut: signe === '<',
+            },
+            {
+              texte: prop3,
+              statut: signe === '=',
+            }
+          ],
+          options: {
+            ordered: true
+          }
+        }
+        const monQcm = propositionsQcm(this, i)
+        texte += monQcm.texte
+        texteCorr += monQcm.texteCorr
       }
-      if (this.listeQuestions.indexOf(texte) === -1) {
+      if (this.questionJamaisPosee(i, x, y)) {
         // Si la question n'a jamais été posée, on en crée une autre
 
         this.listeQuestions.push(texte)
