041eebd5c3cc188a0e758e9ab5734f0698a9fa07
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2024-12-22 05:54:07 +0100

    modif

--- src/exercices/2e/2F20-4.ts
+++ src/exercices/2e/2F20-4.ts
@@ -594,7 +594,7 @@ class resolutionEquationInequationGraphique extends Exercice {
     this.figureApiGeom.ui.send('DRAG')
     if (context.isHtml) {
       if (this.interactif) {
-        this.listeQuestions = [enonce + figureApigeom({ exercice: this, i: 0, figure: this.figureApiGeom, isDynamic: true, width: 348, height: 378 })]
+        this.listeQuestions = [enonce + figureApigeom({ exercice: this, i: 0, figure: this.figureApiGeom, isDynamic: true, width: 378, height: 378 })]
       } else {
         this.listeQuestions = [enonce + wrapperApigeomToMathalea(this.figureApiGeom)]
       }
