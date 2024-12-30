e7e05cb7ff7d742fb4ee3fa311b3aff2eff4d257
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2024-12-30 20:33:06 +0100

    3I10-1 fixé

--- src/exercices/3e/3I10-1.js
+++ src/exercices/3e/3I10-1.js
@@ -184,7 +184,7 @@ export default class ScratchMultiScript extends Exercice {
 }\n`
           for (let k = 0; k < 2; k++) {
             for (let l = 0; l < 2; l++) {
-              const test = pion.testInstruction(`AV${x[i % 3 + 1]}`, lutins[i])
+              const test = testInstruction(`AV${x[i % 3 + 1]}`, lutins[i],pion)
               if (test[0]) {
                 pion.currentPos.x = test[1]
                 pion.currentPos.y = test[2]
@@ -194,7 +194,7 @@ export default class ScratchMultiScript extends Exercice {
                 //   throw Error('Le mouvement n\'est pas valide : sortie de plateau')
               }
               if (rotations[i % 2] === '\\turnright{}') {
-                const test = pion.testInstruction('TD90', lutins[i])
+                const test = testInstruction('TD90', lutins[i],pion)
                 if (test[0]) {
                   pion.currentPos.x = test[1]
                   pion.currentPos.y = test[2]
@@ -204,7 +204,7 @@ export default class ScratchMultiScript extends Exercice {
                   //  throw Error('Le mouvement n\'est pas valide : sortie de plateau')
                 }
               } else {
-                const test = pion.testInstruction('TG90', lutins[i])
+                const test = testInstruction('TG90', lutins[i],pion)
                 if (test[0]) {
                   pion.currentPos.x = test[1]
                   pion.currentPos.y = test[2]
@@ -214,7 +214,7 @@ export default class ScratchMultiScript extends Exercice {
                   //  throw Error('Le mouvement n\'est pas valide : sortie de plateau')
                 }
               }
-              const test2 = pion.testInstruction(`AV${y[i % 3 + 1]}`, lutins[i])
+              const test2 = testInstruction(`AV${y[i % 3 + 1]}`, lutins[i],pion)
               if (test2[0]) {
                 pion.currentPos.x = test2[1]
                 pion.currentPos.y = test2[2]
@@ -224,13 +224,13 @@ export default class ScratchMultiScript extends Exercice {
                 //  throw Error('Le mouvement n\'est pas valide : sortie de plateau')
               }
               if (rotations[(i + 1) % 2] === '\\turnright{}') {
-                const test3 = pion.testInstruction('TD90', lutins[i])
+                const test3 = testInstruction('TD90', lutins[i],pion)
                 pion.currentPos.x = test3[1]
                 pion.currentPos.y = test3[2]
                 pion.currentOrientation = test3[3]
                 lutins[i] = test3[5]
               } else {
-                const test3 = pion.testInstruction('TG90', lutins[i])
+                const test3 = testInstruction('TG90', lutins[i],pion)
                 if (test3[0]) {
                   pion.currentPos.x = test3[1]
                   pion.currentPos.y = test3[2]
@@ -245,9 +245,9 @@ export default class ScratchMultiScript extends Exercice {
             }
             let test4
             if (rotations[(i % 3 === 2 ? 1 : 0)] === '\\turnright{}') {
-              test4 = pion.testInstruction('TD90', lutins[i])
+              test4 = testInstruction('TD90', lutins[i],pion)
             } else {
-              test4 = pion.testInstruction('TG90', lutins[i])
+              test4 = testInstruction('TG90', lutins[i],pion)
             }
             pion.currentPos.x = test4[1]
             pion.currentPos.y = test4[2]
@@ -341,10 +341,10 @@ export default class ScratchMultiScript extends Exercice {
       }
       texteScratch += '\\end{scratch}'
       let texte = `${(this.interactif || context.isAmc) ? '' : 'Noter la séquence de couleurs produite.<br>'}`
-      texte += deuxColonnes(scratchblock(texteScratch), mathalea2d(Object.assign({}, fixeBordures(lePlateau.plateau2d), {
+      texte += deuxColonnes(scratchblock(texteScratch), mathalea2d(Object.assign({}, fixeBordures(lePlateau.objets), {
         scale: 0.4,
         style: 'display: inline'
-      }), lePlateau.plateau2d), 35)
+      }), lePlateau.objets), 35)
 
       let texteCorr = 'On obtient la série de couleurs suivante :<br> '
       texteCorr += `${texteGras(couleurs[i][0])} `
@@ -366,7 +366,7 @@ export default class ScratchMultiScript extends Exercice {
       }
       lutins[i].animation += '" begin="10s" dur="10s" repeatCount="indefinite" />; </circle>'
 
-      objetsCorrection.push(lePlateau.plateau2d, lutins[i])
+      objetsCorrection.push(lePlateau.objets, lutins[i])
       texteCorr += mathalea2d(Object.assign({}, fixeBordures(objetsCorrection), {
         style: 'display: inline',
         scale: 0.4
@@ -386,10 +386,10 @@ export default class ScratchMultiScript extends Exercice {
         }
       } else {
         this.autoCorrection[i] = {}
-        this.autoCorrection[i].enonce = `${deuxColonnes(scratchblock(texteScratch), mathalea2d(Object.assign({}, fixeBordures(lePlateau.plateau2d), {
+        this.autoCorrection[i].enonce = `${deuxColonnes(scratchblock(texteScratch), mathalea2d(Object.assign({}, fixeBordures(lePlateau.objets), {
                     scale: 0.4,
                     style: 'display: inline'
-                }), lePlateau.plateau2d), 35)}`
+                }), lePlateau.objets), 35)}`
         this.autoCorrection[i].propositions = []
         this.autoCorrection[i].propositions.push(
           {
