97bf9333534c787321cdb9ad1f3e490689869083
moragi <gilles.mora@ac-normandie.fr>
2025-01-17 15:53:54 +0100

    can3-Q17

--- src/exercices/can/can3a-2025/can3a-2025-Q17.ts
+++ src/exercices/can/can3a-2025/can3a-2025-Q17.ts
@@ -4,6 +4,7 @@ import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
 import { randint } from '../../../modules/outils'
 import { choice } from '../../../lib/outils/arrayOutils'
 import FractionEtendue from '../../../modules/FractionEtendue'
+import { toutPourUnPoint } from '../../../lib/interactif/mathLive'
 export const titre = 'Additionner un entier et une fraction'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -23,6 +24,7 @@ export default class SommeEntierFraction extends Exercice {
 
     this.typeExercice = 'simple'
     this.nbQuestions = 1
+    this.formatInteractif = 'fillInTheBlank'
     this.formatChampTexte = KeyboardType.clavierDeBaseAvecFraction
     this.canOfficielle = true
     this.optionsChampTexte = { texteAvant: ' $=$' }
@@ -39,20 +41,20 @@ export default class SommeEntierFraction extends Exercice {
     const b = maFraction[0]
     const c = maFraction[1]
     const f = new FractionEtendue(b, c)
-    const d = new FractionEtendue(a * c + b, c).simplifie()
-    const e = new FractionEtendue(a * c - b, c).simplifie()
+    const d = new FractionEtendue(a * c + b, c)
+    const e = new FractionEtendue(a * c - b, c)
     const choix = this.canOfficielle ? true : choice([true, false])
     if (choix === true) {
-      this.question = `$${a}+${f.texFraction}$`
+      this.question = `${a}+${f.texFraction}=\\dfrac{%{champ1}}{%{champ2}}`
       this.correction = `$${a}+${f.texFraction} = \\dfrac{${a} \\times ${c}}{${c}} + \\dfrac{${b}}{${c}} = \\dfrac{${a * c}}{${c}} + \\dfrac{${b}}{${c}}  =${miseEnEvidence(d.texFraction)}$`
-      this.reponse = d
+      this.reponse = { bareme: toutPourUnPoint, champ1: { value: d.n }, champ2: { value: d.d } }
     } else {
-      this.question = `$${a}-${f.texFraction}$`
+      this.question = `${a}-${f.texFraction}=\\dfrac{%{champ1}}{%{champ2}}`
       this.correction = `$${a}-${f.texFraction} = \\dfrac{${a} \\times ${c}}{${c}} - \\dfrac{${b}}{${c}} = \\dfrac{${a * c}}{${c}} - \\dfrac{${b}}{${c}}  =${miseEnEvidence(e.texFraction)}$`
-      this.reponse = e
+      this.reponse = { bareme: toutPourUnPoint, champ1: { value: e.n }, champ2: { value: e.d } }
     }
 
-    this.canEnonce = this.question
+    this.canEnonce = `${choix ? `$${a}+${f.texFraction}$` : `$${a}-${f.texFraction}$`}`
     this.canReponseACompleter = '$\\dfrac{\\ldots}{\\ldots}$'
   }
 }
