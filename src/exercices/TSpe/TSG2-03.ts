832b78da40ab3064fb5062554b619bcb3e280eb5
mathsguyon <mathsguyon@gmail.com>
2025-01-26 14:59:53 +0100

    exo TSG2-03 1ère partie finalisée

--- src/exercices/TSpe/TSG2-03.ts
+++ src/exercices/TSpe/TSG2-03.ts
@@ -1,11 +1,12 @@
 import Exercice from '../Exercice'
 import { combinaisonListes } from '../../lib/outils/arrayOutils'
 import { listeQuestionsToContenu, randint } from '../../modules/outils'
+import { ecritureAlgebriqueSauf1, rienSi1 } from '../../lib/outils/ecritures'
 export const titre = 'Nom de l\'exercice'
 
 export const dateDePublication = '4/5/2024' // La date de publication initiale au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
 
-export const uuid = '55a4e' // Quand on exécute pnpm start la première fois, le terminal renvoie une référence d'uuid, à copier-coller ici
+export const uuid = 'f0619'
 export const refs = {
   'fr-fr': [],
   'fr-ch': []
@@ -23,7 +24,7 @@ export default class nomExercice extends Exercice {
   }
 
   nouvelleVersion () {
-    const typeQuestionsDisponibles = ['type1', 'type2', 'type3'] // On créé 3 types de questions
+    const typeQuestionsDisponibles = ['type1', 'type2'] // On créé 3 types de questions
 
     const listeTypeQuestions = combinaisonListes(typeQuestionsDisponibles, this.nbQuestions) // Tous les types de questions sont posés mais l'ordre diffère à chaque "cycle"
     for (let i = 0, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // Boucle principale où i+1 correspond au numéro de la question
@@ -38,24 +39,27 @@ export default class nomExercice extends Exercice {
       let yM = 0
       let zM = 0
       switch (listeTypeQuestions[i]) { // Suivant le type de question, le contenu sera différent
-        case 'type1':
+        case 'type1':// M appartient à la droite
           xM = xA + t * a
           yM = yA + t * b
           zM = zA + t * c
-          texte = `Déterminer si le point $M(${xM};${yM};${zM}) appartient à la droite $(\\Delta)$$ dont on donne la représentation paramétrique ci-dessous :<br>` // Le LateX entre deux symboles $, les variables dans des ${ }
-          texteCorr = `Correction ${i + 1} de type 1`
+          texteCorr = 'Il suffit de vérifier s\'il existe un réel $t$ qui valide le système pour les coordonnées du point $M$.<br>'
+          texteCorr += `Résolvons le système :<br>$ \\begin{cases}${xM}=${xA}${ecritureAlgebriqueSauf1(a)} t\\\\${yM}=${yA}${ecritureAlgebriqueSauf1(b)} t\\\\${zM}=${zA}${ecritureAlgebriqueSauf1(c)} t\\end{cases}$`
+          texteCorr += `$\\iff \\begin{cases}${rienSi1(a)} t=${xM}${ecritureAlgebriqueSauf1(-xA)}\\\\${rienSi1(b)} t=${yM}${ecritureAlgebriqueSauf1(-yA)}\\\\${rienSi1(c)} t=${zM}${ecritureAlgebriqueSauf1(-zA)}\\end{cases}$`
+          texteCorr += `$\\iff \\begin{cases}${rienSi1(a)} t=${xM - xA}\\\\${rienSi1(b)} t=${yM - yA}\\\\${rienSi1(c)} t=${zM - zA}\\end{cases}$`
+          texteCorr += `$\\iff \\begin{cases} t=${t}\\\\ t=${t}\\\\t=${t}\\end{cases}$`
+          texteCorr += '<br>Pour la valeur $t=2$, on vérifie donc que les coordonnées du point $M$ vérifient la représentation paramétrique de $(\\Delta)$.'
+          texteCorr += '<br>On peut donc conclure : $M\\in(\\Delta)$'
           break
         case 'type2':
+        default :
           texte = `Question ${i + 1} de type 2`
           texteCorr = `Correction ${i + 1} de type 2`
           break
-        case 'type3':
-        default :
-          texte = `Question ${i + 1} de type 3`
-          texteCorr = `Correction ${i + 1} de type 3`
-          break
       }
-      // Si la question n'a jamais été posée, on l'enregistre
+      texte = `Déterminer si le point $M(${xM};${yM};${zM})$ appartient à la droite $(\\Delta)$ dont on donne la représentation paramétrique ci-dessous :<br>`
+      texte += `$(\\Delta) \\begin{cases}x=${xA}${ecritureAlgebriqueSauf1(a)} t\\\\y=${yA}${ecritureAlgebriqueSauf1(b)} t\\quad (t\\in\\mathbb{R})\\\\z=${zA}${ecritureAlgebriqueSauf1(c)} t\\end{cases}$`
+
       if (this.questionJamaisPosee(i, a, b, c, xA, yA, zA, t, texte)) { // <- laisser le i et ajouter toutes les variables qui rendent les exercices différents (par exemple a, b, c et d)
         this.listeQuestions[i] = texte
         this.listeCorrections[i] = texteCorr
