31c0699bef0a7f45563478dd47f87993097637a1
mathsguyon <mathsguyon@gmail.com>
2025-01-30 08:38:21 +0100

    bug Can2G05

--- src/exercices/can/2e/can2G05.ts
+++ src/exercices/can/2e/can2G05.ts
@@ -4,6 +4,7 @@ import { ecritureParentheseSiNegatif } from '../../../lib/outils/ecritures'
 import { creerNomDePolygone } from '../../../lib/outils/outilString'
 import Exercice from '../../Exercice'
 import { randint } from '../../../modules/outils'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
 export const titre = 'Calculer une distance avec les coordonnées'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -26,6 +27,7 @@ export default class DistanceRepere extends Exercice {
 
     this.typeExercice = 'simple'
     this.nbQuestions = 1
+    this.formatChampTexte = KeyboardType.clavierFullOperations
   }
 
   nouvelleVersion () {
