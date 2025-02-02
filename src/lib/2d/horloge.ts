6cce62f26519dedafd243694ef1dd6958dbe1b37
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2025-02-02 13:56:16 +0100

    Amélioration visuelle horloge

--- src/lib/2d/horloge.ts
+++ src/lib/2d/horloge.ts
@@ -2,9 +2,10 @@ import { ObjetMathalea2D } from '../../modules/2dGeneralites'
 import type Hms from '../../modules/Hms'
 import { cercle } from './cercle'
 import { point } from './points'
+import { polygone } from './polygones'
 import { segment, vecteur } from './segmentsVecteurs'
 import { latex2d } from './textes'
-import { rotation, translation } from './transformations'
+import { rotation, similitude, translation } from './transformations'
 
 /**
  * Un objet pour avoir une horloge
@@ -20,35 +21,46 @@ export default class Horloge extends ObjetMathalea2D {
     const C = cercle(O, rayon)
     C.opacite = 0.8
     this.objets.push(C)
-    const rouage = cercle(O, 0.15)
+    const rouage = cercle(O, 0.15, 'black', 'gray')
     rouage.epaisseur = 3
+    const ecartNumeros = 0.5
+    for (let i = 1; i <= 12; i++) {
+      const angleRot = Math.PI / 180 * (90 - i * 30)
+      const numero = latex2d(String(i), x + (rayon - ecartNumeros) * Math.cos(angleRot), y + (rayon - ecartNumeros) * Math.sin(angleRot), { letterSize: 'footnotesize', orientation: 0, opacity: 0.6 })
+      this.objets.push(numero)
+    }
     this.objets.push(rouage)
-    const t = segment(x + rayon - 0.2, y, x + rayon - 0.1, y)
+    const t = segment(x + rayon - 0.2, y, x + rayon - 0.05, y)
     for (let i = 0; i < 4; i++) {
       const tick1 = rotation(t, O, 30 + i * 90)
       const tick2 = rotation(t, O, 60 + i * 90)
       const tick3 = rotation(t, O, 90 + i * 90)
-      const opaciteTick = 0.6
+      const opaciteTick = 0.8
       tick1.opacite = opaciteTick
       tick2.opacite = opaciteTick
       tick3.opacite = opaciteTick
       this.objets.push(tick1, tick2, tick3)
     }
-    const ecartNumeros = 0.45
-    for (let i = 1; i <= 12; i++) {
-      const angleRot = Math.PI / 180 * (90 - i * 30)
-      const numero = latex2d(String(i), x + (rayon - ecartNumeros) * Math.cos(angleRot), y + (rayon - ecartNumeros) * Math.sin(angleRot), { letterSize: 'tiny', orientation: 0, opacity: 0.6 })
-      this.objets.push(numero)
-    }
+
     if (heure != null) {
       const h = heure.hour
       const m = heure.minute
       const alpha = 90 - h * 30 - m / 2
       const beta = 90 - m * 6
-      const grandeAiguille = rotation(segment(translation(O, vecteur(0.2, 0)), point(x + rayon - 0.6, y)), O, beta)
-      const petiteAiguille = rotation(segment(translation(O, vecteur(0.2, 0)), point(x + rayon - 1, y)), O, alpha)
-      petiteAiguille.epaisseur = 3
-      grandeAiguille.epaisseur = 2
+      const epissure = 3
+      const point0 = translation(O, vecteur(0.2, 0))
+      const extTitAig = point(x + rayon - 1, y)
+      const m1TitAig = similitude(extTitAig, point0, epissure, 0.9)
+      const m2TitAig = similitude(extTitAig, point0, -epissure, 0.9)
+      const polyTitAig = polygone(point0, m1TitAig, extTitAig, m2TitAig)
+      const extGdeAig = point(x + rayon - 0.6, y)
+      const m1GdeAig = similitude(extGdeAig, point0, epissure, 0.9)
+      const m2GdeAig = similitude(extGdeAig, point0, -epissure, 0.9)
+      const polyGdeAig = polygone(point0, m1GdeAig, extGdeAig, m2GdeAig)
+      const grandeAiguille = rotation(polyGdeAig, O, beta)
+      const petiteAiguille = rotation(polyTitAig, O, alpha)
+      petiteAiguille.epaisseur = 1
+      grandeAiguille.epaisseur = 1
       this.objets.push(petiteAiguille, grandeAiguille)
     }
   }
