6b29442f75c69a782a779b617fcb9734deb129ea
Rémi Angot <remi.angot@gmail.com>
2025-02-08 19:07:25 +0100

    root directory = alea

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/alea/',
+  base: '/alea/',
   build: {
     target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
     sourcemap: true,
