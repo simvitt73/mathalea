afaefdbaef50bbff46f9e9c72e648aa0c9566955
mathsguyon <mathsguyon@gmail.com>
2025-02-08 23:11:25 +0100

    TSG2-05

--- src/exercices/TSpe/TSG2-05.ts
+++ src/exercices/TSpe/TSG2-05.ts
@@ -1,13 +1,13 @@
 import Exercice from '../Exercice'
 import { combinaisonListes } from '../../lib/outils/arrayOutils'
 import { listeQuestionsToContenu, randint } from '../../modules/outils'
-import { ecritureAlgebriqueSauf1, rienSi1 } from '../../lib/outils/ecritures'
+import { ecritureAlgebrique, ecritureParentheseSiMoins, ecritureParentheseSiNegatif } from '../../lib/outils/ecritures'
 import { texNombre } from '../../lib/outils/texNombre'
 export const titre = 'Déterminer si un vecteur est normal à un plan.'
 
 export const dateDePublication = '26/01/2025' // La date de publication initiale au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
 
-export const uuid = 'f0619'
+export const uuid = 'd5253'
 export const refs = {
   'fr-fr': ['TSG2-05'],
   'fr-ch': []
@@ -21,63 +21,72 @@ export const refs = {
 export default class nomExercice extends Exercice {
   constructor () {
     super()
+    this.nbQuestions = 1
     this.consigne = ''
   }
 
   nouvelleVersion () {
-    const typeQuestionsDisponibles = ['type1', 'type2'] // On créé 3 types de questions
+    const typeQuestionsDisponibles = ['type1', 'type1'] // On créé 3 types de questions
 
     const listeTypeQuestions = combinaisonListes(typeQuestionsDisponibles, this.nbQuestions) // Tous les types de questions sont posés mais l'ordre diffère à chaque "cycle"
     for (let i = 0, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // Boucle principale où i+1 correspond au numéro de la question
-      const xA = randint(-10, 10, 0)
-      const yA = randint(-10, 10, 0)
-      const zA = randint(-10, 10, 0)
-      const xB = randint(-10, 10, [0, xA])
-      const yB = randint(-10, 10, [0, yA])
-      const zB = randint(-10, 10, [0, zA])
-      let xC = randint(-10, 10, [0, xA, xB])
-      const yC = randint(-10, 10, [0, yA, yB])
-      const zC = randint(-10, 10, [0, zA, zB])
-      if ((xC - xA) / (xB - xA) === (yC - yA) / (yB - yA)) { xC = xC + 1 }
-       a=randint(-5,5,0)
-      let b=0
-      let c=0
-      const k=randint(-5,5,[-1,0,1])
+      let a1 = randint(-5, 5, 0)// coord du vecteur AB
+      let b1 = randint(-5, 5, 0)// coord du vecteur AB
+      let c1 = 0// coord du vecteur AB
+      let a2 = randint(-5, 5, [0, a1])// coord du vecteur AC
+      let b2 = randint(-5, 5, [0, b1])// coord du vecteur AC
+      let c2 = 0// coord du vecteur AC
+      const a = randint(-5, 5, 0) // coord du vecteur normal
+      const b = randint(-5, 5, 0)// coord du vecteur normal
+      const c = randint(-5, 5, 0)// coord du vecteur normal
+      const xA = randint(-5, 5, 0) // coord de A
+      const yA = randint(-5, 5, 0)// coord de A
+      const zA = randint(-5, 5, 0)// coord de A
+      let xB = 0
+      let yB = 0
+      let zB = 0
+      let xC = 0
+      let yC = 0
+      let zC = 0
+
       switch (listeTypeQuestions[i]) { // Suivant le type de question, le contenu sera différent
         case 'type1':// Normal
-          
-        texteCorr = 'Il suffit de vérifier s\'il existe un réel $t$ qui valide le système pour les coordonnées du point $M$.<br>'
-          texteCorr += `Résolvons le système :<br><br>$ \\phantom{\\iff }\\begin{cases}${xM}=${xA}${ecritureAlgebriqueSauf1(a)} t\\\\${yM}=${yA}${ecritureAlgebriqueSauf1(b)} t\\\\${zM}=${zA}${ecritureAlgebriqueSauf1(c)} t\\end{cases}$<br>`
-          texteCorr += `<br>$\\iff \\begin{cases}${rienSi1(a)} t=${xM}${ecritureAlgebriqueSauf1(-xA)}\\\\${rienSi1(b)} t=${yM}${ecritureAlgebriqueSauf1(-yA)}\\\\${rienSi1(c)} t=${zM}${ecritureAlgebriqueSauf1(-zA)}\\end{cases}$<br>`
-          texteCorr += `<br>$\\iff \\begin{cases}${rienSi1(a)} t=${xM - xA}\\\\${rienSi1(b)} t=${yM - yA}\\\\${rienSi1(c)} t=${zM - zA}\\end{cases}$<br>`
-          texteCorr += `<br>$\\iff \\begin{cases} t=${t}\\\\ t=${t}\\\\t=${t}\\end{cases}$`
-          texteCorr += `<br>Le système a pour solution $t=${t}$ donc le point M appartient à la droite  $(\\Delta)$.`
+          c1 = -(a * a1 + b * b1)// On calcule c1 pour annuler le pdt scla
+          a1 = a1 * c// on adapte les coord du vect AB pour avoir des coord entières
+          b1 = b1 * c// on adapte les coord du vect AB pour avoir des coord entières
+          c2 = -(a * a2 + b * b2)// idem pour c2
+          a2 = a2 * c// et vecteur AC
+          b2 = b2 * c
+          xB = xA + a1// on déduit les coord de B
+          yB = yA + b1
+          zB = zA + c1
+          xC = xA + a2// et celles de C
+          yC = yA + b2
+          zC = zA + c2
+          texteCorr = 'On calcule les coordonnées des vecteurs $\\overrightarrow{AB}$ et $\\overrightarrow{AC}$ :<br> '
+          texteCorr += `$\\overrightarrow{AB}\\begin{pmatrix}${xB}-${ecritureParentheseSiNegatif(xA)}\\\\${yB}-${ecritureParentheseSiNegatif(yA)}\\\\${zB}-${ecritureParentheseSiNegatif(zA)}\\\\\\end{pmatrix}$ `
+          texteCorr += `$\\iff\\overrightarrow{AB}\\begin{pmatrix}${xB - xA}\\\\${yB - yA}\\\\${zB - zA}\\\\\\end{pmatrix}$<br> `
+          texteCorr += `$\\overrightarrow{AC}\\begin{pmatrix}${xC}-${ecritureParentheseSiNegatif(xA)}\\\\${yC}-${ecritureParentheseSiNegatif(yA)}\\\\${zC}-${ecritureParentheseSiNegatif(zA)}\\\\\\end{pmatrix}$ `
+          texteCorr += `$\\iff\\overrightarrow{AC}\\begin{pmatrix}${xC - xA}\\\\${yC - yA}\\\\${zC - zA}\\\\\\end{pmatrix}$<br> `
+          texteCorr += `On vérifie trivialement qu'ils ne sont pas colinéaires : $\\dfrac{x_B-x_A}{x_C-x_A}=\\dfrac{${texNombre(xB - xA)}}{${texNombre(xC - xA)}}\\neq\\dfrac{y_B-y_A}{y_C-y_A}=\\dfrac{${texNombre(yB - yA)}}{${texNombre(yC - yA)}}$`
+          texteCorr += '<br>$\\overrightarrow{AB}$ et $\\overrightarrow{AC}$ forment donc une base du plan $\\mathcal{ABC}$.'
+          texteCorr += '<br>Pour vérifier si $\\vec n$ est normal au plan $\\mathcal{ABC}$, il suffit de vérifier que le vecteur $\\vec n$ est orthogonal aux deux vecteurs de sa base, donc à $\\overrightarrow{AB}$ et $\\overrightarrow{AC}$.'
+          texteCorr += '<br>On calcule alors les deux produits scalaires :'
+          texteCorr += `<br>$\\begin{aligned}\\overrightarrow{AB}\\cdot\\vec{n}&=${a1}\\times ${ecritureParentheseSiMoins(a)}${ecritureAlgebrique(b1)}\\times ${ecritureParentheseSiMoins(b)}${ecritureAlgebrique(c1)}\\times ${ecritureParentheseSiMoins(c)}\\\\&=${a1 * a + b1 * b + c1 * c}\\end{aligned}$`
+          texteCorr += `<br>$\\begin{aligned}\\overrightarrow{AC}\\cdot\\vec{n}&=${a2}\\times ${ecritureParentheseSiMoins(a)}${ecritureAlgebrique(b2)}\\times ${ecritureParentheseSiMoins(b)}${ecritureAlgebrique(c2)}\\times ${ecritureParentheseSiMoins(c)}\\\\&=${a2 * a + b2 * b + c2 * c}\\end{aligned}$`
+  texteCorr += '<br>On en déduit que $\\vec n$ est orthogonal aux vecteurs $\\overrightarrow{AB}$ et $\\overrightarrow{AC}$, il est donc normal au plan $\\mathcal{ABC}$'
           break
         case 'type2':// pas Normal
         default :
-          xM = xA + t * a
-          yM = yA + t * b
-          zM = zA + t * c
-          if (alea === 1) { xM = xM + randint(-1, 1, 0) }// on détermine quelle coordonnée est affectée d'un +/- 1
-          if (alea === 2) { yM = yM + randint(-1, 1, 0) }
-          if (alea === 3) { zM = zM + randint(-1, 1, 0) }
-
-          texteCorr = 'Il suffit de vérifier s\'il existe un réel $t$ qui valide le système pour les coordonnées du point $M$.<br>'
-          texteCorr += `Résolvons le système :<br><br>$ \\phantom{\\iff } \\begin{cases}${xM}=${xA}${ecritureAlgebriqueSauf1(a)} t\\\\${yM}=${yA}${ecritureAlgebriqueSauf1(b)} t\\\\${zM}=${zA}${ecritureAlgebriqueSauf1(c)} t\\end{cases}$<br>`
-          texteCorr += `<br>$\\iff \\begin{cases}${rienSi1(a)} t=${xM}${ecritureAlgebriqueSauf1(-xA)}\\\\${rienSi1(b)} t=${yM}${ecritureAlgebriqueSauf1(-yA)}\\\\${rienSi1(c)} t=${zM}${ecritureAlgebriqueSauf1(-zA)}\\end{cases}$<br>`
-          texteCorr += `<br>$\\iff \\begin{cases}${rienSi1(a)} t=${xM - xA}\\\\${rienSi1(b)} t=${yM - yA}\\\\${rienSi1(c)} t=${zM - zA}\\end{cases}$<br><br>`
-          if (alea === 1) { texteCorr += `$\\iff \\begin{cases} t=\\dfrac{${texNombre(xM - xA)}}{${texNombre(a)}}\\\\ t=${t}\\\\t=${t}\\end{cases}$<br>` }
-          if (alea === 2) { texteCorr += `$\\iff \\begin{cases} t=${t}\\\\t=\\dfrac{${texNombre(yM - yA)}}{${texNombre(b)}}\\\\ t=${t}\\end{cases}$<br>` }
-          if (alea === 3) { texteCorr += `$\\iff \\begin{cases} t=${t}\\\\t=${t}\\\\t=\\dfrac{${texNombre(zM - zA)}}{${texNombre(c)}}\\end{cases}$<br>` }
 
           texteCorr += '<br>Le système n\'admet pas de solution.  Le point M n\'appartient pas à la droite  $(\\Delta)$.'
 
           break
       }
-      texte = `Déterminer si le point $M(${xM};${yM};${zM})$ appartient à la droite $(\\Delta)$ dont on donne la représentation paramétrique ci-dessous :<br>`
-      texte += `$(\\Delta) \\begin{cases}x=${xA}${ecritureAlgebriqueSauf1(a)} t\\\\y=${yA}${ecritureAlgebriqueSauf1(b)} t\\quad (t\\in\\mathbb{R})\\\\z=${zA}${ecritureAlgebriqueSauf1(c)} t\\end{cases}$`
+      texte = `Dans un repère orthonormé de l'espace, déterminer si le vecteur $\\vec{n}\\begin{pmatrix}${a}\\\\${b}\\\\${c}\\end{pmatrix}$ est normal au plan $\\mathcal{ABC}$ engendré par les points :<br>`
+      texte += `$A(${xA}~;${yA}~;${zA})\\quad B(${xB}~;${yB}~;${zB})\\quad\\text{et}~~C(${xC}~;${yC}~;${zC})$`
 
-      if (this.questionJamaisPosee(i, a, b, c, xA, yA, zA, t, texte)) { // <- laisser le i et ajouter toutes les variables qui rendent les exercices différents (par exemple a, b, c et d)
+      if (this.questionJamaisPosee(i, a, b, c, a2, b2, c2, texte)) { // <- laisser le i et ajouter toutes les variables qui rendent les exercices différents (par exemple a, b, c et d)
         this.listeQuestions[i] = texte
         this.listeCorrections[i] = texteCorr
         i++
