c485c36ac6f54fdb552806348fe3334cb7626e0e
Rémi Angot <remi.angot@gmail.com>
2025-02-23 20:49:11 +0100

    Suppression des liens vers les réseaux sociaux

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -6,20 +6,13 @@
 <a href="https://coopmaths.fr/www/mentions_legales/" target="_blank">
   <img class="hidden md:block px-6" style="height: 20px" src='assets/images/logo2.png' alt="Logo Coopmaths 2" />
 </a>
-  <a
-    class="flex flex-row items-center justify-start pr-6 font-thin text-coopmaths-action hover:text-coopmaths-action-lightest dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
-    href="https://x.com/CoopMaths_fr"
-    target="_blank"
-  >
-    <i class="bx bx-sm bxl-twitter" />
-  </a>
-  <a
+  <!-- <a
     class="flex flex-row items-center justify-start pr-6 font-thin text-coopmaths-action hover:text-coopmaths-action-lightest dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
     href="https://mathstodon.xyz/@coopmaths"
     target="_blank"
   >
     <i class="bx bx-sm bxl-mastodon" />
-  </a>
+  </a> -->
   <a
     class="flex flex-row items-center justify-start pr-6 font-thin text-coopmaths-action hover:text-coopmaths-action-lightest dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
     href="mailto:contact@coopmaths.fr"
