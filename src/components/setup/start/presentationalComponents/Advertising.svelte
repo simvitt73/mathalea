8b497301f5b4bb629b8a23ac23004e1bb852fac5
Rémi Angot <remi.angot@gmail.com>
2025-02-24 21:40:05 +0100

    Homepage lien vers le site de la CAN

--- src/components/setup/start/presentationalComponents/Advertising.svelte
+++ src/components/setup/start/presentationalComponents/Advertising.svelte
@@ -1,1 +1,7 @@
-<img src="/alea/assets/images/homepage/CAN_rebours.png" class="w-3/4  mx-auto" alt="CAN rebours" />
+<a href="https://codimd.apps.education.fr/s/7G91CuKJR" target="_blank">
+  <img
+    src="/alea/assets/images/homepage/CAN_rebours.png"
+    class="w-3/4 mx-auto"
+    alt="CAN rebours"
+  />
+</a>
