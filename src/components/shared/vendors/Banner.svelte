ee8951059fc56f72b822a891539b30d64c5d6417
Sylvain Chambon <chambon.sylvain@gmail.com>
2025-02-27 00:10:42 +0100

    fix: vue smartphone

--- src/components/shared/vendors/Banner.svelte
+++ src/components/shared/vendors/Banner.svelte
@@ -6,14 +6,15 @@
 
 <div
   id="vendor-banner"
-  class="flex items-center justify-center w-full px-2 lg:px-8 bg-coopmaths-canvas"
+  class="flex flex-col items-center justify-center w-full px-2 lg:px-8 bg-coopmaths-canvas"
 >
   <div class="flex items-center justify-between w-full p-4">
-    <img src="assets/svg/logo_mathalea.svg" alt="Dé MathALÉA" class="h-9" />
+    <img src="assets/svg/logo_mathalea.svg" alt="Dé MathALÉA" class="h-7 lg:h-9" />
     <div class="flex flex-row m-0 p-0 items-center justify-center space-x-3">
-      <img class="h-8" src={productImagePath} alt="Manual" />
-      <img class="h-8" src={brandImagePath} alt="Brand" />
+      <img class="h-6 lg:h-8" src={productImagePath} alt="Manual" />
+      <img class="h-6 lg:h-8" src={brandImagePath} alt="Brand" />
     </div>
-    <ButtonIcon icon="bxs-cog" class="text-3xl" />
+    <ButtonIcon icon="bxs-cog" class="hidden lg:block text-3xl" />
   </div>
+  <ButtonIcon icon="bxs-cog" class="block lg:hidden text-3xl" />
 </div>
