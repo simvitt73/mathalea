52299b6e60d58ff3a900bd17a51dfb0d2c43d984
gillesMora <gilles.mora@ac-normandie.fr>
2025-02-26 18:02:08 +0100

    fix: can2-Q26

--- src/exercices/can/can2a-2025/can2a-2025-Q26.ts
+++ src/exercices/can/can2a-2025/can2a-2025-Q26.ts
@@ -37,16 +37,16 @@ export default class CoeffDirecteurGraphique extends Exercice {
   nouvelleVersion () {
     let lABx
     const xA = this.canOfficielle ? 0 : randint(-4, 0)
-    const yA = this.canOfficielle ? -1 : randint(-5, 5) / 2
-    const xB = this.canOfficielle ? 3 : randint(1, 4)
-    const yB = this.canOfficielle ? 1 : randint(-5, 5, 0) / 2
+    const yA = this.canOfficielle ? -1 : randint(-4, 4)
+    const xB = this.canOfficielle ? 3 : xA + randint(3, 4)
+    const yB = this.canOfficielle ? 1 : randint(-4, 4, [0, yA])
     const o = latex2d('\\text{O}', -0.2, -0.3, { color: 'black', letterSize: 'scriptsize', backgroundColor: '' })
     const A = point(xA, yA)
     const B = point(xB, yB)
     const Bx = point(B.x, A.y)
     const sABx = segment(A, Bx)
     const sBBx = segment(B, Bx)
-    const m = new FractionEtendue(2 * (yB - yA), xB - xA)
+    const m = new FractionEtendue(yB - yA, xB - xA)
     sBBx.epaisseur = 2
     sBBx.pointilles = 5
     sABx.epaisseur = 2
@@ -55,7 +55,7 @@ export default class CoeffDirecteurGraphique extends Exercice {
     const traceA = tracePoint(A, 'black') // Variable qui trace les points avec une croix
     const lB = latex2d('B', xB, yB + 0.5, { color: 'black', backgroundColor: '' })
     if (yA > yB) { lABx = latex2d(`${xB - xA}`, milieu(A, Bx).x, A.y + 0.3, { color: 'red', backgroundColor: '' }) } else { lABx = latex2d(`${xB - xA}`, milieu(A, Bx).x, A.y - 0.3, { color: 'red', backgroundColor: '' }) }
-    const lBBx = latex2d(`${2 * (yB - yA)}`, B.x + 0.5, milieu(B, Bx).y, { color: 'blue', backgroundColor: '' })
+    const lBBx = latex2d(`${yB - yA}`, B.x + 0.5, milieu(B, Bx).y, { color: 'blue', backgroundColor: '' })
     const traceB = tracePoint(B, 'black') // Variable qui trace les points avec une croix
     const d = droite(A, B, '', 'blue')
     d.epaisseur = 2
@@ -64,30 +64,30 @@ export default class CoeffDirecteurGraphique extends Exercice {
     traceB.taille = 2
     traceB.epaisseur = 2
     const xmin = -5
-    const ymin = Math.min(yA, yB, 0) - 1
+    const ymin = -5
     const xmax = 5
-    const ymax = Math.max(yA, yB, 0) + 1
+    const ymax = 5
     const r1 = repere({
       xMin: xmin,
       xMax: xmax,
       xUnite: 1,
-      yMin: 2 * ymin,
-      yMax: 2 * ymax,
-      yUnite: 0.5,
+      yMin: ymin,
+      yMax: ymax,
+      yUnite: 1,
       thickHauteur: 0.1,
       xLabelMin: xmin + 1,
       xLabelMax: xmax - 1,
-      yLabelMax: 2 * ymax - 1,
-      yLabelMin: 2 * ymin + 1,
+      yLabelMax: ymax - 1,
+      yLabelMin: ymin + 1,
       axeXStyle: '->',
       axeYStyle: '->',
-      yLabelDistance: 2,
-      yLabelEcart: 0.4,
+      yLabelDistance: 1,
+      yLabelEcart: 0.6,
       grilleXDistance: 1,
-      grilleYDistance: 0.5
+      grilleYDistance: 1
     })
-    const objet = mathalea2d({ xmin, xmax, ymin, ymax: ymax + 0.25, pixelsParCm: 30, scale: 0.75, style: 'margin: auto' }, d, r1, traceA, lA, lB, traceB, o)
-    const objetC = mathalea2d({ xmin, xmax, ymin, ymax: ymax + 0.25, pixelsParCm: 30, scale: 0.75, style: 'margin: auto' }, d, r1, traceA, lA, lB, traceB, o, sABx, sBBx, lABx, lBBx)
+    const objet = mathalea2d({ xmin, xmax, ymin: ymin - 0.25, ymax: ymax + 0.25, pixelsParCm: 25, scale: 0.6, style: 'margin: auto' }, d, r1, traceA, lA, lB, traceB, o)
+    const objetC = mathalea2d({ xmin, xmax, ymin: ymin - 0.25, ymax: ymax + 0.25, pixelsParCm: 25, scale: 0.6, style: 'margin: auto' }, d, r1, traceA, lA, lB, traceB, o, sABx, sBBx, lABx, lBBx)
 
     this.question = 'Coefficient directeur de la droite $(AB)$.<br>'
     this.question += `${objet}`
@@ -96,7 +96,7 @@ export default class CoeffDirecteurGraphique extends Exercice {
       this.correction = `La droite est horizontale. <br>On en déduit que $m=${miseEnEvidence('0')}$.`
     } else {
       this.correction = `Le coefficient directeur $m$ de la droite $(AB)$ est donné par :<br><br>
-            $m=\\dfrac{y_B-y_A}{x_B-x_A}=\\dfrac{${2 * yB}-${ecritureParentheseSiNegatif(2 * yA)}}{${xB}-${ecritureParentheseSiNegatif(xA)}}=\\dfrac{${miseEnEvidence(2 * (yB - yA), 'blue')}}{${miseEnEvidence(xB - xA, 'red')}}${miseEnEvidence(m.texSimplificationAvecEtapes())}$.<br><br>`
+            $m=\\dfrac{y_B-y_A}{x_B-x_A}=\\dfrac{${yB}-${ecritureParentheseSiNegatif(yA)}}{${xB}-${ecritureParentheseSiNegatif(xA)}}=\\dfrac{${miseEnEvidence(yB - yA, 'blue')}}{${miseEnEvidence(xB - xA, 'red')}}${miseEnEvidence(m.texSimplificationAvecEtapes())}$.<br><br>`
       this.correction += `${objetC}`
     }
 
