4c6f86ebc58d7282f931b9573d34d6cf84b26410
Guillaume Valmont <guillaume.valmont@ac-reunion.fr>
2025-02-28 13:56:05 +0400

    Ajout de newExercise.test.ts qui fait des captures d'écran de toutes les vues

---
+++ tests/e2e/vitest.config.new_exercise.js
@@ -0,0 +1,26 @@
+import { mergeConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
+import viteConfig from './vite.config'
+import { resolve } from 'node:path'
+
+const arg = process.argv[2]
+
+console.log('Running newExercise test with', arg)
+
+export default mergeConfig(viteConfig, defineConfig({
+  resolve: {
+    alias: {
+      testBrowser: resolve(__dirname, 'e2e')
+    }
+  },
+  test: {
+    include: ['./tests/new_exercise/newExercise.test.{js,ts}'],
+    hookTimeout: 600_000,
+    testTimeout: 60_000,
+    poolOptions: {
+      threads: {
+        singleThread: true
+      }
+    }
+  }
+}))
