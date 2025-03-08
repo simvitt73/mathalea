c835bcc079641a55bbccc827f8f2f0261f3999f4
Rémi Angot <remi.angot@gmail.com>
2025-03-08 18:04:28 +0100

    fix: on ne cache plus les figures apigeom lorsqu'on montre la correction en vue prof

--- src/components/shared/exercice/exerciceMathalea/exerciceMathaleaVueProf/ExerciceMathaleaVueProf.svelte
+++ src/components/shared/exercice/exerciceMathalea/exerciceMathaleaVueProf/ExerciceMathaleaVueProf.svelte
@@ -570,10 +570,6 @@
     on:clickCorrection={async (event) => {
       isContentVisible = event.detail.isContentVisible
       isCorrectionVisible = event.detail.isCorrectionVisible
-      // Cache les éventuelles figureApigeom
-      divExercice?.querySelectorAll('[id^="apigeomEx"]').forEach((e) => {
-        e.classList.toggle('hidden', isCorrectionVisible)
-      })
       if (
         isLocalStorageAvailable() &&
         exercise.id !== undefined &&
