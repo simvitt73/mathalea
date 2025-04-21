bd1c1981a8c85b91def786aff4255f196ba3a7c0
EricElter <eric.elter@ac-normandie.fr>
2025-04-21 22:24:08 +0200

    feat : 5N20 avec numérateurs possiblement négatifs #7993

--- src/exercices/5e/5N20.ts
+++ src/exercices/5e/5N20.ts
@@ -8,30 +8,26 @@ import { ajouteChampTexteMathLive } from '../../lib/interactif/questionMathLive'
 import FractionEtendue from '../../modules/FractionEtendue'
 import { propositionsQcm } from '../../lib/interactif/qcm'
 import { handleAnswers } from '../../lib/interactif/gestionInteractif'
-import { fraction } from '../../modules/fractions'
 import { texNombre } from '../../lib/outils/texNombre'
 import { sp } from '../../lib/outils/outilString'
-
-export const dateDeModifImportante = '09/04/2025'
+import { ecritureParentheseSiNegatif } from '../../lib/outils/ecritures'
+export const dateDeModifImportante = '21/04/2025'
 export const amcReady = true
 export const amcType = 'qcmMono'
 export const interactifReady = true
 export const interactifType = ['qcm', 'mathLive']
-
 export const titre = 'Additionner ou soustraire deux fractions (dénominateurs multiples)'
-
 /**
  * Effectuer l'addition ou la soustraction de deux fractions dont un dénominateur est un multiple de l'autre.
- *
- * Le résultat de la soustraction sera toujours positif.
+ * Les numérateurs peuvent être positifs ou négatifs.
  *
  * Le coefficient est paramétrable, par défaut il est inférieur à 11.
  *
  * On peut paramétrer de n'avoir que des soustractions.
- * @author Rémi Angot
+ * @author Rémi Angot, modifié par Eric Elter
+ * Modif : Rajout des numérateurs négatifs par Eric Elter
  */
 export const uuid = 'd5ee3'
-
 export const refs = {
   'fr-fr': ['5N20', 'BP2AutoH22'],
   'fr-ch': ['9NO13-6']
@@ -43,7 +39,8 @@ export default class ExerciceAdditionnerSoustraireFractions5ebis extends Exercic
     this.sup = 11 // Correspond au facteur commun
     this.sup2 = 3 // Si 1 alors il n'y aura pas de soustraction
     this.sup3 = true // Si false alors le résultat n'est pas en fraction simplifiée
-    this.sup4 = true
+    this.sup4 = true // QCM pour l'interactif
+    this.sup5 = 50 // Pourcentage de fractions avec numérateur négatif
     this.spacing = 2
     this.spacingCorr = 2
     this.nbQuestions = 5
@@ -51,7 +48,7 @@ export default class ExerciceAdditionnerSoustraireFractions5ebis extends Exercic
     this.besoinFormulaire2Numerique = ['Type de calculs', 3, '1 : Additions\n2 : Soustractions\n3 : Mélange']
     this.besoinFormulaire3CaseACocher = ['Avec l\'écriture simplifiée de la fraction résultat']
     this.besoinFormulaire4CaseACocher = ['QCM pour l\'interactif']
-
+    this.besoinFormulaire5Numerique = ['Pourcentage de fractions avec numérateur négatif', 100]
     this.level = 5
   }
 
@@ -67,7 +64,9 @@ export default class ExerciceAdditionnerSoustraireFractions5ebis extends Exercic
         this.consigne += this.interactif ? ' au brouillon et indiquer seulement le résultat final.' : '.'
       }
     }
+
     let listeTypeDeQuestions
+
     if (this.sup2 === 1) {
       listeTypeDeQuestions = combinaisonListes(['+'], this.nbQuestions)
     } else if (this.sup2 === 2) {
@@ -75,44 +74,57 @@ export default class ExerciceAdditionnerSoustraireFractions5ebis extends Exercic
     } else {
       listeTypeDeQuestions = combinaisonListes(['+', '-'], this.nbQuestions)
     }
+
     this.interactifType = this.sup4 ? 'qcm' : 'mathLive'
+
     for (let i = 0, a, b, c, d, k, s, ordreDesFractions, texte, texteCorr; i < this.nbQuestions;) {
       this.autoCorrection[i] = {}
       texte = ''
       texteCorr = ''
-      // les numérateurs
-      a = randint(1, 9)
-      // les dénominateurs
-      b = randint(2, 9, a)
+
+      // Décision si les numérateurs seront négatifs ou non
+      const aNegatif = randint(1, 100) <= this.sup5
+      const cNegatif = randint(1, 100) <= this.sup5
+
+      // Les numérateurs (positifs ou négatifs selon décision précédente)
+      a = randint(1, 9) * (aNegatif ? -1 : 1)
+
+      // Les dénominateurs (toujours positifs)
+      b = randint(2, 9, Math.abs(a))
+
       if (this.sup > 1) {
         k = randint(2, this.sup)
       } else k = 1
+
       d = b * k
+
       if (listeTypeDeQuestions[i] === '-') {
-        c = choice([randint(1, b * k), randint(b * k, 9 * k)])
+        c = choice([randint(1, b * k), randint(b * k, 9 * k)]) * (cNegatif ? -1 : 1)
       } else {
-        c = randint(1, 19, d)
+        c = randint(1, 19, d) * (cNegatif ? -1 : 1)
       }
+
       const f1 = new FractionEtendue(a, b)
       const f2 = new FractionEtendue(c, d)
+
       if (listeTypeDeQuestions[i] === '+') { // une addition
         if (!context.isAmc || (this.interactif && this.interactifType === 'qcm')) {
           /** ***************** Choix des réponses du QCM ***********************************/
           this.autoCorrection[i].propositions = [
             {
-              texte: this.sup3 ? `$${fraction(a * k + c, d).texFractionSimplifiee}$` : `$${fraction(a * k + c, d).texFraction}$`,
+              texte: this.sup3 ? `$${new FractionEtendue(a * k + c, d).texFractionSimplifiee}$` : `$${new FractionEtendue(a * k + c, d).texFraction}$`,
               statut: true
             },
             {
-              texte: this.sup3 ? `$${fraction(a + c, d).texFractionSimplifiee}$` : `$${fraction(a + c, d).texFraction}$`,
+              texte: this.sup3 ? `$${new FractionEtendue(a + c, d).texFractionSimplifiee}$` : `$${new FractionEtendue(a + c, d).texFraction}$`,
               statut: false
             },
             {
-              texte: this.sup3 ? `$${fraction(a + c, b + d).texFractionSimplifiee}$` : `$${fraction(a + c, b + d).texFraction}$`,
+              texte: this.sup3 ? `$${new FractionEtendue(a + c, b + d).texFractionSimplifiee}$` : `$${new FractionEtendue(a + c, b + d).texFraction}$`,
               statut: false
             },
             {
-              texte: this.sup3 ? `$${fraction(a * c, d).texFractionSimplifiee}$` : `$${fraction(a * c, d).texFraction}$`,
+              texte: this.sup3 ? `$${new FractionEtendue(a * c, d).texFractionSimplifiee}$` : `$${new FractionEtendue(a * c, d).texFraction}$`,
               statut: false
             }
           ]
@@ -121,54 +133,64 @@ export default class ExerciceAdditionnerSoustraireFractions5ebis extends Exercic
             lastChoice: 5
           }
           if (this.level === 6) {
-          // En 6e, pas de fraction simplifiée
-          // Les fractions ont le même dénominateur (b=d)
-          // @ts-expect-error
-            this.autoCorrection[i].propositions[0].texte = `$${fraction(a + c, b).texFraction}$`
+            // En 6e, pas de fraction simplifiée
+            // Les fractions ont le même dénominateur (b=d)
+            // @ts-expect-error
+            this.autoCorrection[i].propositions[0].texte = `$${new FractionEtendue(a + c, b).texFraction}$`
           }
           /*************************************************************************/
-          ordreDesFractions = randint(1, 2)
-          if (ordreDesFractions === 1) {
-            texte = `$${f1.texFraction}+${f2.texFraction}$`
-            /** ****************** AMC question/questionmult ********************************/
-            this.autoCorrection[i].enonce = `${texte}\n`
-          /*******************************************************************************/
-          } else {
-            texte = `$${f2.texFraction}+${f1.texFraction}$`
-            /** ****************** AMC question/questionmult ******************************/
-            this.autoCorrection[i].enonce = `${texte}\n`
-          /*******************************************************************************/
-          }
         }
+
+        ordreDesFractions = randint(1, 2)
+
+        if (ordreDesFractions === 1) {
+          texte = `$${f1.texFraction}+${f2.texFraction}$`
+          /** ****************** AMC question/questionmult ********************************/
+          this.autoCorrection[i].enonce = `${texte}\n`
+          /*******************************************************************************/
+        } else {
+          texte = `$${f2.texFraction}+${f1.texFraction}$`
+          /** ****************** AMC question/questionmult ******************************/
+          this.autoCorrection[i].enonce = `${texte}\n`
+          /*******************************************************************************/
+        }
+
         if (ordreDesFractions === 1) {
           texteCorr = `$${f1.texFraction}+${f2.texFraction}=`
           texte = `$${f1.texFraction}+${f2.texFraction}$`
           if (this.level !== 6) {
-            texteCorr += `\\dfrac{${a}${miseEnEvidence('\\times ' + k, 'blue')}}{${b}${miseEnEvidence('\\times ' + k, 'blue')}}+${f2.texFraction}=${fraction(a * k, b * k).texFraction}+${f2.texFraction}=`
+            texteCorr += `\\dfrac{${a}${miseEnEvidence('\\times ' + k, 'blue')}}{${b}${miseEnEvidence('\\times ' + k, 'blue')}}+${f2.texFraction}=${new FractionEtendue(a * k, b * k).texFraction}+${f2.texFraction}=`
           }
-          texteCorr += `\\dfrac{${a * k}+${c}}{${d}}=${fraction(a * k + c, d).texFraction}$`
+          texteCorr += `\\dfrac{${a * k}+${ecritureParentheseSiNegatif(c)}}{${d}}=${new FractionEtendue(a * k + c, d).texFraction}`
         } else {
           texteCorr = `$${f2.texFraction}+${f1.texFraction}=`
           texte = `$${f2.texFraction}+${f1.texFraction}$`
           if (this.level !== 6) {
-            texteCorr += `${f2.texFraction}+\\dfrac{${a}${miseEnEvidence('\\times ' + k, 'blue')}}{${b}${miseEnEvidence('\\times ' + k, 'blue')}}=${f2.texFraction}+${fraction(a * k, b * k).texFraction}=`
+            texteCorr += `${f2.texFraction}+\\dfrac{${a}${miseEnEvidence('\\times ' + k, 'blue')}}{${b}${miseEnEvidence('\\times ' + k, 'blue')}}=${f2.texFraction}+${new FractionEtendue(a * k, b * k).texFraction}=`
           }
-          texteCorr += `\\dfrac{${c}+${a * k}}{${d}}=${fraction(a * k + c, d).texFraction}$`
+          texteCorr += `\\dfrac{${c}+${ecritureParentheseSiNegatif(a * k)}}{${d}}=${new FractionEtendue(a * k + c, d).texFraction}`
         }
+        texteCorr += a * k + c < 0 ? `=${new FractionEtendue(a * k + c, d).texFSD}$` : '$'
+
         // Est-ce que le résultat est simplifiable ?
         if (this.sup3) {
-          s = pgcd(a * k + c, d)
+          s = pgcd(Math.abs(a * k + c), d)
           if (s !== 1) {
-            texteCorr += `$=\\dfrac{${(a * k + c) / s}${miseEnEvidence('\\times ' + s, 'blue')}}{${d / s}${miseEnEvidence('\\times ' + s, 'blue')}}=${fraction((a * k + c) / s, d / s).texFractionSimplifiee}$`
+            if (a * k + c < 0) {
+              texteCorr += `$=-\\dfrac{${Math.abs(a * k + c) / s}${miseEnEvidence('\\times ' + s, 'blue')}}{${d / s}${miseEnEvidence('\\times ' + s, 'blue')}}=${new FractionEtendue((a * k + c) / s, d / s).texFractionSimplifiee}$`
+            } else {
+              texteCorr += `$=\\dfrac{${(a * k + c) / s}${miseEnEvidence('\\times ' + s, 'blue')}}{${d / s}${miseEnEvidence('\\times ' + s, 'blue')}}=${new FractionEtendue((a * k + c) / s, d / s).texFractionSimplifiee}$`
+            }
           }
         }
+
         if (!context.isAmc) {
           if (this.interactif && this.interactifType === 'qcm') {
             const props = propositionsQcm(this, i)
             texte += '<br>' + props.texte
           } else if (this.interactifType === 'mathLive') {
             texte += ajouteChampTexteMathLive(this, i, '  clavierDeBaseAvecFraction', { texteAvant: sp() + '$=$' })
-            handleAnswers(this, i, { reponse: { value: new FractionEtendue(a * d + c * b, b * d).toLatex(), options: { fractionIrreductible: this.sup3, fractionEgale: !this.sup3 } } })
+            handleAnswers(this, i, { reponse: { value: new FractionEtendue(a * k + c, d).toLatex(), options: { fractionIrreductible: this.sup3, fractionEgale: !this.sup3 } } })
           }
         }
       } else { // une soustraction
@@ -176,19 +198,19 @@ export default class ExerciceAdditionnerSoustraireFractions5ebis extends Exercic
         if (!context.isAmc || (this.interactif && this.interactifType === 'qcm')) {
           this.autoCorrection[i].propositions = [
             {
-              texte: this.sup3 ? `$${fraction(Math.abs(a * k - c), Math.abs(d)).texFractionSimplifiee}$` : `$${fraction(Math.abs(a * k - c), Math.abs(d)).texFraction}$`,
+              texte: this.sup3 ? `$${new FractionEtendue(a * k - c, d).texFractionSimplifiee}$` : `$${new FractionEtendue(a * k - c, d).texFraction}$`,
               statut: true
             },
             {
-              texte: this.sup3 ? `$${fraction(Math.abs(a - c), Math.abs(b - d)).texFractionSimplifiee}$` : `$${fraction(Math.abs(a - c), Math.abs(b - d)).texFraction}$`,
+              texte: this.sup3 ? `$${new FractionEtendue(a - c, Math.abs(b - d)).texFractionSimplifiee}$` : `$${new FractionEtendue(a - c, Math.abs(b - d)).texFraction}$`,
               statut: false
             },
             {
-              texte: this.sup3 ? `$${fraction(Math.abs(a - c), d).texFractionSimplifiee}$` : `$${fraction(Math.abs(a - c), d).texFraction}$`,
+              texte: this.sup3 ? `$${new FractionEtendue(a - c, d).texFractionSimplifiee}$` : `$${new FractionEtendue(a - c, d).texFraction}$`,
               statut: false
             },
             {
-              texte: this.sup3 ? `$${fraction(a * c, d).texFractionSimplifiee}$` : `$${fraction(a * c, d).texFraction}$`,
+              texte: this.sup3 ? `$${new FractionEtendue(a * c, d).texFractionSimplifiee}$` : `$${new FractionEtendue(a * c, d).texFraction}$`,
               statut: false
             }
           ]
@@ -197,48 +219,39 @@ export default class ExerciceAdditionnerSoustraireFractions5ebis extends Exercic
             lastChoice: 5
           }
           if (this.level === 6) {
-          // En 6e, pas de fraction simplifiée
-          // Les fractions ont le même dénominateur (b=d)
-          // @ts-expect-error
-            this.autoCorrection[i].propositions[0].texte = `$${fraction(Math.abs(a - c), b).texFraction}$`
+            // En 6e, pas de fraction simplifiée
+            // Les fractions ont le même dénominateur (b=d)
+            // @ts-expect-error
+            this.autoCorrection[i].propositions[0].texte = `$${new FractionEtendue(a - c, b).texFraction}$`
           }
-
           /*********************************************************************************/
         }
-        let reponse
-        if ((a / b) > (c / d)) {
-          texte = `$${f1.texFraction}-${f2.texFraction}$`
-          reponse = new FractionEtendue(a * d - c * b, b * d).toLatex()
-        } else {
-          texte = `$${f2.texFraction}-${f1.texFraction}$`
-          reponse = new FractionEtendue(-a * d + c * b, b * d).toLatex()
-          /** ****************** AMC question/questionmult ******************************/
 
-          /*****************************************************************************/
-        }
+        // On ne vérifie plus si a/b > c/d car on accepte les fractions négatives comme résultat
+        texte = `$${f1.texFraction}-${f2.texFraction}$`
+        const reponse = new FractionEtendue(a * k - c, d).toLatex()
 
-        if ((a / b) > (c / d)) {
-          texteCorr = `$${f1.texFraction}-${f2.texFraction}=`
-          if (this.level !== 6) {
-            texteCorr += `\\dfrac{${a}${miseEnEvidence('\\times ' + k, 'blue')}}{${b}${miseEnEvidence('\\times ' + k, 'blue')}}-${f2.texFraction}=${fraction(a * k, b * k).texFraction}-${f2.texFraction}=`
-          }
-          texteCorr += `\\dfrac{${a * k}-${c}}{${d}}=${fraction(a * k - c, d).texFraction}$`
-        } else {
-          texteCorr = `$${f2.texFraction}-${f1.texFraction}=`
-          if (this.level !== 6) {
-            texteCorr += `${f2.texFraction}-\\dfrac{${a}${miseEnEvidence('\\times ' + k, 'blue')}}{${b}${miseEnEvidence('\\times ' + k, 'blue')}}=${f2.texFraction}-${fraction(a * k, b * k).texFraction}=\\dfrac{${c}-${a * k}}{${d}}=`
-          }
-          texteCorr += `${fraction(c - a * k, d).texFraction}$`
+        texteCorr = `$${f1.texFraction}-${f2.texFraction}=`
+        if (this.level !== 6) {
+          texteCorr += `\\dfrac{${a}${miseEnEvidence('\\times ' + k, 'blue')}}{${b}${miseEnEvidence('\\times ' + k, 'blue')}}-${f2.texFraction}=${new FractionEtendue(a * k, b * k).texFraction}-${f2.texFraction}=`
         }
+        texteCorr += `\\dfrac{${a * k}-${ecritureParentheseSiNegatif(c)}}{${d}}=${new FractionEtendue(a * k - c, d).texFraction}`
+        texteCorr += a * k - c < 0 ? `=${new FractionEtendue(a * k - c, d).texFSD}$` : '$'
+
         // Est-ce que le résultat est simplifiable ?
         if (this.sup3) {
           s = pgcd(Math.abs(a * k - c), d)
-          if (Math.abs(a * k - c) % d === 0) { // si la fraction peut-être un nombre entier
-            texteCorr += `$=${texNombre(Math.abs(a * k - c) / d, 0)}$`
+          if (Math.abs(a * k - c) % d === 0) { // Si la fraction peut être un nombre entier
+            texteCorr += `$=${texNombre((a * k - c) / d, 0)}$`
           } else if (s !== 1) {
-            texteCorr += `$=\\dfrac{${Math.abs(a * k - c) / s}${miseEnEvidence('\\times ' + s, 'blue')}}{${d / s}${miseEnEvidence('\\times ' + s, 'blue')}}=${fraction((Math.abs(a * k - c) / s), d / s).texFractionSimplifiee}$`
+            if (a * k - c < 0) {
+              texteCorr += `$=-\\dfrac{${Math.abs(a * k - c) / s}${miseEnEvidence('\\times ' + s, 'blue')}}{${d / s}${miseEnEvidence('\\times ' + s, 'blue')}}=${new FractionEtendue((a * k - c) / s, d / s).texFractionSimplifiee}$`
+            } else {
+              texteCorr += `$=\\dfrac{${(a * k - c) / s}${miseEnEvidence('\\times ' + s, 'blue')}}{${d / s}${miseEnEvidence('\\times ' + s, 'blue')}}=${new FractionEtendue((a * k - c) / s, d / s).texFractionSimplifiee}$`
+            }
           }
         }
+
         if (!context.isAmc) {
           if (this.interactif && this.interactifType === 'qcm') {
             const props = propositionsQcm(this, i)
@@ -249,29 +262,32 @@ export default class ExerciceAdditionnerSoustraireFractions5ebis extends Exercic
           }
         }
       }
-      texte = texte.replaceAll('$$', ' ')
 
+      texte = texte.replaceAll('$$', ' ')
       texteCorr = texteCorr.replaceAll('$$', ' ')
+
       if (context.isAmc) {
         this.autoCorrection[i].enonce = texte
       }
+
       // Uniformisation : Mise en place de la réponse attendue en interactif en orange et gras
       const textCorrSplit = texteCorr.split('=')
       let aRemplacer = textCorrSplit[textCorrSplit.length - 1]
       aRemplacer = aRemplacer.replace('$', '').replace('<br>', '')
-
       texteCorr = ''
       for (let ee = 0; ee < textCorrSplit.length - 1; ee++) {
         texteCorr += textCorrSplit[ee] + '='
       }
       texteCorr += `$ $${miseEnEvidence(aRemplacer)}$`
       // Fin de cette uniformisation
+
       if (this.questionJamaisPosee(i, a, k, b, c)) {
         this.listeQuestions[i] = texte
         this.listeCorrections[i] = texteCorr
         i++
       }
     }
+
     listeQuestionsToContenu(this) // Espacement de 2 em entre chaque questions.
   }
 }
