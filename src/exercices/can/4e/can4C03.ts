c9023cf055af86ec362cd78cbabd5eb544b54034
gillesMora <gilles.mora@ac-normandie.fr>
2025-05-17 08:46:01 +0200

    fix: can4C03

--- src/exercices/can/4e/can4C03.ts
+++ src/exercices/can/4e/can4C03.ts
@@ -1,13 +1,15 @@
 import { choice } from '../../../lib/outils/arrayOutils'
-import { texteEnCouleur } from '../../../lib/outils/embellissements'
+import { miseEnEvidence, texteEnCouleur } from '../../../lib/outils/embellissements'
 import { obtenirListeFractionsIrreductibles } from '../../../modules/fractions'
 import FractionEtendue from '../../../modules/FractionEtendue'
 import Exercice from '../../Exercice'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
 export const titre = 'Effectuer une division avec une fraction'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 export const amcReady = true
 export const amcType = 'AMCNum'
+export const dateDeModifImportante = '17/05/2025'
 
 /**
  * @author Jean-Claude Lhote
@@ -25,27 +27,27 @@ export default class QuotientEntierQuiVaBienParFraction extends Exercice {
     super()
     this.typeExercice = 'simple'
     this.nbQuestions = 1
-
-    this.formatInteractif = 'fractionEgale'
+    this.formatChampTexte = KeyboardType.clavierDeBaseAvecFraction
+    this.optionsDeComparaison = { fractionEgale: true }
   }
 
   nouvelleVersion () {
     const a = choice(obtenirListeFractionsIrreductibles())
     const c = choice([2, 3, 4, 5, 6])
     const b = a.n * c
-    this.question = `Calculer $${b}\\div ${a.texFraction}$.`
+    this.question = `Calculer $${b}\\div ${a.texFraction}$.<br>`
     this.reponse = new FractionEtendue(b * a.d, a.n)
     if (a.n === 1) {
       this.correction = `Diviser par un nombre revient à multiplier par son inverse. <br>
     Ici, on divise par $${a.texFraction}$, donc cela revient à multiplier par son inverse : $${a.inverse().texFraction}$.<br>
     $${b}\\div ${a.texFraction}=${b}\\times ${a.inverse().texFraction}=
-        ${c * a.d}$`
+        ${miseEnEvidence(c * a.d)}$`
     } else {
       this.correction = `Diviser par un nombre revient à multiplier par son inverse. <br>
     Ici, on divise par $${a.texFraction}$, donc cela revient à multiplier par son inverse :  $${a.inverse().texFraction}$.<br>
     $${b}\\div ${a.texFraction}=${b}\\times ${a.inverse().texFraction}=
     \\dfrac{${b}\\times ${a.d}}{${a.n}}=
-    ${c}\\times ${a.d}=${c * a.d}$`
+    ${c}\\times ${a.d}=${miseEnEvidence(c * a.d)}$`
       this.correction += texteEnCouleur(`<br> Mentalement : <br>
     Pour multiplier $${b}$ par $${a.inverse().texFraction}$,
     on commence par diviser $${b}$ par $${a.n}$, ce qui donne $${b / a.n}$,
