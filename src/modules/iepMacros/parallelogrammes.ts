80b7a36400ba4ed95c72ab2752bc53f003e7d5b2
Guillaume Valmont <guillaume.valmont@ac-reunion.fr>
2025-06-01 13:17:43 +0400

    Typage de parallelogrammes.ts

--- src/modules/iepMacros/parallelogrammes.ts
+++ src/modules/iepMacros/parallelogrammes.ts
@@ -60,7 +60,7 @@ export function parallelogramme3sommetsConsecutifs (this: Alea2iep, A: PointAbst
      * @param {string} nomD
      * @param {boolean} description
      */
-export function parallelogramme2sommetsConsecutifsCentre (A, B, O, nomC = '', nomD = '', description = true) {
+export function parallelogramme2sommetsConsecutifsCentre (this: Alea2iep, A: PointAbstrait, B: PointAbstrait, O: PointAbstrait, nomC = '', nomD = '', description = true) {
   const C = translation2Points(O, A, O)
   C.nom = nomC
   const D = translation2Points(O, B, O)
@@ -80,7 +80,7 @@ export function parallelogramme2sommetsConsecutifsCentre (A, B, O, nomC = '', no
   if (description && nom.length === 4) {
     this.textePosition(`${A.nom + B.nom + C.nom + D.nom} est un parallélogramme donc ses diagonales se coupent en leur milieu.`, xMin - 1, yMin - 1)
   }
-  this.pointilles = 5
+  this.pointilles = true
   this.epaisseur = 1
   this.couleur = this.couleurTraitsDeConstruction
   this.regleDemiDroiteOriginePoint(A, O, { longueur: longueur(A, C) + 3 })
@@ -93,7 +93,7 @@ export function parallelogramme2sommetsConsecutifsCentre (A, B, O, nomC = '', no
   this.pointilles = false
   this.compasTracerArcCentrePoint(O, C)
   this.compasMasquer()
-  this.pointilles = 5
+  this.pointilles = true
   this.regleDemiDroiteOriginePoint(B, O, { longueur: longueur(B, D) + 3 })
   this.regleMasquer()
   this.crayonMasquer()
@@ -125,7 +125,7 @@ export function parallelogramme2sommetsConsecutifsCentre (A, B, O, nomC = '', no
      * @param {point} B
      * @param {point} O
      */
-export function parallelogrammeAngleCentre (D, A, B, O) {
+export function parallelogrammeAngleCentre (this: Alea2iep, D: PointAbstrait, A: PointAbstrait, B: PointAbstrait, O: PointAbstrait) {
   const B1 = pointSurSegment(A, B, longueur(A, B) + 2)
   const D1 = pointSurSegment(A, D, longueur(A, D) + 2)
   const C = translation2Points(B, A, D)
@@ -133,7 +133,7 @@ export function parallelogrammeAngleCentre (D, A, B, O) {
   this.traitRapide(A, D1)
   this.pointCreer(O, { tempo: 0 })
   this.pointCreer(A, { tempo: 0 })
-  this.regleDemiDroiteOriginePoint(A, O, { couleur: this.couleurTraitsDeConstruction, epaisseur: 1, pointilles: 5 })
+  this.regleDemiDroiteOriginePoint(A, O, { couleur: this.couleurTraitsDeConstruction, epaisseur: 1, pointilles: true })
   this.pointilles = false
   this.regleMasquer()
   this.crayonMasquer()
@@ -155,15 +155,15 @@ export function parallelogrammeAngleCentre (D, A, B, O) {
   this.crayonMasquer()
 }
 /**
-     * Macro pour placer le point M sur un segment [AB] tel que AM = n/d AB
-     * @param {point} A
-     * @param {point} B
-     * @param {int} n numérateur
-     * @param {int} d dénominateur
-     * @param {object} options { distance: 1, monAngle: 40, nom: '', nommerGraduations: false }
-     * @returns M
-     */
-export function partageSegment (A, B, n, d, { distance = 1, monAngle = 40, nom = '', nommerGraduations = false } = {}) {
+ * Macro pour placer le point M sur un segment [AB] tel que AM = n/d AB
+ * @param {point} A
+ * @param {point} B
+ * @param {int} n numérateur
+ * @param {int} d dénominateur
+ * @param {object} options { distance: 1, monAngle: 40, nom: '', nommerGraduations: false }
+ * @returns M
+ */
+export function partageSegment (this: Alea2iep, A: PointAbstrait, B: PointAbstrait, n: number, d: number, { distance = 1, monAngle = 40, nom = '', nommerGraduations = false } = {}) {
   this.traitRapide(A, B)
   this.regleMasquerGraduations()
   this.regleMontrer(A)
