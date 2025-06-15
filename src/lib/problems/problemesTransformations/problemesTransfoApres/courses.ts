17132cd0b8ec173855def58c3a2249ac610c37b9
EricElter <eric.elter@ac-normandie.fr>
2025-06-15 09:40:45 +0200

    feat : courses - correction ponctuation

--- src/lib/problems/problemesTransformations/problemesTransfoApres/courses.ts
+++ src/lib/problems/problemesTransformations/problemesTransfoApres/courses.ts
@@ -25,7 +25,7 @@ export function courses3 (decimal = true): Probleme {
     augmentation = randint(1, Math.ceil(prix1 * 0.05))
   }
   const data = { nb1: prix1, nb2: augmentation }
-  const enonce = `${personnage.prenom} a repéré hier ${objet.nom} à $${texNombre(prix1, 2, true)}$ € dans une boutique. Mais aujourd'hui le prix a augmenté de $${texNombre(augmentation, 2, true)}$ €.
+  const enonce = `${personnage.prenom} a repéré hier ${objet.nom} à $${texNombre(prix1, 2, true)}$ € dans une boutique. Mais aujourd'hui, le prix a augmenté de $${texNombre(augmentation, 2, true)}$ €.
 Combien ${personnage.prenom} doit-${personnage.pronom} dépenser aujourd'hui ?`
   const correction = `Aujourd'hui, le prix d'${objet.nom} est de $${texNombre(prix1, 2, true)}\\text{\\,€}+${texNombre(augmentation, 2, true)}\\text{\\,€} = ${miseEnEvidence(texNombre(prix1 + augmentation, 2, true))}$ €.`
   const probleme = new ProblemeTransfoApres('courses3', data)
