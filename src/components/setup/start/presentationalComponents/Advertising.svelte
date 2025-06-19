84d282fcf2dab8f373d3d2407cdbac0a48ea9ab4
Rémi Angot <remi.angot@gmail.com>
2025-06-19 15:45:24 +0200

    homepage : Ajout du cahier 6e

--- src/components/setup/start/presentationalComponents/Advertising.svelte
+++ src/components/setup/start/presentationalComponents/Advertising.svelte
@@ -8,24 +8,14 @@
   <h5 class="mb-2 text-2xl font-bold tracking-tight text-coopmaths-struct dark:text-white">
     Nouvelle version des cahiers de vacances !
   </h5>
-  <div class="hidden lg:flex space-x-2 sm:hiidden">
+  <div class="hidden lg:block sm:hiidden">
     <img
-      src="/alea/assets/images/cahier_vers_la_2nde.png"
-      alt="Cahier de vacances vers la première spécialité"
-      class="max-w-36 mb-4 flex"
-    />
-    <img
-      src="/alea/assets/images/cahier_vers_la_1ere_spe.png"
-      alt="Cahier de vacances vers la première spécialité"
-      class="max-w-36 mb-4 flex"
-    />
-    <img
-      src="/alea/assets/images/cahier_vers_la_terminale.png"
-      alt="Cahier de vacances vers la terminale spécialité"
-      class="max-w-36 mb-4 flex"
+      src="/alea/assets/images/cahiers2025.png"
+      alt="Cahier de vacances"
+      class="max-w-100 mb-4 flex"
     />
   </div>
   <p class="text-center font-normal text-gray-700 dark:text-gray-400">
-    Pour la seconde, la première spécialité, la terminale spécialité et bientôt pour la sixième !
+    Pour la sixième, la seconde et la spécialité mathématiques en première et terminale.
   </p>
 </a>
