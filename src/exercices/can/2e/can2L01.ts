e10c5edaab9337e20102e6916f003c479282678a
gillesMora <gilles.mora@ac-normandie.fr>
2025-07-07 10:46:31 +0200

    can En auto (2L01 et 2L19)

--- src/exercices/can/2e/can2L01.ts
+++ src/exercices/can/2e/can2L01.ts
@@ -1,7 +1,8 @@
 import { choice } from '../../../lib/outils/arrayOutils'
+import { miseEnEvidence } from '../../../lib/outils/embellissements'
 import { texRacineCarree } from '../../../lib/outils/texNombre'
 import { randint } from '../../../modules/outils'
-import Exercice from '../../Exercice'
+import ExerciceSimple from '../../ExerciceSimple'
 export const titre = 'Déterminer le nombre de solutions d’une équation se ramenant à $x^2=a$'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -19,10 +20,10 @@ export const refs = {
   'fr-fr': ['can2L01'],
   'fr-ch': []
 }
-export default class EquationPlusMoinsX2PlusAEgalB extends Exercice {
+export default class EquationPlusMoinsX2PlusAEgalB extends ExerciceSimple {
   constructor () {
     super()
-
+    this.versionQcmDisponible = true
     this.typeExercice = 'simple'
     this.nbQuestions = 1
   }
@@ -30,69 +31,81 @@ export default class EquationPlusMoinsX2PlusAEgalB extends Exercice {
   nouvelleVersion () {
     const a = randint(1, 20)
     const b = randint(1, 20)
-    switch (choice(['a', 'b', 'c', 'd'])) { //
+    switch (choice(['a'])) { //, 'b', 'c', 'd'
       case 'a':
         this.question = ` Combien de solutions réelles possède l'équation  $-x^2+${a}=${b}$ ?<br>`
         if (a - b > 0) {
           this.correction = `L'équation est équivalente à $-x^2=${b}-${a}$, soit $x^2=${a - b}$.<br>
-      $${a - b}$ étant strictement positif, cette équation a deux solutions : $${texRacineCarree(a - b)}$ et  $-${texRacineCarree(a - b)}$.`
-          this.reponse = 2
+      $${a - b}$ étant strictement positif, cette équation a $${miseEnEvidence(2)}$ solutions : $${texRacineCarree(a - b)}$ et  $-${texRacineCarree(a - b)}$.`
+          this.reponse = this.versionQcm ? '$2$ solutions' : 2
+          this.distracteurs = ['$0$ solution', '$1$ solution', 'Une infinité de solutions']
         } else if (a - b === 0) {
           this.correction = `L'équation est équivalente à$-x^2=${b}-${a}$, soit $x^2=${a - b}$.<br>
-      cette équation a une seul solution réelle : 0.`
-          this.reponse = 1
+      cette équation a $${miseEnEvidence(1)}$ seule solution réelle : 0.`
+          this.reponse = this.versionQcm ? '$1$ solution' : 1
+          this.distracteurs = ['$0$ solution', '$2$ solutions', 'Une infinité de solutions']
         } else if (a - b < 0) {
           this.correction = `L'équation est équivalente à $-x^2=${b}-${a}$, soit $x^2=${a - b}$.<br>
-     Cette équation n'a pas de solution réelle car $${a - b}<0$.`
-          this.reponse = 0
+     Cette équation a $${miseEnEvidence(0)}$ solution réelle car $${a - b}<0$.`
+          this.reponse = this.versionQcm ? '$0$ solution' : 0
+          this.distracteurs = ['$2$ solutions', '$1$ solution', 'Une infinité de solutions']
         }
         break
       case 'b':
         this.question = ` Combien de solutions réelles possède l'équation $-${a}+x^2=-${b}$ ?<br>`
         if (a - b > 0) {
           this.correction = `L'équation est équivalente à $x^2=-${b}+${a}$, soit $x^2=${a - b}$.<br>
-      $${a - b}$ étant strictement positif, cette équation a deux solutions : $${texRacineCarree(a - b)}$ et  $-${texRacineCarree(a - b)}$.`
-          this.reponse = 2
+      $${a - b}$ étant strictement positif, cette équation a $${miseEnEvidence(2)}$ solutions : $${texRacineCarree(a - b)}$ et  $-${texRacineCarree(a - b)}$.`
+          this.reponse = this.versionQcm ? '$2$ solutions' : 2
+          this.distracteurs = ['$0$ solution', '$1$ solution', 'Une infinité de solutions']
         } else if (a - b === 0) {
-          this.correction = `L'équation est équivalente à$x^2=-${b}+${a}$, soit $x^2=${a - b}$.<br>
-      cette équation a une seul solution réelle : 0.`
-          this.reponse = 1
+          this.correction = `L'équation est équivalente à $x^2=-${b}+${a}$, soit $x^2=${a - b}$.<br>
+      cette équation a $${miseEnEvidence(1)}$ seule solution réelle : 0.`
+          this.distracteurs = ['$0$ solution', '$2$ solutions', 'Une infinité de solutions']
+          this.reponse = this.versionQcm ? '$1$ solution' : 1
         } else if (a - b < 0) {
           this.correction = `L'équation est équivalente à $x^2=-${b}+${a}$, soit $x^2=${a - b}$.<br>
-     Cette équation n'a pas de solution réelle car $${a - b}<0$.`
-          this.reponse = 0
+     Cette équation a $${miseEnEvidence(0)}$ solution réelle car $${a - b}<0$.`
+          this.reponse = this.versionQcm ? '$0$ solution' : 0
+          this.distracteurs = ['$2$ solutions', '$1$ solution', 'Une infinité de solutions']
         }
         break
       case 'c':
         this.question = ` Combien de solutions réelles possède l'équation $${b}=-x^2+${a}$ ?<br>`
         if (a - b > 0) {
           this.correction = `L'équation est équivalente à $-x^2=${b}-${a}$, soit $x^2=${a - b}$.<br>
-        $${a - b}$ étant strictement positif, cette équation a deux solutions : $${texRacineCarree(a - b)}$ et  $-${texRacineCarree(a - b)}$.`
-          this.reponse = 2
+        $${a - b}$ étant strictement positif, cette équation a $${miseEnEvidence(2)}$ solutions : $${texRacineCarree(a - b)}$ et  $-${texRacineCarree(a - b)}$.`
+          this.reponse = this.versionQcm ? '$2$ solutions' : 2
+          this.distracteurs = ['$0$ solution', '$1$ solution', 'Une infinité de solutions']
         } else if (a - b === 0) {
           this.correction = `L'équation est équivalente à$-x^2=${b}-${a}$, soit $x^2=${a - b}$.<br>
-        cette équation a une seul solution réelle : 0.`
-          this.reponse = 1
+        cette équation a $${miseEnEvidence(1)}$ seule solution réelle : 0.`
+          this.reponse = this.versionQcm ? '$1$ solution' : 1
+          this.distracteurs = ['$0$ solution', '$2$ solutions', 'Une infinité de solutions']
         } else if (a - b < 0) {
           this.correction = `L'équation est équivalente à $-x^2=${b}-${a}$, soit $x^2=${a - b}$.<br>
-       Cette équation n'a pas de solution réelle car $${a - b}<0$.`
-          this.reponse = 0
+       Cette équation a $${miseEnEvidence(0)}$ solution réelle car $${a - b}<0$.`
+          this.reponse = this.versionQcm ? '$0$ solution' : 0
+          this.distracteurs = ['$2$ solutions', '$1$ solution', 'Une infinité de solutions']
         }
         break
       case 'd':
         this.question = ` Combien de solutions réelles possède l'équation  $-${b}=-x^2-${a}$ ?<br>`
         if (b - a > 0) {
           this.correction = `L'équation est équivalente à $-x^2=-${b}+${a}$, soit $x^2=${b - a}$.<br>
-        $${b - a}$ étant strictement positif, cette équation a deux solutions : $${texRacineCarree(b - a)}$ et  $-${texRacineCarree(b - a)}$.`
-          this.reponse = 2
+        $${b - a}$ étant strictement positif, cette équation a $${miseEnEvidence(2)}$ solutions : $${texRacineCarree(b - a)}$ et  $-${texRacineCarree(b - a)}$.`
+          this.reponse = this.versionQcm ? '$2$ solutions' : 2
+          this.distracteurs = ['$0$ solution', '$1$ solution', 'Une infinité de solutions']
         } else if (b - a === 0) {
           this.correction = `L'équation est équivalente à$-x^2=-${b}+${a}$, soit $x^2=${b - a}$.<br>
-        cette équation a une seul solution réelle : 0.`
-          this.reponse = 1
+        cette équation a $${miseEnEvidence(1)}$ seul solution réelle : 0.`
+          this.reponse = this.versionQcm ? '$1$ solution' : 1
+          this.distracteurs = ['$0$ solution', '$2$ solutions', 'Une infinité de solutions']
         } else if (b - a < 0) {
           this.correction = `L'équation est équivalente à $-x^2=-${b}+${a}$, soit $x^2=${b - a}$.<br>
-       Cette équation n'a pas de solution réelle car $${b - a}<0$.`
-          this.reponse = 0
+       Cette équation a $${miseEnEvidence(0)}$ solution réelle car $${b - a}<0$.`
+          this.reponse = this.versionQcm ? '$0$ solution' : 0
+          this.distracteurs = ['$2$ solutions', '$1$ solution', 'Une infinité de solutions']
         }
         break
     }
--- src/exercices/can/2e/can2L19.ts
+++ src/exercices/can/2e/can2L19.ts
@@ -1,9 +1,8 @@
 import { choice } from '../../../lib/outils/arrayOutils'
-import Exercice from '../../Exercice'
-import { listeQuestionsToContenu, randint } from '../../../modules/outils'
-import { propositionsQcm } from '../../../lib/interactif/qcm'
+import ExerciceSimple from '../../ExerciceSimple'
 import { texteCentre } from '../../../lib/format/miseEnPage'
-export const titre = 'Exprimer une variable en fonction des autres (QCM)'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
+export const titre = 'Exprimer une variable en fonction des autres'
 export const interactifReady = true
 export const interactifType = 'qcm'
 
@@ -22,159 +21,71 @@ export const refs = {
   'fr-fr': ['can2L19'],
   'fr-ch': []
 }
-export default class ExprimerEnFonction extends Exercice {
+export default class ExprimerEnFonction extends ExerciceSimple {
   constructor () {
     super()
+    this.typeExercice = 'simple'
     this.nbQuestions = 1
+    this.versionQcmDisponible = true
+    this.formatChampTexte = KeyboardType.alphanumeric
+    this.optionsChampTexte = { texteAvant: '<br> ', texteApres: ' ' }
   }
 
   nouvelleVersion () {
-    let texte, texteCorr, choixQ, monQcm, a
-    for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50;) {
-      choixQ = choice([true, false])
-      a = randint(1, 10)
-      switch (choice([1, 2])) { // 1
-        case 1 :
-          texte = `Lorsqu’un point mobile suit une trajectoire circulaire de rayon $R$, 
+    const choixQ = choice([true, false])
+
+    switch (choice([1, 2])) {
+      case 1 :
+        this.question = `Lorsqu’un point mobile suit une trajectoire circulaire de rayon $R$, 
   en mètre (m), son accélération centripète $a$ (en m/s²) 
   et sa vitesse $v$ (en m/s) sont liés par la relation : <br>
   ${choixQ ? `${texteCentre('$a=\\dfrac{v^2}{R}$')}` : `${texteCentre('$v=\\sqrt{aR}$')}`}`
 
-          texte += choixQ
-            ? 'L\'expression permettant, à partir de cette formule, d\'exprimer la vitesse $v$ est :'
-            : 'L\'expression permettant, à partir de cette formule, d\'exprimer l\'accélération $a$ est :'
-          this.canEnonce = texte
-          this.autoCorrection[i] = {
-            enonce: texte,
-            propositions: choixQ
-              ? [
-                  {
-                    texte: '$v=\\sqrt{aR}$',
-                    statut: true
-                  },
-                  {
-                    texte: '$v=aR^2$',
-                    statut: false
-                  },
-                  {
-                    texte: '$v=\\sqrt{\\dfrac{a}{R}}$',
-                    statut: false
-                  },
-                  {
-                    texte: '$v=\\dfrac{a^2}{R}$',
-                    statut: false
-                  }
-                ]
-              : [
-                  {
-                    texte: '$a=\\dfrac{v^2}{R}$',
-                    statut: true
-                  },
-                  {
-                    texte: '$a=\\dfrac{v}{\\sqrt{R}}$',
-                    statut: false
-                  },
-                  {
-                    texte: '$a=v^2R$',
-                    statut: false
-                  },
-                  {
-                    texte: '$a=\\dfrac{R}{v^2}$',
-                    statut: false
-                  }
-                ]
-          }
-          monQcm = propositionsQcm(this, i)
-          texte += monQcm.texte
-          this.canEnonce = texte
+        this.question += choixQ
+          ? 'L\'expression permettant, à partir de cette formule, d\'exprimer la vitesse $v$ est :'
+          : 'L\'expression permettant, à partir de cette formule, d\'exprimer l\'accélération $a$ est :'
+        this.reponse = choixQ
+          ? '$v=\\sqrt{aR}$'
+          : '$a=\\dfrac{v^2}{R}$'
+        this.distracteurs = choixQ
+          ? ['$v=aR^2$', '$v=\\sqrt{\\dfrac{a}{R}}$', '$v=\\dfrac{a^2}{R}$']
+          : ['$a=v^2R$', '$a=\\dfrac{v}{\\sqrt{R}}$', '$a=\\dfrac{R}{v^2}$']
 
-          texteCorr = choixQ
-            ? 'Puisque $a=\\dfrac{v^2}{R}$, alors $v^2 = a \\times R$, soit $v = \\sqrt{aR}$.'
-            : `On part de la formule $v = \\sqrt{aR}$.<br>
+        this.correction = choixQ
+          ? 'Puisque $a=\\dfrac{v^2}{R}$, alors $v^2 = a \\times R$, soit $v = \\sqrt{aR}$.'
+          : `On part de la formule $v = \\sqrt{aR}$.<br>
 On élève les deux membres au carré : $v^2 = aR$<br>
 Puis on isole $a$ : $a = \\dfrac{v^2}{R}$.`
-          break
-        case 2 :
-        default:
-          texte = 'On considère $x$, $y$ et $v$ des nombres réels positifs non nuls liés par la relation : <br>'
-
-          texte += choixQ
-            ? `${texteCentre('$v=\\sqrt{\\dfrac{x}{y}}$')}`
-            : `${texteCentre('$y=(1+vx)^2$')}`
-          texte += choixQ
-            ? 'L\'expression permettant, à partir de cette formule, d\'exprimer  $y$ est :'
-            : 'L\'expression permettant, à partir de cette formule, d\'exprimer $v$ est :'
-
-          this.canEnonce = texte
-
-          this.autoCorrection[i] = {
-            enonce: texte,
-            propositions: choixQ
-              ? [
-                  {
-                    texte: '$y=\\dfrac{x}{v^2}$',
-                    statut: true
-                  },
-                  {
-                    texte: '$y=\\sqrt{xv}$',
-                    statut: false
-                  },
-                  {
-                    texte: '$y=x \\times v^2$',
-                    statut: false
-                  },
-                  {
-                    texte: '$y=\\dfrac{v^2}{x}$',
-                    statut: false
-                  }
-                ]
-              : [
-                  {
-                    texte: '$v=\\dfrac{\\sqrt{y}-1}{x}$',
-                    statut: true
-                  },
-                  {
-                    texte: '$v=\\dfrac{y-1}{x}$',
-                    statut: false
-                  },
-                  {
-                    texte: '$v=\\sqrt{y} \\times x - 1$',
-                    statut: false
-                  },
-                  {
-                    texte: '$v=\\dfrac{1}{x \\times \\sqrt{y}}$',
-                    statut: false
-                  }
-                ]
-          }
-
-          monQcm = propositionsQcm(this, i)
-          texte += monQcm.texte
-          this.canEnonce = texte
-
-          if (choixQ) {
-            texteCorr = `On part de la relation : $v = \\sqrt{\\dfrac{x}{y}}$<br>
+        break
+      case 2 :
+      default:
+        this.question = 'On considère $x$, $y$ et $v$ des nombres réels positifs non nuls liés par la relation : <br>'
+        this.question += choixQ
+          ? `${texteCentre('$v=\\sqrt{\\dfrac{x}{y}}$')}`
+          : `${texteCentre('$y=(1+vx)^2$')}`
+        this.question += choixQ
+          ? 'L\'expression permettant, à partir de cette formule, d\'exprimer  $y$ est :'
+          : 'L\'expression permettant, à partir de cette formule, d\'exprimer $v$ est :'
+        this.reponse = choixQ
+          ? '$y=\\dfrac{x}{v^2}$'
+          : '$v=\\dfrac{\\sqrt{y}-1}{x}$'
+        this.distracteurs = choixQ
+          ? ['$y=\\sqrt{xv}$', '$y=x \\times v^2$', '$y=\\dfrac{v^2}{x}$']
+          : ['$v=\\dfrac{y-1}{x}$', '$v=\\sqrt{y} \\times x - 1$', '$v=\\dfrac{1}{x \\times \\sqrt{y}}$']
+        if (choixQ) {
+          this.correction = `On part de la relation : $v = \\sqrt{\\dfrac{x}{y}}$<br>
 On élève les deux membres au carré : $v^2 = \\dfrac{x}{y}$<br>
 Puis on isole $y$ : $y = \\dfrac{x}{v^2}$.`
-          } else {
-            texteCorr = `On part de la formule : $y = (1 + vx)^2$<br>
+        } else {
+          this.correction = `On part de la formule : $y = (1 + vx)^2$<br>
 On prend la racine carrée des deux membres : $\\sqrt{y} = 1 + vx$<br>
 On isole $v$ : $vx = \\sqrt{y} - 1$<br>
 Donc : $v = \\dfrac{\\sqrt{y} - 1}{x}$.`
-          }
+        }
 
-          break
-      }
-      if (this.questionJamaisPosee(i, a)) {
-        this.listeQuestions[i] = texte
-        this.listeCorrections[i] = texteCorr
-        i++
-      }
-      this.canReponseACompleter = monQcm.texte
-      this.listeCanEnonces.push(this.canEnonce)
-      this.listeCanReponsesACompleter.push(this.canReponseACompleter)
-      cpt++
+        break
     }
-    listeQuestionsToContenu(this)
+    this.canEnonce = this.question
+    this.canReponseACompleter = ''
   }
 }
