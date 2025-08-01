c98a40565c078d49106650f7cbcf6ec2ccf474c1
gillesMora <gilles.mora@ac-normandie.fr>
2025-08-01 16:16:57 +0200

    CAN1F24

--- src/exercices/can/1e/can1F24.ts
+++ src/exercices/can/1e/can1F24.ts
@@ -3,19 +3,19 @@ import { Repere, repere } from '../../../lib/2d/reperes'
 import { Latex2d, TexteParPoint, texteParPosition } from '../../../lib/2d/textes'
 import { choice } from '../../../lib/outils/arrayOutils'
 import { ecritureAlgebrique, rienSi1 } from '../../../lib/outils/ecritures'
-import Exercice from '../../Exercice'
+import ExerciceSimple from '../../ExerciceSimple'
 import { mathalea2d } from '../../../modules/2dGeneralites'
-import { listeQuestionsToContenu, randint } from '../../../modules/outils'
-import { ajouteChampTexteMathLive } from '../../../lib/interactif/questionMathLive'
-import { setReponse } from '../../../lib/interactif/gestionInteractif'
+import { randint } from '../../../modules/outils'
 import { miseEnEvidence } from '../../../lib/outils/embellissements'
+import { texNombre } from '../../../lib/outils/texNombre'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
 export const titre = 'Lire graphiquement la valeur de $a$ dans $ax^2+b$'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 
 // Les exports suivants sont optionnels mais au moins la date de publication semble essentielle
 export const dateDePublication = '17/06/2022' // La date de publication initiale au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
-
+export const dateDeModifImportante = '01/08/2025'
 /**
  *
  * @author Gilles Mora
@@ -26,210 +26,210 @@ export const refs = {
   'fr-fr': ['can1F24'],
   'fr-ch': []
 }
-export default class LectureGraphiqueParaboleA extends Exercice {
+export default class LectureGraphiqueParaboleA extends ExerciceSimple {
   constructor () {
     super()
-
+    this.versionQcmDisponible = true
     this.nbQuestions = 1
+    this.optionsChampTexte = { texteAvant: '$a=$' }
+    this.formatChampTexte = KeyboardType.clavierDeBaseAvecFraction
   }
 
   nouvelleVersion () {
-    let texte = ''
-    let texteCorr = ''
     let a: number
     let b: number
     let o: TexteParPoint | Latex2d
     let f: (x:number)=>number
     let r: Repere
-    for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50;) {
-      switch (choice([1, 2])) {
-        case 1:// cas parabole a>0
 
-          a = randint(1, 4)
-          b = randint(-3, 3, 0)
-          o = texteParPosition('O', -0.3, -0.3, 0, 'black', 1)
+    switch (choice([1, 2])) {
+      case 1:// cas parabole a>0
 
-          f = function (x) {
-            return a * x ** 2 + b
-          }
-          if (b > 0) {
-            r = repere({
-              yUnite: 1,
-              xUnite: 2,
-              xMin: -3,
-              yMin: -1,
-              yMax: 8,
-              xMax: 3,
-              thickHauteur: 0.1,
-              xLabelMin: -2,
-              xLabelMax: 2,
-              yLabelMax: 7,
-              yLabelMin: 0,
-              // yLabelMin: -9,
-              // yLabelListe:[-8,-6,-4,-2,2,4,6,8],
-              axeXStyle: '->',
-              axeYStyle: '->'
-            })
+        a = randint(1, 8) / 2
+        b = randint(-6, 6, 0) / 2
+        o = texteParPosition('O', -0.3, -0.3, 0, 'black', 1)
 
-            f = x => a * x ** 2 + b
+        f = function (x) {
+          return a * x ** 2 + b
+        }
+        if (b > 0) {
+          r = repere({
+            yUnite: 2,
+            xUnite: 2,
+            xMin: -3,
+            yMin: -1,
+            yMax: 6,
+            xMax: 3,
+            thickHauteur: 0.1,
+            xLabelMin: -2,
+            xLabelMax: 2,
+            yLabelMax: 5,
+            yLabelMin: -2,
+            grilleSecondaire: true,
+            grilleSecondaireYDistance: 1,
+            grilleSecondaireXDistance: 1,
+            grilleSecondaireYMin: -2.1,
+            grilleSecondaireYMax: 12.1,
+            grilleSecondaireXMin: -3.1,
+            grilleSecondaireXMax: 3.1,
+            yLabelEcart: 0.7,
+            axeXStyle: '->',
+            axeYStyle: '->'
+          })
+          f = x => a * x ** 2 + b
 
-            texte = `Soit $f$ une fonction définie sur $\\mathbb{R}$ par $f(x)=ax^2+${b}$ .<br>
-                        `
-            texte += `A l'aide de la représentation graphique, déterminer la valeur de $a$.<br>
-            
-            ` + mathalea2d({
-                xmin: -6,
-                xmax: 6,
-                ymin: -1.5,
-                ymax: 8,
-                pixelsParCm: 25,
-                scale: 0.6,
-                style: 'margin: auto'
-              }, r, o, courbe(f, { repere: r, color: 'blue', epaisseur: 2 }))
-          } else {
-            r = repere({
-              yUnite: 1,
-              xUnite: 2,
-              xMin: -3,
-              yMin: -4,
-              yMax: 4,
-              xMax: 3,
-              thickHauteur: 0.1,
-              xLabelMin: -2,
-              xLabelMax: 2,
-              yLabelMax: 3,
-              yLabelMin: -3,
-              // yLabelMin: -9,
-              // yLabelListe:[-8,-6,-4,-2,2,4,6,8],
-              axeXStyle: '->',
-              axeYStyle: '->'
-            })
+          this.question = `Voici la représentation graphique d'une fonction $f$  définie sur $\\mathbb{R}$ par ${this.versionQcm ? '$f(x)=ax^2+b$' : `$f(x)=ax^2${ecritureAlgebrique(b)}$`}.<br>`
+          this.question +=
+             mathalea2d({
+               xmin: -6.1,
+               xmax: 6.1,
+               ymin: -2.1,
+               ymax: 12.1,
+               pixelsParCm: 20,
+               scale: 0.45,
+               style: 'margin: auto'
+             }, r, o, courbe(f, { repere: r, color: 'blue', epaisseur: 2 })) + '<br>'
+          this.question += `${this.versionQcm ? 'À partir de cette représentation graphique, on a : ' : 'À partir de cette représentation graphique, déterminer la valeur de $a$.<br>'}`
+        } else {
+          r = repere({
+            yUnite: 2,
+            xUnite: 2,
+            xMin: -3,
+            yMin: -3,
+            yMax: 3,
+            xMax: 3,
+            thickHauteur: 0.1,
+            xLabelMin: -2,
+            xLabelMax: 2,
+            yLabelMax: 2,
+            yLabelMin: -2,
+            grilleSecondaire: true,
+            grilleSecondaireYDistance: 1,
+            grilleSecondaireXDistance: 1,
+            grilleSecondaireYMin: -3.1,
+            grilleSecondaireYMax: 3.1,
+            grilleSecondaireXMin: -3.1,
+            grilleSecondaireXMax: 3.1,
+            yLabelEcart: 0.7,
+            axeXStyle: '->',
+            axeYStyle: '->'
+          })
 
-            f = x => a * x ** 2 + b
+          f = x => a * x ** 2 + b
 
-            texte = `Soit $f$ une fonction définie sur $\\mathbb{R}$ par $f(x)=ax^2${ecritureAlgebrique(b)}$ , où $a$ est un réel non-nul.<br>
-            `
-            texte += `A l'aide de la représentation graphique, déterminer la valeur de $a$.<br>
-            
-            ` + mathalea2d({
-                xmin: -6,
-                xmax: 6,
-                ymin: -4.5,
-                ymax: 4,
-                pixelsParCm: 25,
-                scale: 0.6,
-                style: 'margin: auto'
-              }, r, o, courbe(f, { repere: r, color: 'blue', epaisseur: 2 }))
-          }
-          if (this.interactif) {
-            texte += ajouteChampTexteMathLive(this, i, ' ', { texteAvant: '$a=$' })
-            setReponse(this, i, a)
-          }
+          this.question = `Voici la représentation graphique d'une fonction $f$  définie sur $\\mathbb{R}$ par ${this.versionQcm ? '$f(x)=ax^2+b$' : `$f(x)=ax^2${ecritureAlgebrique(b)}$`}.<br> `
+          this.question +=
+            mathalea2d({
+              xmin: -6.1,
+              xmax: 6.1,
+              ymin: -6.1,
+              ymax: 6.1,
+              pixelsParCm: 20,
+              scale: 0.45,
+              style: 'margin: auto'
+            }, r, o, courbe(f, { repere: r, color: 'blue', epaisseur: 2 })) +
+       '<br>'
+          this.question += `${this.versionQcm ? 'À partir de cette représentation graphique, on a : ' : 'À partir de cette représentation graphique, déterminer la valeur de $a$.<br>'}`
+        }
 
-          texteCorr = `La valeur de $a$ s'obtient (par exemple) grâce à l'image de $1$ par la fonction $f$.<br>
-          On lit $f(1)=${f(1)}$. D'où, $a\\times 1^2${ecritureAlgebrique(b)}=${f(1)}$, soit $a=${miseEnEvidence(a)}$.<br>
-          Ainsi, $f(x)=${rienSi1(a)}x^2${ecritureAlgebrique(b)}$.`
-          break
+        break
 
-        case 2:// cas parabole a<0
-        default:
+      case 2:// cas parabole a<0
+      default:
 
-          a = randint(-4, -1)
-          b = randint(-3, 3, 0)
-          o = texteParPosition('O', -0.3, -0.3, 0, 'black', 1)
+        a = randint(-4, -1) / 2
+        b = randint(-6, 6, 0) / 2
+        o = texteParPosition('O', -0.3, -0.3, 0, 'black', 1)
 
-          f = function (x) {
-            return a * x ** 2 + b
-          }
-          if (b > 0) {
-            r = repere({
-              yUnite: 1,
-              xUnite: 2,
-              xMin: -3,
-              yMin: -4,
-              yMax: 4,
-              xMax: 3,
-              thickHauteur: 0.1,
-              xLabelMin: -2,
-              xLabelMax: 2,
-              yLabelMax: 3,
-              yLabelMin: -3,
-              // yLabelMin: -9,
-              // yLabelListe:[-8,-6,-4,-2,2,4,6,8],
-              axeXStyle: '->',
-              axeYStyle: '->'
-            })
+        f = function (x) {
+          return a * x ** 2 + b
+        }
+        if (b > 0) {
+          r = repere({
+            yUnite: 2,
+            xUnite: 2,
+            xMin: -3,
+            yMin: -2,
+            yMax: 4,
+            xMax: 3,
+            thickHauteur: 0.1,
+            xLabelMin: -2,
+            xLabelMax: 2,
+            yLabelMax: 3,
+            yLabelMin: -1,
+            grilleSecondaire: true,
+            grilleSecondaireYDistance: 1,
+            grilleSecondaireXDistance: 1,
+            grilleSecondaireYMin: -4.1,
+            grilleSecondaireYMax: 4.1,
+            grilleSecondaireXMin: -3.1,
+            grilleSecondaireXMax: 3.1,
+            yLabelEcart: 0.7,
+            axeXStyle: '->',
+            axeYStyle: '->'
+          })
 
-            f = x => a * x ** 2 + b
+          f = x => a * x ** 2 + b
 
-            texte = `Soit $f$ une fonction définie sur $\\mathbb{R}$ par $f(x)=ax^2${ecritureAlgebrique(b)}$ .<br>
-            `
-            texte += `A l'aide de la représentation graphique, déterminer la valeur de $a$.<br>
-          ` +
+          this.question = `Voici la représentation graphique d'une fonction $f$  définie sur $\\mathbb{R}$ par ${this.versionQcm ? '$f(x)=ax^2+b$' : `$f(x)=ax^2${ecritureAlgebrique(b)}$`}.<br> ` +
            mathalea2d({
              xmin: -6,
              xmax: 6,
-             ymin: -4.2,
-             ymax: 4,
-             pixelsParCm: 25,
+             ymin: -4.1,
+             ymax: 8.1,
+             pixelsParCm: 20,
              scale: 0.6,
              style: 'margin: auto'
-           }, r, o, courbe(f, { repere: r, color: 'blue', epaisseur: 2 }))
-          } else {
-            r = repere({
-              yUnite: 1,
-              xUnite: 2,
-              xMin: -3,
-              yMin: -7,
-              yMax: 1,
-              xMax: 3,
-              thickHauteur: 0.1,
-              xLabelMin: -2,
-              xLabelMax: 2,
-              yLabelMax: 0,
-              yLabelMin: -6,
-              // yLabelMin: -9,
-              // yLabelListe:[-8,-6,-4,-2,2,4,6,8],
-              axeXStyle: '->',
-              axeYStyle: '->'
-            })
+           }, r, o, courbe(f, { repere: r, color: 'blue', epaisseur: 2 })) + '<br>'
+          this.question += `${this.versionQcm ? 'À partir de cette représentation graphique, on a : ' : 'À partir de cette représentation graphique, déterminer la valeur de $a$.<br>'}`
+        } else {
+          r = repere({
+            yUnite: 2,
+            xUnite: 2,
+            xMin: -3,
+            yMin: -5,
+            yMax: 1,
+            xMax: 3,
+            thickHauteur: 0.1,
+            xLabelMin: -2,
+            xLabelMax: 2,
+            yLabelMax: 0,
+            yLabelMin: -4,
+            grilleSecondaire: true,
+            grilleSecondaireYDistance: 1,
+            grilleSecondaireXDistance: 1,
+            grilleSecondaireYMin: -5.1,
+            grilleSecondaireYMax: 1.1,
+            grilleSecondaireXMin: -3.1,
+            grilleSecondaireXMax: 3.1,
+            yLabelEcart: 0.7,
+            axeXStyle: '->',
+            axeYStyle: '->'
+          })
 
-            f = x => a * x ** 2 + b
+          f = x => a * x ** 2 + b
 
-            texte = `Soit $f$ une fonction définie sur $\\mathbb{R}$ par $f(x)=ax^2${ecritureAlgebrique(b)}$ .<br>
-            `
-            texte += `A l'aide de la représentation graphique, déterminer la valeur de $a$.<br>
-            
-            ` + mathalea2d({
-                xmin: -6,
-                xmax: 6,
-                ymin: -7.2,
-                ymax: 1,
-                pixelsParCm: 25,
-                scale: 0.6,
-                style: 'margin: auto'
-              }, r, o, courbe(f, { repere: r, color: 'blue', epaisseur: 2 }))
-          }
-          if (this.interactif) {
-            texte += ajouteChampTexteMathLive(this, i, ' ', { texteAvant: '$a=$' })
-            setReponse(this, i, a)
-          }
+          this.question = `Voici la représentation graphique d'une fonction $f$  définie sur $\\mathbb{R}$ par ${this.versionQcm ? '$f(x)=ax^2+b$' : `$f(x)=ax^2${ecritureAlgebrique(b)}$`}.<br> ` +
+          mathalea2d({
+            xmin: -6,
+            xmax: 6,
+            ymin: -10.1,
+            ymax: 2.1,
+            pixelsParCm: 20,
+            scale: 0.45,
+            style: 'margin: auto'
+          }, r, o, courbe(f, { repere: r, color: 'blue', epaisseur: 2 })) + '<br>'
+          this.question += `${this.versionQcm ? 'À partir de cette représentation graphique, on a : ' : 'À partir de cette représentation graphique, déterminer la valeur de $a$.<br>'}`
+        }
 
-          texteCorr = `La valeur de $a$ s'obtient grâce à l'image de $1$ par la fonction $f$.<br>
-          On lit $f(1)=${f(1)}$. D'où, $a\\times 1^2${ecritureAlgebrique(b)}=${f(1)}$, soit $a=${miseEnEvidence(a)}$.<br>
-          Ainsi, $f(x)=${rienSi1(a)}x^2${ecritureAlgebrique(b)}$.`
-          break
-      }
-
-      if (this.questionJamaisPosee(i, a, b)) {
-        this.listeQuestions[i] = texte
-        this.listeCorrections[i] = texteCorr
-        i++
-      }
-      cpt++
+        break
     }
-    listeQuestionsToContenu(this)
-    this.listeCanEnonces.push(texte)
+    this.correction = `${this.versionQcm ? `La valeur de $b$ est donnée par l'image de $0$ par $f$ (ordonnée du point d'intersection entre la courbe et l'axe des ordonnées).<br> Ainsi, $b=${miseEnEvidence(texNombre(b, 1))}$.<br>` : ''}La valeur de $a$ s'obtient (par exemple) grâce à l'image de $1$ par la fonction $f$.<br>
+          On lit $f(1)=${texNombre(f(1), 1)}$. D'où, $a\\times 1^2${ecritureAlgebrique(b)}=${texNombre(f(1), 1)}$, soit $a=${miseEnEvidence(texNombre(a, 1))}$.<br>
+          Ainsi, $f(x)=${rienSi1(a)}x^2${ecritureAlgebrique(b)}$.`
+    this.reponse = this.versionQcm ? `$a=${texNombre(a, 1)}$ et $b=${texNombre(b, 1)}$` : a
+    this.distracteurs = [`$a=${texNombre(b, 1)}$ et $b=${texNombre(b, 1)}$`, `$a=${texNombre(a + 0.5, 1)}$ et $b=${texNombre(b, 1)}$`, `$a=${texNombre(-a, 1)}$ et $b=${texNombre(b, 1)}$`, `$a=${texNombre(a, 1)}$ et $b=${texNombre(-b, 1)}$`]
+    this.listeCanEnonces.push(this.question)
     this.listeCanReponsesACompleter.push('$a=\\ldots$')
   }
 }
