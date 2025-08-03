95d6fee9432f9891ef23afeb6d6aa03410ca1650
Eric Elter <eric.elter@ac-normandie.fr>
2025-08-03 09:34:19 +0000

    New can6D05 (Nouveau programme de 6ème)

---
+++ src/exercices/can/6e/can6D05.ts
@@ -0,0 +1,41 @@
+import { miseEnEvidence } from '../../../lib/outils/embellissements'
+import { randint } from '../../../modules/outils'
+import ExerciceSimple from '../../ExerciceSimple'
+export const titre = 'Connaître les unités de mesure de durées courtes et les relations qui les lient'
+export const dateDePublication = '01/08/2025'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const amcReady = true
+export const amcType = 'AMCNum'
+
+/**
+ * @author Eric Elter
+
+ */
+export const uuid = '62450'
+
+export const refs = {
+  'fr-fr': ['can6D05'],
+  'fr-ch': []
+}
+export default class AutoDureeMinutes extends ExerciceSimple {
+  constructor () {
+    super()
+    this.nbQuestions = 1
+
+    this.typeExercice = 'simple'
+  }
+
+  nouvelleVersion () {
+    const durees = [['jour', 'heures', 24], ['heure', 'minutes', 60], ['minute', 'secondes', 60]]
+    const choix = randint(0, 2)
+
+    this.reponse = durees[choix][2]
+    this.question = `$1$ ${durees[choix][0]} = `
+    this.canReponseACompleter = this.question + '$\\ldots\\ldots$ ' + durees[choix][1]
+    this.correction = this.question + `$${miseEnEvidence(this.reponse)}$ ${durees[choix][1]}`
+    if (!this.interactif) this.question = this.canReponseACompleter
+    this.canEnonce = 'Compléter.'
+    this.optionsChampTexte = { texteApres: durees[choix][1] }
+  }
+}
