e500ffdfc0603f03b2d48ddc49c841a3cf12338c
EricElter <eric.elter@ac-normandie.fr>
2025-08-09 10:05:34 +0200

    New can6S02 : Nouveau programme de 6ème

---
+++ src/exercices/can/6e/can6S02.ts
@@ -0,0 +1,46 @@
+import { randint } from '../../../modules/outils'
+import ExerciceSimple from '../../ExerciceSimple'
+import FractionEtendue from '../../../modules/FractionEtendue'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
+import { miseEnEvidence } from '../../../lib/outils/embellissements'
+import { context } from '../../../modules/context'
+export const titre = 'Enrichir son vocabulaire sur les probabilités'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const amcReady = true
+export const amcType = 'AMCNum'
+
+/**
+ * @author Eric Elter
+
+ */
+export const uuid = '2a91a'
+
+export const refs = {
+  'fr-fr': ['can6S02'],
+  'fr-ch': []
+}
+export default class ChancesSurEtProbabilites extends ExerciceSimple {
+  constructor () {
+    super()
+    this.typeExercice = 'simple'
+    this.formatChampTexte = KeyboardType.clavierDeBaseAvecFraction
+    this.optionsChampTexte = { texteApres: '.' }
+    this.nbQuestions = 1
+  }
+
+  nouvelleVersion () {
+    const nbChances = randint(1, 3)
+    const nbTotal = nbChances + randint(1, 3)
+    this.reponse = new FractionEtendue(nbChances, nbTotal).texFraction
+    this.question = `S'il y a $${nbChances}$ chance${nbChances === 1 ? '' : 's'} sur $${nbTotal}$ qu'un événement se produise, alors la probabilité
+    que cet événement se réalise est de : `
+    if (!this.interactif && context.isHtml) {
+      this.question += '$\\ldots$'
+    }
+    this.correction = `S'il y a $${nbChances}$ chance${nbChances === 1 ? '' : 's'} sur $${nbTotal}$ qu'un événement se produise, alors la probabilité
+    que cet événement se réalise est de $${miseEnEvidence(this.reponse)}$.`
+    this.canEnonce = this.question
+    this.canReponseACompleter = '$\\ldots$'
+  }
+}
