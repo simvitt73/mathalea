134ac9361e6eb25dc571611d58efeb15645da259
Stephane Guyon <stephane.guyon@ac-montpellier.fr>
2025-08-12 08:27:40 +0000

    Nouveaux exos automatismes rangés dans niveau 1ère : 1AC3-6 à 1AC3-10

---
+++ src/exercices/1e/1A-C3-9.ts
@@ -0,0 +1,54 @@
+import { randint } from '../../modules/outils'
+import ExerciceQcmA from '../ExerciceQcmA'
+export const dateDePublication = '10/08/2025'
+export const uuid = '7fe71'
+// Author Stéphane Guyon
+export const refs = {
+  'fr-fr': ['1A-C3-9'],
+  'fr-ch': []
+}
+export const interactifReady = true
+export const interactifType = 'qcm'
+export const amcReady = 'true'
+export const amcType = 'qcmMono'
+export const titre = 'Calculer avec des puissances (9)'
+export default class Puissances extends ExerciceQcmA {
+  versionOriginale: () => void = () => {
+    this.enonce = 'Soit $n$ un entier non-nul. A quelle expression est égale $\\left(-1\\right)^{n+2}$ ?'
+    this.correction = '$\\begin{aligned} \\left(-1\\right)^{n+2}&=\\left(-1\\right)^{2}\\times \\left(-1\\right)^{n} \\\\    &=1\\times \\left(-1\\right)^{n} \\\\    &= \\left(-1\\right)^{n}    \\end{aligned}$<br>'
+    this.reponses = [
+      '$\\left(-1\\right)^{n} $',
+      '$\\left(-1\\right)^{n+1}$ ',
+      '$-\\left(-1\\right)^{n} $',
+      '$\\left(-1\\right)^{n-1}$ ',
+    ]
+  }
+
+  versionAleatoire = () => {
+    const k = randint(3, 6)
+
+    this.enonce = `Soit $n$ un entier non-nul. A quelle expression est égale $\\left(-1\\right)^{n+${k}}$ ?`
+    if (k === 2 || k === 4 || k === 6) {
+      this.correction = `$\\begin{aligned} \\left(-1\\right)^{n+${k}}&=\\left(-1\\right)^{${k}} \\times \\left(-1\\right)^{n} \\\\  &=1\\times \\left(-1\\right)^{n} \\\\    &= \\left(-1\\right)^{n}      \\end{aligned}$<br>`
+      this.reponses = [
+        '$\\left(-1\\right)^{n} $',
+        '$\\left(-1\\right)^{n+1}$ ',
+        '$-\\left(-1\\right)^{n} $',
+        '$\\left(-1\\right)^{n-1}$ '
+      ]
+    } else {
+      this.correction = `$\\begin{aligned} \\left(-1\\right)^{n+${k}}&=\\left(-1\\right)^{${k-1}}\\times \\left(-1\\right)^{n+1} \\\\    &=1\\times \\left(-1\\right)^{n+1} \\\\    &= \\left(-1\\right)^{n+1}   \\end{aligned}$<br>`
+      this.reponses = [
+        '$\\left(-1\\right)^{n+1} $',
+        '$\\left(-1\\right)^{n+2} $',
+        '$-\\left(-1\\right)^{n+1} $',
+        '$\\left(-1\\right)^{n}$ '
+      ]
+    }
+  }
+
+  constructor () {
+    super()
+    this.versionAleatoire()
+  }
+}
