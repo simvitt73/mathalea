d5ec6dd5ac71020796ed4952a5717e298a8b45bc
mathsguyon <mathsguyon@gmail.com>
2025-09-07 22:13:11 +0200

    Update canTEC1-04.ts

--- src/exercices/can/Ex/canTEC1-04.ts
+++ src/exercices/can/Ex/canTEC1-04.ts
@@ -13,19 +13,19 @@ export const interactifReady = true
 export const interactifType = 'qcm'
 export const amcReady = 'true'
 export const amcType = 'qcmMono'
-export const titre = 'Calculer les puissances de $\\mathrm{i}$'
+export const titre = 'Calculer les puissances de $i$'
 export default class Complexes1 extends ExerciceQcmA {
   versionOriginale: () => void = () => {
     this.enonce =
-      '$\\mathrm{i}^{4}$ est égal à '
+      '$i^{4}$ est égal à '
     this.correction =
-      `On sait que $\\mathrm{i}^{2}=-1$ donc $\\mathrm{i}^{4}=\\left(-1\\right)^{2}=${miseEnEvidence('1')}.$`
+      `On sait que $i^{2}=-1$ donc $i^{4}=\\left(-1\\right)^{2}=${miseEnEvidence('1')}.$`
 
     this.reponses = [
       '$1 $',
       '$-1$ ',
-      '$\\mathrm{i} $',
-      '$-\\mathrm{i}$ ',
+      '$i $',
+      '$-i$ ',
     ]
   }
 
@@ -37,82 +37,82 @@ const moins = randint(1, 2)
           switch(moins) {
     case 1: 
      this.enonce =
-        `$\\mathrm{i}^{${k}}$ est égal à `
+        `$i^{${k}}$ est égal à `
 this.correction =
-        `Par définition, on sait que $\\mathrm{i}^{2}=-1$ `
+        `Par définition, on sait que $i^{2}=-1$. `
     
     if (k%4===0) {
-      if (k===4) {this.correction += ` donc $\\mathrm{i}^{4}=${miseEnEvidence('1')}$ .`}
+      if (k===4) {this.correction += ` donc $i^{4}=${miseEnEvidence('1')}$ .`}
       else {
         this.correction +=
-        ` donc $\\mathrm{i}^{4}=1$ .<br>$\\begin{aligned}\\mathrm{i}^{${k}}&=\\left(\\mathrm{i}^{4}\\right)^{${k/4}}\\\\
+        ` donc $i^{4}=1$ .<br>$\\begin{aligned}i^{${k}}&=\\left(i^{4}\\right)^{${k/4}}\\\\
         &=${miseEnEvidence('1')}.
         \\end{aligned}$`
         }
        this.reponses = [
           '$1 $',
           '$-1$ ',
-          '$\\mathrm{i} $',
-      '$-\\mathrm{i}$ ',
+          '$i $',
+      '$-i$ ',
     ]}
      if (k===7) {
        this.correction +=
-         ` donc $\\mathrm{i}^{4}=1$, <br> 
+         ` donc $i^{4}=1$, <br> 
          $\\begin{aligned}
-         \\mathrm{i}^{7}&=\\mathrm{i}^{4}\\times \\mathrm{i}^{3}\\\\
-          &=\\mathrm{i}^{3}\\\\
-         &=\\mathrm{i}^{2}\\times\\mathrm{i}\\\\
-          &=${miseEnEvidence('-\\mathrm{i}.')}
+         i^{7}&=i^{4}\\times i^{3}\\\\
+          &=i^{3}\\\\
+         &=i^{2}\\timesi\\\\
+          &=${miseEnEvidence('-i.')}
          \\end{aligned}$`
        this.reponses = [
-           '$-\\mathrm{i} $',
+           '$-i $',
            '$1 $',
            '$-1$ ',
-           '$\\mathrm{i}$ ',
+           '$i$ ',
        ]
     }
      if (k===3) {
       this.correction +=
          `  <br> 
          $\\begin{aligned}
-         \\mathrm{i}^{3}&=\\mathrm{i}^{2}\\times \\mathrm{i}\\\\
-         &=${miseEnEvidence('-\\mathrm{i}.')}
+         i^{3}&=i^{2}\\times i\\\\
+         &=${miseEnEvidence('-i.')}
          \\end{aligned}$`
       this.reponses = [
-           '$-\\mathrm{i} $',
+           '$-i $',
            '$1 $',
            '$-1$ ',
-           '$\\mathrm{i}$ ',
+           '$i$ ',
        ]
     }
       if (k%4===2) {
         this.correction +=
-         ` donc $\\mathrm{i}^{4}=1$, <br>  
+         ` donc $i^{4}=1$, <br>  
          $\\begin{aligned}
-         \\mathrm{i}^{${k}}&=\\mathrm{i}^{${k-2}}\\times \\mathrm{i}^{2}\\\\
-         &=\\left(\\mathrm{i}^{4}\\right)^{${(k-2)/4}}\\times \\mathrm{i}^{2}\\\\
+         i^{${k}}&=i^{${k-2}}\\times i^{2}\\\\
+         &=\\left(i^{4}\\right)^{${(k-2)/4}}\\times i^{2}\\\\
          &=${miseEnEvidence('-1.')}
          \\end{aligned}$`
          this.reponses = [
            '$-1$ ',
-           '$-\\mathrm{i}$ ',
+           '$-i$ ',
            '$1 $',
-           '$\\mathrm{i} $',
+           '$i $',
        ]
     }
      if (k%4===1) {
          this.correction +=
-         ` donc $\\mathrm{i}^{4}=1$, <br>  
+         ` donc $i^{4}=1$, <br>  
          $\\begin{aligned}
-         \\mathrm{i}^{${k}}&=\\mathrm{i}^{${k-1}}\\times\\mathrm{i}\\\\
-         &=\\left(\\mathrm{i}^{4}\\right)^{${(k-1)/4}}\\times\\mathrm{i}\\\\
-         &=${miseEnEvidence('\\mathrm{i}.')}
+         i^{${k}}&=i^{${k-1}}\\timesi\\\\
+         &=\\left(i^{4}\\right)^{${(k-1)/4}}\\timesi\\\\
+         &=${miseEnEvidence('i.')}
          \\end{aligned}$`
          this.reponses = [
-            '$\\mathrm{i}$ ',
+            '$i$ ',
             '$1 $',
             '$-1$ ',
-            '-$\\mathrm{i} $'
+            '-$i $'
        ]
      }
      break
@@ -120,88 +120,88 @@ this.correction =
       this.enonce =
         `$\\left(\\mathrm{-i}\\right)^{${k}}$ est égal à `
       this.correction =
-        `On sait que $\\left(\\mathrm{-i}\\right)^{${k}}=\\left(\\mathrm{-1}\\right)^{${k}}\\times \\mathrm{i}^{${k}}$. <br>`
+        `On sait que $\\left(\\mathrm{-i}\\right)^{${k}}=\\left(\\mathrm{-1}\\right)^{${k}}\\times i^{${k}}$. <br>`
      this.correction +=
-        'Par définition, on a $\\mathrm{i}^{2}=-1$ '
+        'Par définition, on a $i^{2}=-1$ '
 
     if (k%4===0) {
-      if (k===4) {this.correction += ` donc $\\mathrm{i}^{4}=1$ et finalement $\\left(\\mathrm{-i}\\right)^{4}=${miseEnEvidence('1')}$ .`}
+      if (k===4) {this.correction += ` donc $i^{4}=1$ et finalement $\\left(\\mathrm{-i}\\right)^{4}=${miseEnEvidence('1')}$ .`}
       else {
         this.correction +=
-        ` donc $\\mathrm{i}^{4}=1$ .<br>
+        ` donc $i^{4}=1$ .<br>
         $\\begin{aligned}\\left(\\mathrm{-i}\\right)^{${k}}
         &=\\mathrm{-i}^{${k}}\\\\
-        &=\\left(\\mathrm{i}^{4}\\right)^{${k/4}}\\\\
+        &=\\left(i^{4}\\right)^{${k/4}}\\\\
         &=${miseEnEvidence('1')}.
         \\end{aligned}$`
         }
        this.reponses = [
           '$1 $',
           '$-1$ ',
-          '$\\mathrm{i} $',
-      '$-\\mathrm{i}$ ',
+          '$i $',
+      '$-i$ ',
     ]}
      if (k===7) {
        this.correction +=
-         ` donc $\\mathrm{i}^{4}=1$, <br> 
+         ` donc $i^{4}=1$, <br> 
          $\\begin{aligned}
-         \\left(\\mathrm{-i}\\right)^{7}&=\\left(-1\\right)^{7}\\times \\mathrm{i}^{7}\\\\
-         &=-\\mathrm{i}^{4}\\times \\mathrm{i}^{3}\\\\
-         &=-\\mathrm{i}^{3}\\\\
-         &=-\\mathrm{i}^{2}\\times\\mathrm{i}\\\\
-          &=${miseEnEvidence('\\mathrm{i}.')}
+         \\left(\\mathrm{-i}\\right)^{7}&=\\left(-1\\right)^{7}\\times i^{7}\\\\
+         &=-i^{4}\\times i^{3}\\\\
+         &=-i^{3}\\\\
+         &=-i^{2}\\timesi\\\\
+          &=${miseEnEvidence('i.')}
          \\end{aligned}$`
        this.reponses = [
-           '$\\mathrm{i} $',
+           '$i $',
            '$1 $',
            '$-1$ ',
-           '$-\\mathrm{i}$ ',
+           '$-i$ ',
        ]
     }
      if (k===3) {
       this.correction +=
          `  <br> 
          $\\begin{aligned}
-         \\left(\\mathrm{-i}\\right)^{3}&= -\\mathrm{i}^{3}\\\\
-         &=-\\mathrm{i}^{2}\\times \\mathrm{i}\\\\
-         &=${miseEnEvidence('\\mathrm{i}.')}
+         \\left(\\mathrm{-i}\\right)^{3}&= -i^{3}\\\\
+         &=-i^{2}\\times i\\\\
+         &=${miseEnEvidence('i.')}
          \\end{aligned}$`
       this.reponses = [
-           '$\\mathrm{i} $',
+           '$i $',
            '$1 $',
            '$-1$ ',
-           '$-\\mathrm{i}$ ',
+           '$-i$ ',
        ]
     }
       if (k%4===2) {
         this.correction +=
-         ` donc $\\mathrm{i}^{4}=1$, <br>  
+         ` donc $i^{4}=1$, <br>  
          $\\begin{aligned}
-         \\left(\\mathrm{-i}\\right)^{${k}}&=\\left(-1\\right)^{${k}}\\times \\mathrm{i}^{${k-2}}\\times \\mathrm{i}^{2}\\\\
-         &=\\mathrm{i}^{${k-2}}\\times \\mathrm{i}^{2}\\\\
-         &=\\left(\\mathrm{i}^{4}\\right)^{${(k-2)/4}}\\times \\mathrm{i}^{2}\\\\
+         \\left(\\mathrm{-i}\\right)^{${k}}&=\\left(-1\\right)^{${k}}\\times i^{${k-2}}\\times i^{2}\\\\
+         &=i^{${k-2}}\\times i^{2}\\\\
+         &=\\left(i^{4}\\right)^{${(k-2)/4}}\\times i^{2}\\\\
          &=${miseEnEvidence('-1.')}
          \\end{aligned}$`
          this.reponses = [
            '$-1$ ',
-           '$-\\mathrm{i}$ ',
+           '$-i$ ',
            '$1 $',
-           '$\\mathrm{i} $',
+           '$i $',
        ]
     }
      if (k%4===1) {
          this.correction +=
-         ` donc $\\mathrm{i}^{4}=1$, <br>  
+         ` donc $i^{4}=1$, <br>  
          $\\begin{aligned}
-         \\left(\\mathrm{-i}\\right)^{${k}}&=(-1)^{${k}}\\times\\mathrm{i}^{${k-1}}\\times\\mathrm{i}\\\\
-         &=-\\mathrm{i}^{${k-1}}\\times\\mathrm{i}\\\\
-         &=${miseEnEvidence('-\\mathrm{i}.')}
+         \\left(\\mathrm{-i}\\right)^{${k}}&=(-1)^{${k}}\\timesi^{${k-1}}\\timesi\\\\
+         &=-i^{${k-1}}\\timesi\\\\
+         &=${miseEnEvidence('-i.')}
          \\end{aligned}$`
          this.reponses = [
-            '$-\\mathrm{i}$ ',
+            '$-i$ ',
             '$1 $',
             '$-1$ ',
-            '$\\mathrm{i} $'
+            '$i $'
        ]
     
      
