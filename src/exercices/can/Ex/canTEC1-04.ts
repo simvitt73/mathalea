41abbefe473649f6236f912ae3f831b5e5a1efc7
mathsguyon <mathsguyon@gmail.com>
2025-09-07 21:52:32 +0200

    Update canTEC1-04.ts

--- src/exercices/can/Ex/canTEC1-04.ts
+++ src/exercices/can/Ex/canTEC1-04.ts
@@ -32,14 +32,14 @@ export default class Complexes1 extends ExerciceQcmA {
   versionAleatoire = () => {
     const k = randint(3,15)
 
-const moins = randint(1, 1)
+const moins = randint(1, 2)
 
           switch(moins) {
     case 1: 
      this.enonce =
         `$\\mathrm{i}^{${k}}$ est égal à `
 this.correction =
-        `On sait que $\\mathrm{i}^{2}=-1$ `
+        `Par définition, on sait que $\\mathrm{i}^{2}=-1$ `
     
     if (k%4===0) {
       if (k===4) {this.correction += ` donc $\\mathrm{i}^{4}=${miseEnEvidence('1')}$ .`}
@@ -118,28 +118,96 @@ this.correction =
      break
     case 2:
       this.enonce =
-        '$\\left(\\mathrm{i}\\right)^{n}$ est égal à '
+        `$\\left(\\mathrm{-i}\\right)^{${k}}$ est égal à `
       this.correction =
-        'On sait que $\\left(\\mathrm{i}\\right)^{2}=-1$ donc'
+        `On sait que $\\left(\\mathrm{-i}\\right)^{${k}}=\\left(\\mathrm{-1}\\right)^{${k}}\\times \\mathrm{i}^{${k}}$. <br>`
+     this.correction +=
+        'Par définition, on a $\\mathrm{i}^{2}=-1$ '
+
+    if (k%4===0) {
+      if (k===4) {this.correction += ` donc $\\mathrm{i}^{4}=1$ et finalement $\\left(\\mathrm{-i}\\right)^{4}=${miseEnEvidence('1')}$ .`}
+      else {
+        this.correction +=
+        ` donc $\\mathrm{i}^{4}=1$ .<br>
+        $\\begin{aligned}\\left(\\mathrm{-i}\\right)^{${k}}
+        &=\\mathrm{-i}^{${k}}\\\\
+        &=\\left(\\mathrm{i}^{4}\\right)^{${k/4}}\\\\
+        &=${miseEnEvidence('1')}.
+        \\end{aligned}$`
+        }
+       this.reponses = [
+          '$1 $',
+          '$-1$ ',
+          '$\\mathrm{i} $',
+      '$-\\mathrm{i}$ ',
+    ]}
+     if (k===7) {
+       this.correction +=
+         ` donc $\\mathrm{i}^{4}=1$, <br> 
+         $\\begin{aligned}
+         \\left(\\mathrm{-i}\\right)^{7}&=\\left(-1\\right)^{7}\\times \\mathrm{i}^{7}\\\\
+         &=-\\mathrm{i}^{4}\\times \\mathrm{i}^{3}\\\\
+         &=-\\mathrm{i}^{3}\\\\
+         &=-\\mathrm{i}^{2}\\times\\mathrm{i}\\\\
+          &=${miseEnEvidence('\\mathrm{i}.')}
+         \\end{aligned}$`
+       this.reponses = [
+           '$\\mathrm{i} $',
+           '$1 $',
+           '$-1$ ',
+           '$-\\mathrm{i}$ ',
+       ]
+    }
+     if (k===3) {
       this.correction +=
-        ` $\\left(\\mathrm{i}\\right)^{${k}}=`
-       
+         `  <br> 
+         $\\begin{aligned}
+         \\left(\\mathrm{-i}\\right)^{3}&= -\\mathrm{i}^{3}\\\\
+         &=-\\mathrm{i}^{2}\\times \\mathrm{i}\\\\
+         &=${miseEnEvidence('\\mathrm{i}.')}
+         \\end{aligned}$`
       this.reponses = [
-        '$\\left(-1\\right)^{n} $',
-        '$\\left(-1\\right)^{n+1}$ ',
-        '$-\\left(-1\\right)^{n} $',
-        '$\\left(-1\\right)^{n-1}$ ',
-      ]
+           '$\\mathrm{i} $',
+           '$1 $',
+           '$-1$ ',
+           '$-\\mathrm{i}$ ',
+       ]
+    }
+      if (k%4===2) {
+        this.correction +=
+         ` donc $\\mathrm{i}^{4}=1$, <br>  
+         $\\begin{aligned}
+         \\left(\\mathrm{-i}\\right)^{${k}}&=\\left(-1\\right)^{${k}}\\times \\mathrm{i}^{${k-2}}\\times \\mathrm{i}^{2}\\\\
+         &=\\mathrm{i}^{${k-2}}\\times \\mathrm{i}^{2}\\\\
+         &=\\left(\\mathrm{i}^{4}\\right)^{${(k-2)/4}}\\times \\mathrm{i}^{2}\\\\
+         &=${miseEnEvidence('-1.')}
+         \\end{aligned}$`
+         this.reponses = [
+           '$-1$ ',
+           '$-\\mathrm{i}$ ',
+           '$1 $',
+           '$\\mathrm{i} $',
+       ]
+    }
+     if (k%4===1) {
+         this.correction +=
+         ` donc $\\mathrm{i}^{4}=1$, <br>  
+         $\\begin{aligned}
+         \\left(\\mathrm{-i}\\right)^{${k}}&=(-1)^{${k}}\\times\\mathrm{i}^{${k-1}}\\times\\mathrm{i}\\\\
+         &=-\\mathrm{i}^{${k-1}}\\times\\mathrm{i}\\\\
+         &=${miseEnEvidence('-\\mathrm{i}.')}
+         \\end{aligned}$`
+         this.reponses = [
+            '$-\\mathrm{i}$ ',
+            '$1 $',
+            '$-1$ ',
+            '$\\mathrm{i} $'
+       ]
     
-      this.reponses = [
-        '$1 $',
-        '$-1$ ',
-        '$\\mathrm{i} $',
-        '$-\\mathrm{i}$ ',
-      ]
+     
       break
-  }
-}
+      }
+}}
   constructor() {
     super()
     this.versionAleatoire()
