ddb1328005ed2c00bb7b8b878ca4b635cd1fc26b
mathsguyon <mathsguyon@gmail.com>
2025-09-09 09:31:53 +0200

    Correction 1AC3-6

--- src/exercices/1e/1A-C3-6.ts
+++ src/exercices/1e/1A-C3-6.ts
@@ -38,7 +38,7 @@ export default class Puissances extends ExerciceQcmA {
     this.correction = `On applique la propriété du quotient des puissances d'un réel : <br>
     Soit $n$ et $p$ deux entiers et $a$ un réel :  $\\dfrac{a^n}{a^p}=a^{n-p}$<br>
     $\\begin{aligned} \\dfrac{a^{n^{${k}}}}{a^{n}}&=a^{n^{${k}}-n}\\\\
-    &=${miseEnEvidence(`a^{n(n^{${rienSi1(k - 1)}}-1)}`)}
+    &=${miseEnEvidence(`a^{n^{${rienSi1(k - 1)}}(n-1)}`)}
     \\end{aligned}$<br>`
     this.reponses = [
       `$a^{n^{${rienSi1(k - 1)}}(n-1)}$`,
