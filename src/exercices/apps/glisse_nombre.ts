785ae59ea4eb9aa063814d930d68dcb047df33fc
Rémi Angot <remi.angot@gmail.com>
2025-09-09 21:19:13 +0200

    feat: ajout du glisse-nombre dans les apps tierces

---
+++ src/exercices/apps/glisse_nombre.ts
@@ -0,0 +1,10 @@
+import ExternalApp from './_ExternalApp'
+
+export const uuid = 'glisseNombre'
+export const titre = 'Glisse-nombre'
+
+export default class GlisseNombre extends ExternalApp {
+  constructor() {
+    super('https://coopmaths.fr/apps/glisse-nombre/')
+  }
+}
