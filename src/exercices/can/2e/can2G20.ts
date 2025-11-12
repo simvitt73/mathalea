4d339e81fdeedd328a29e20dc818b15cbe42bc34
gillesMora <gilles.mora@ac-normandie.fr>
2025-11-12 15:47:27 +0100

    fix:can2G20

--- src/exercices/can/2e/can2G20.ts
+++ src/exercices/can/2e/can2G20.ts
@@ -203,20 +203,34 @@ $m=\\dfrac{${miseEnEvidence(deltaY, 'blue')}}{${miseEnEvidence(deltaX, 'red')}}$
       this.correction += `<br>${objetC}<br>`
 
       // Les réponses sont toujours basées sur la vraie équation y = mx + p où m = (yB-yA)/(xB-xA) et p = yA
+      if((yB - yA) / (xB - xA) === 1 || (yB - yA) / (xB - xA) === -1) {
       this.reponse = this.versionQcm
+        ? `$y= ${rienSi1(maFraction.valeurDecimale)}x${yA === 0 ? '' : ` ${ecritureAlgebrique(yA)}`}$`
+        : [
+            `y=${maFraction.valeurDecimale}x${ecritureAlgebrique(yA)}`,
+            `y=\\frac{${yB - yA}}{${xB - xA}}x${ecritureAlgebrique(yA)}`,
+            `y=\\frac{${yA - yB}}{${xA - xB}}x${ecritureAlgebrique(yA)}`,
+          ]}
+          else{this.reponse = this.versionQcm
         ? `$y= ${maFraction.texFractionSimplifiee}x${yA === 0 ? '' : ` ${ecritureAlgebrique(yA)}`}$`
         : [
             `y=${maFraction.texFraction}x${ecritureAlgebrique(yA)}`,
             `y=\\frac{${yB - yA}}{${xB - xA}}x${ecritureAlgebrique(yA)}`,
             `y=\\frac{${yA - yB}}{${xA - xB}}x${ecritureAlgebrique(yA)}`,
-          ]
+          ]}
 
       // Distracteurs basés sur des erreurs classiques
+       if((yB - yA) / (xB - xA) === 1 || (yB - yA) / (xB - xA) === -1) {
       this.distracteurs = [
+        `$y= ${rienSi1(maFraction.multiplieEntier(-1).valeurDecimale)}x${ecritureAlgebrique(yA)}$`, // Coefficient opposé
+        `$y=${yA}$`, // Droite horizontale (erreur)
+        `$y= ${rienSi1(yA)}x${ecritureAlgebrique(yA)}$`, // Fraction inversée
+      ]}
+      else{ this.distracteurs = [
         `$y= ${maFraction.multiplieEntier(-1).texFractionSimplifiee}x${ecritureAlgebrique(yA)}$`, // Coefficient opposé
         `$y=${yA}$`, // Droite horizontale (erreur)
         `$y= ${new FractionEtendue(xB - xA, yB - yA).texFractionSimplifiee}x${ecritureAlgebrique(yA)}$`, // Fraction inversée
-      ]
+      ]}
     }
 
     this.canEnonce = this.question
