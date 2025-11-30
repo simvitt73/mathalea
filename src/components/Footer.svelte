3aa22de869848e6c35b5e3a914db1dbbd474132c
Guillaume Valmont <guillaume.valmont@ac-reunion.fr>
2025-11-30 15:16:04 +0400

    Ajout d'un lien pour faire un don

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -8,14 +8,6 @@
     : 'pt-4'} w-full bg-coopmaths-canvas dark:bg-coopmathsdark-canvas h-12 flex justify-center items-center opacity-100 pb-4 px-4"
 >
   <a
-    class="flex flex-row items-center mx-1
-    text-coopmaths-action hover:text-coopmaths-action-lightest
-    dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
-    href="mailto:contact@coopmaths.fr"
-  >
-    <i class="bx bx-sm bx-envelope"></i>
-  </a>
-  <a
     href="https://coopmaths.fr/www/mentions_legales/"
     target="_blank"
     class="mx-2"
@@ -30,8 +22,18 @@
     class="flex flex-row items-center mx-1
     text-coopmaths-action hover:text-coopmaths-action-lightest
     dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
+    href="mailto:contact@coopmaths.fr"
+    aria-label="enveloppe"
+  >
+    <i class="bx bx-sm bx-envelope"></i>
+  </a>
+  <a
+    class="flex flex-row items-center mx-1
+    text-coopmaths-action hover:text-coopmaths-action-lightest
+    dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
     href="https://www.helloasso.com/associations/coopmaths/formulaires/1"
     target="_blank"
+    aria-label="faire un don"
   >
     <i class="bx bx-sm bx-donate-heart"></i>
   </a>
