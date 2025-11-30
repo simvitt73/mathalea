0d7e2814c70140b67569c9bf2ceb974b37cac0c5
Guillaume Valmont <guillaume.valmont@ac-reunion.fr>
2025-11-30 15:16:04 +0400

    Ajout d'un lien pour faire un don

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,27 +5,34 @@
 <footer
   class="{$keyboardState.isVisible
     ? 'pt-48 md:pt-60'
-    : 'pt-4'} w-full bg-coopmaths-canvas dark:bg-coopmathsdark-canvas h-12 flex flex-row-reverse justify-center items-center opacity-100 pb-4 px-4"
+    : 'pt-4'} w-full bg-coopmaths-canvas dark:bg-coopmathsdark-canvas h-12 flex justify-center items-center opacity-100 pb-4 px-4"
 >
-  <a href="https://coopmaths.fr/www/mentions_legales/" target="_blank">
+  <a
+    class="flex flex-row items-center mx-1
+    text-coopmaths-action hover:text-coopmaths-action-lightest
+    dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
+    href="mailto:contact@coopmaths.fr"
+  >
+    <i class="bx bx-sm bx-envelope"></i>
+  </a>
+  <a
+    href="https://coopmaths.fr/www/mentions_legales/"
+    target="_blank"
+    class="mx-2"
+  >
     <img
-      class="hidden md:block px-6"
       style="height: 20px"
       src="assets/images/logo2.png"
       alt="Logo Coopmaths 2"
     />
   </a>
-  <!-- <a
-    class="flex flex-row items-center justify-start pr-6 font-thin text-coopmaths-action hover:text-coopmaths-action-lightest dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
-    href="https://mathstodon.xyz/@coopmaths"
+  <a
+    class="flex flex-row items-center mx-1
+    text-coopmaths-action hover:text-coopmaths-action-lightest
+    dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
+    href="https://www.helloasso.com/associations/coopmaths/formulaires/1"
     target="_blank"
   >
-    <i class="bx bx-sm bxl-mastodon" />
-  </a> -->
-  <a
-    class="flex flex-row items-center justify-start pr-6 font-thin text-coopmaths-action hover:text-coopmaths-action-lightest dark:text-coopmathsdark-action dark:hover:text-coopmathsdark-action-lightest"
-    href="mailto:contact@coopmaths.fr"
-  >
-    <i class="bx bx-sm bx-envelope"></i>
+    <i class="bx bx-sm bx-donate-heart"></i>
   </a>
 </footer>
