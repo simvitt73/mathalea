873048e70f3e14515ddf56422b1a3acae8ebef2b
Olivier Mimeau <olivier.mimeau@ac-orleans-tours.fr>
2025-12-14 06:13:01 +0000

    Om 3 l11 10 passage ts et factorisation

---
+++ src/exercices/3e/3L11-10.ts
@@ -0,0 +1,275 @@
+import { tableauColonneLigne } from '../../lib/2d/tableau'
+import { KeyboardType } from '../../lib/interactif/claviers/keyboard'
+import { functionCompare } from '../../lib/interactif/comparisonFunctions'
+import { handleAnswers } from '../../lib/interactif/gestionInteractif'
+import { toutPourUnPoint } from '../../lib/interactif/mathLive'
+import { ajouteChampTexteMathLive } from '../../lib/interactif/questionMathLive'
+import { AddTabDbleEntryMathlive } from '../../lib/interactif/tableaux/AjouteTableauMathlive'
+import { combinaisonListes } from '../../lib/outils/arrayOutils'
+import {
+  reduireAxPlusB,
+  reduirePolynomeDegre3,
+} from '../../lib/outils/ecritures'
+import { lettreDepuisChiffre } from '../../lib/outils/outilString'
+import { context } from '../../modules/context'
+import {
+  egal,
+  listeQuestionsToContenuSansNumero,
+  randint,
+} from '../../modules/outils'
+import Exercice from '../Exercice'
+export const titre = 'Table de double distributivité'
+export const dateDePublication = '23/02/2023'
+export const dateDeModifImportante = '04/01/2025'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+/**
+ * Développer des expressions de double distributivité à l'aide d'un tableau de  multiplication
+ * @author Sébastien LOZANO
+ */
+
+export const uuid = 'c8403'
+
+export const refs = {
+  'fr-fr': ['3L11-10'],
+  'fr-ch': ['11FA2-6'],
+}
+export default class TableDoubleDistributivite extends Exercice {
+  constructor() {
+    super()
+    this.besoinFormulaireNumerique = [
+      'Niveau de difficulté',
+      3,
+      ' 1 : (x+a)(x+b) et (ax+b)(cx+d)\n 2 : (ax-b)(cx+d) et (ax-b)(cx-d)\n 3 : Mélange',
+    ]
+
+    this.spacing = context.isHtml ? 3 : 2
+    this.spacingCorr = context.isHtml ? 3 : 2
+    this.nbQuestions = 5
+    this.sup = 1
+
+    this.listeAvecNumerotation = false
+    this.exoCustomResultat = true
+  }
+
+  nouvelleVersion() {
+    this.answers = {}
+    this.consigne =
+      this.nbQuestions > 1
+        ? 'Dans chaque cas, compléter les tables de multiplication, écrire le développement obtenu et le réduire.'
+        : 'Compléter la table de multiplication, écrire le développement obtenu et le réduire.'
+
+    let typesDeQuestionsDisponibles = [1, 2]
+    if (this.sup === 2) {
+      typesDeQuestionsDisponibles = [3, 4]
+    } else if (this.sup === 3) {
+      typesDeQuestionsDisponibles = [1, 2, 3, 4]
+    }
+
+    const listeTypeDeQuestions = combinaisonListes(
+      typesDeQuestionsDisponibles,
+      this.nbQuestions,
+    )
+
+    for (
+      let i = 0, cpt = 0, a, b, c, d, typesDeQuestions;
+      i < this.nbQuestions && cpt < 50;
+
+    ) {
+      typesDeQuestions = listeTypeDeQuestions[i]
+      let texte = ''
+      let texteCorr = ''
+      let developpements: Developpements = {
+        termesRectangles: [],
+        eclate: '',
+        reduit: '',
+      }
+      let tableau: TableauData = {
+        entetesCol: [],
+        entetesLgn: [],
+        contenu: [],
+        L1C1txt: '',
+        L1C2txt: '',
+        L2C1txt: '',
+        L2C2nbr: 0,
+      }
+
+      a = randint(2, 9)
+      b = randint(2, 9)
+      c = randint(2, 9, [a])
+      d = randint(2, 9, [b])
+
+      this.autoCorrection[3 * i] = {}
+      this.autoCorrection[3 * i + 1] = {}
+      this.autoCorrection[3 * i + 2] = {}
+      switch (typesDeQuestions) {
+        case 1: // (x+b)(x+d)
+          b = randint(2, 10)
+          d = randint(2, 12)
+          texte = `$${lettreDepuisChiffre(i + 1)} = (x+${b})(x+${d})$`
+          texteCorr = `$${lettreDepuisChiffre(i + 1)} = (x+${b})(x+${d})$`
+          developpements = faitDeveloppement(1, d, b, b * d)
+          tableau = faitTableau(1, b, 1, d)
+          break
+        case 2: // (ax+b)(cx+d)
+          texte = `$${lettreDepuisChiffre(i + 1)} = (${a}x+${b})(${c}x+${d})$`
+          texteCorr = `$${lettreDepuisChiffre(i + 1)} = (${a}x+${b})(${c}x+${d})$`
+          developpements = faitDeveloppement(a * c, b * c, a * d, b * d)
+          tableau = faitTableau(a, b, c, d)
+          break
+        case 3: // (ax-b)(cx+d)
+          texte = `$${lettreDepuisChiffre(i + 1)} = (${a}x-${b})(${c}x+${d})$`
+          texteCorr = `$${lettreDepuisChiffre(i + 1)} = (${a}x-${b})(${c}x+${d})$`
+          if (egal(a * d - b * c, 0)) {
+            developpements = faitDeveloppement(a * c, a * d, -b * c, -b * d)
+          } else {
+            developpements = faitDeveloppement(a * c, a * d, -b * c, -b * d)
+          }
+          tableau = faitTableau(a, -b, c, d)
+          break
+        case 4: // (ax-b)(cx-d)
+          texte = `$${lettreDepuisChiffre(i + 1)} = (${a}x-${b})(${c}x-${d})$`
+          texteCorr = `$${lettreDepuisChiffre(i + 1)} = (${a}x-${b})(${c}x-${d})$`
+          developpements = faitDeveloppement(a * c, -a * d, -b * c, b * d)
+          tableau = faitTableau(a, -b, c, -d)
+          break
+      }
+      texte += context.isHtml ? '<br>' : '\\par\\medskip'
+
+      if (this.interactif) {
+        const tableauVide = AddTabDbleEntryMathlive.convertTclToTableauMathlive(
+          tableau.entetesCol,
+          tableau.entetesLgn,
+          ['', '', '', ''],
+        )
+        // const NumEx = this.numeroExercice ? this.numeroExercice : 1
+        // OM:  fonctionne pas
+        const tabMathlive = AddTabDbleEntryMathlive.create(
+          this.numeroExercice,
+          3 * i,
+          tableauVide,
+          ` ${KeyboardType.clavierDeBaseAvecVariable}`,
+          this.interactif,
+          { L0C0: 'red' },
+        )
+        texte += tabMathlive.output
+      } else {
+        texte += tableauColonneLigne(
+          tableau.entetesCol,
+          tableau.entetesLgn,
+          tableau.contenu,
+          1,
+          true,
+          this.numeroExercice,
+          i,
+          false,
+          { L0C0: 'red' },
+        )
+      }
+      texte += context.isHtml
+        ? '<br> Développement : '
+        : '\\par\\medskip Développement : '
+      texte += ajouteChampTexteMathLive(this, 3 * i + 1, '')
+      texte += context.isHtml
+        ? '<br> Développement réduit : '
+        : '\\par\\medskip Développement réduit: '
+      texte += ajouteChampTexteMathLive(this, 3 * i + 2, '')
+      texteCorr += context.isHtml ? '<br>' : '\\par\\medskip'
+      texteCorr += tableauColonneLigne(
+        tableau.entetesCol,
+        tableau.entetesLgn,
+        [
+          tableau.L1C1txt,
+          tableau.L1C2txt,
+          tableau.L2C1txt,
+          `${tableau.L2C2nbr}`,
+        ],
+        1,
+        true,
+        this.numeroExercice,
+        i,
+        false,
+        { L0C0: 'red' },
+      )
+      texteCorr += context.isHtml ? '<br>' : '\\par\\medskip '
+      texteCorr += `Développement : $${lettreDepuisChiffre(i + 1)} = ${developpements.eclate}$`
+      texteCorr += context.isHtml ? '<br>' : '\\par\\medskip '
+      texteCorr += `Développement réduit : $${lettreDepuisChiffre(i + 1)} = ${developpements.reduit}$`
+
+      handleAnswers(this, 3 * i, {
+        bareme: toutPourUnPoint,
+        L1C1: { value: tableau.L1C1txt, compare: functionCompare },
+        L1C2: { value: tableau.L1C2txt, compare: functionCompare },
+        L2C1: { value: tableau.L2C1txt, compare: functionCompare },
+        L2C2: { value: tableau.L2C2nbr, compare: functionCompare },
+      })
+      handleAnswers(this, 3 * i + 1, {
+        reponse: { value: developpements.eclate, compare: functionCompare },
+      })
+      const reponse = developpements.reduit
+      handleAnswers(this, 3 * i + 2, { reponse: { value: reponse } })
+
+      if (this.questionJamaisPosee(i, a, b, c, d, typesDeQuestions)) {
+        // Si la question n'a jamais été posée, on en créé une autre
+        this.listeQuestions[i] = texte
+        this.listeCorrections[i] = texteCorr
+        i++
+      }
+      cpt++
+    }
+    listeQuestionsToContenuSansNumero(this)
+  }
+}
+
+interface Developpements {
+  termesRectangles: number[]
+  eclate: string
+  reduit: string
+}
+
+interface TableauData {
+  entetesCol: string[]
+  entetesLgn: string[]
+  contenu: string[]
+  L1C1txt: string
+  L1C2txt: string
+  L2C1txt: string
+  L2C2nbr: number
+}
+
+function faitDeveloppement(
+  a: number,
+  b: number,
+  c: number,
+  d: number,
+): Developpements {
+  const plusMoins = c >= 0 ? '+' : '-'
+  const reponse = {
+    termesRectangles: [a, b, c, d],
+    eclate:
+      reduirePolynomeDegre3(0, a, b, 0) +
+      ` ${plusMoins} ` +
+      reduireAxPlusB(Math.abs(c), d), // `${a}x^2+${b}x+${c}x+${d}`,
+    reduit: reduirePolynomeDegre3(0, a, b + c, d), // `${a}x^2+${b + c}x+${d}`,
+  }
+  return reponse
+}
+
+function faitTableau(a: number, b: number, c: number, d: number): TableauData {
+  // ici (ax+b)(cx+d)
+  const tableau: TableauData = {
+    entetesCol: ['\\times', reduireAxPlusB(a, 0), `${b}`],
+    entetesLgn: [reduireAxPlusB(c, 0), `${d}`],
+    contenu: [
+      `\\phantom{${a}x}`,
+      `\\phantom{${b}}`,
+      `\\phantom{${c}x}`,
+      `\\phantom{${d}}`,
+    ],
+    L1C1txt: reduirePolynomeDegre3(0, a * c, 0, 0), // `${a * c}x^2`,
+    L1C2txt: reduireAxPlusB(b * c, 0), // `${b * c}x`,
+    L2C1txt: reduireAxPlusB(a * d, 0), // `${a * c}x`,
+    L2C2nbr: b * d,
+  }
+  return tableau
+}
