a71d805a5de00869abb36840b7ad5b4fa81473f2
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2025-12-15 20:33:55 +0000

    4 g22 10 up

--- src/exercices/2e/2G11-10.js
+++ src/exercices/2e/2G11-10.js
@@ -1,6 +1,7 @@
 import TriangleEquilateral from '../4e/4G22-0'
 
-export const titre = 'Calculer des longueurs dans un triangle équilatéral'
+export const titre =
+  'Calculer dans un triangle équilatéral (longueur, hauteur, aire)'
 export const dateDePublication = '12/12/2025'
 export const interactifReady = true
 export const interactifType = 'mathLive'
@@ -15,9 +16,19 @@ export default class TriangleEquilateral2e extends TriangleEquilateral {
   constructor() {
     super()
     this.niveau = 2
+    this.sup = 3
     this.besoinFormulaireTexte = [
       'Type de questions',
       "Nombres séparés par des tirets\n1 : Calcul de la hauteur\n2 : Calcul de l'aire\n3 : Calcul du côté",
     ]
+    this.besoinFormulaire2Numerique = [
+      'Type de réponse attendue',
+      3,
+      '1 : Valeur exacte\n2 : Valeur approchée par excès ou par défaut à 0,1 cm près\n3 : Valeur arrondie à 0,1 cm près',
+    ]
+    this.sup2 = 1
+    this.besoinFormulaire3CaseACocher = ['Avec figure', true]
+    this.sup3 = true
+    this.nbQuestions = 1
   }
 }
