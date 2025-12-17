d15354505d7f2b6d005e9299d25617935f805ff1
Eric Elter <eric.elter@ac-normandie.fr>
2025-12-17 16:36:35 +0000

    fix : Interactif changé sur can3NY-2026-Q20

--- src/exercices/can/canNY-2026/can3NY-2026-Q20.ts
+++ src/exercices/can/canNY-2026/can3NY-2026-Q20.ts
@@ -23,7 +23,7 @@ export default class additionFraction2026 extends ExerciceSimple {
     super()
     this.typeExercice = 'simple' // Cette ligne est très importante pour faire un exercice simple !
     this.nbQuestions = 1
-    this.optionsDeComparaison = { fractionEgale: true }
+    // this.optionsDeComparaison = { fractionEgale: true }
     this.formatChampTexte = KeyboardType.clavierDeBaseAvecFraction
     this.optionsChampTexte = { texteAvant: ' $=$' }
   }
@@ -31,16 +31,16 @@ export default class additionFraction2026 extends ExerciceSimple {
   nouvelleVersion() {
     const a = this.canOfficielle ? 2 : randint(1, 10)
     const annee = 2026
+    // this.question = `Calculer sous la forme d'une fraction :<br>`
+    this.question = `Calculer :<br>`
     if (this.canOfficielle ? true : choice([true, false])) {
       this.reponse = new FractionEtendue(1 + 2 * a, 2 * annee).toLatex()
-      this.question = `Calculer sous la forme d'une fraction :<br>
-    $\\dfrac{${a}}{${texNombre(annee, 0)}} +\\dfrac{1}{${texNombre(2 * annee)}}$.`
+      this.question += `$\\dfrac{${a}}{${texNombre(annee, 0)}} +\\dfrac{1}{${texNombre(2 * annee)}}$`
 
       this.correction = ` $\\dfrac{${a}}{${texNombre(annee, 0)}} +\\dfrac{1}{${texNombre(2 * annee, 0)}}=\\dfrac{${2 * a}}{${texNombre(2 * annee, 0)}} +\\dfrac{1}{${texNombre(2 * annee, 0)}}=${miseEnEvidence(`\\dfrac{${1 + 2 * a}}{${texNombre(2 * annee, 0)}}`)}$`
     } else {
       this.reponse = new FractionEtendue(2 * a - 1, 2 * annee).toLatex()
-      this.question = `Calculer sous la forme d'une fraction : <br>
-    $\\dfrac{${a}}{${texNombre(annee, 0)}} -\\dfrac{1}{${texNombre(2 * annee)}}$`
+      this.question += `$\\dfrac{${a}}{${texNombre(annee, 0)}} -\\dfrac{1}{${texNombre(2 * annee)}}$`
       this.correction = ` $\\dfrac{${a}}{${texNombre(annee, 0)}} -\\dfrac{1}{${texNombre(2 * annee, 0)}}=\\dfrac{${2 * a}}{${texNombre(2 * annee, 0)}} -\\dfrac{1}{${texNombre(2 * annee, 0)}}=${miseEnEvidence(`\\dfrac{${2 * a - 1}}{${texNombre(2 * annee, 0)}}`)}$`
     }
 
