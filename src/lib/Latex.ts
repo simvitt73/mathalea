857164c992af3a6bb63d7cc410cd8c9aa9197f4e
Guillaume Valmont <guillaume.valmont@ac-reunion.fr>
2025-12-27 14:31:31 +0400

    refactor: writeInCols(writeQuestions()) = buildContent()

--- src/lib/Latex.ts
+++ src/lib/Latex.ts
@@ -236,13 +236,10 @@ class Latex {
             '\\anote{',
             '\n\\resetcustomnotes',
           )
-          contentCorr += writeInCols(
-            writeQuestions(
-              exercice.listeCorrections,
-              exercice.spacingCorr,
-              Boolean(exercice.listeAvecNumerotation),
-              Number(exercice.nbColsCorr),
-            ),
+          contentCorr += buildContent(
+            exercice.listeCorrections,
+            exercice.spacingCorr,
+            Boolean(exercice.listeAvecNumerotation),
             Number(exercice.nbColsCorr),
           )
           contentCorr += testIfLoaded(
@@ -254,13 +251,10 @@ class Latex {
           content += `\n% @see : ${getUrlFromExercice(exercice)}`
           content += `\n\\begin{EXO}{${testIfLoaded([exercice.introduction, exercice.consigne, ...exercice.listeQuestions], '\\anote{', '\n\\resetcustomnotes ')}${format(exercice.consigne, false)}}{${String(exercice.id).replace('.js', '')}}\n`
           content += writeIntroduction(exercice.introduction)
-          content += writeInCols(
-            writeQuestions(
-              exercice.listeQuestions,
-              exercice.spacing,
-              Boolean(exercice.listeAvecNumerotation),
-              Number(exercice.nbCols),
-            ),
+          content += buildContent(
+            exercice.listeQuestions,
+            exercice.spacing,
+            Boolean(exercice.listeAvecNumerotation),
             Number(exercice.nbCols),
           )
           content += testIfLoaded(
@@ -393,15 +387,12 @@ class Latex {
         }
         content += writeIntroduction(exercice.introduction)
         content += '\n' + format(exercice.consigne)
-        content += writeInCols(
-          writeQuestions(
-            exercice.listeQuestions,
-            exercice.spacing,
-            Boolean(exercice.listeAvecNumerotation),
-            Number(exercice.nbCols),
-            confExo,
-          ),
+        content += buildContent(
+          exercice.listeQuestions,
+          exercice.spacing,
+          Boolean(exercice.listeAvecNumerotation),
           confExo.cols ? confExo.cols : Number(exercice.nbCols),
+          confExo,
         )
         content += testIfLoaded(
           [
@@ -419,13 +410,10 @@ class Latex {
           '\\anote{',
           '\n\\resetcustomnotes',
         )
-        content += writeInCols(
-          writeQuestions(
-            exercice.listeCorrections,
-            exercice.spacingCorr,
-            Boolean(exercice.listeAvecNumerotation),
-            Number(exercice.nbCols),
-          ),
+        content += buildContent(
+          exercice.listeCorrections,
+          exercice.spacingCorr,
+          Boolean(exercice.listeAvecNumerotation),
           confExo.cols_corr ? confExo.cols_corr : Number(exercice.nbColsCorr),
         )
         content += testIfLoaded(
@@ -745,7 +733,7 @@ function writeIntroduction(introduction = ''): string {
   return content
 }
 
-function writeQuestions(
+function buildContent(
   questions: string[],
   spacing = 1,
   numbersNeeded: boolean,
@@ -791,12 +779,10 @@ function writeQuestions(
       content += `\n ${format(questions[0])}${blocrep}`
     }
   }
-  return content
-}
-
-function writeInCols(text: string, nb: number): string {
-  if (nb < 2) return text
-  return `\\begin{multicols}{${nb}}${text}\n\\end{multicols}`
+  if (nbCols < 2) {
+    return content
+  }
+  return `\\begin{multicols}{${nbCols}}${content}\n\\end{multicols}`
 }
 
 /**
