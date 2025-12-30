bd7abba4bd73975f96ecdc47b5dbcf03d23d102b
Olivier Mimeau <olivier.mimeau@ac-orleans-tours.fr>
2025-12-30 19:29:34 +0000

    Om 3 l11 17 variables didactiques

--- src/exercices/3e/3L11-17.ts
+++ src/exercices/3e/3L11-17.ts
@@ -1,6 +1,6 @@
 import { tableauColonneLigne } from '../../lib/2d/tableau'
 import { handleAnswers } from '../../lib/interactif/gestionInteractif'
-import { toutPourUnPoint } from '../../lib/interactif/mathlive'
+import { toutPourUnPoint } from '../../lib/interactif/mathLive'
 import { ajouteChampTexteMathLive } from '../../lib/interactif/questionMathLive'
 import { AddTabDbleEntryMathlive } from '../../lib/interactif/tableaux/AjouteTableauMathlive'
 import { combinaisonListes } from '../../lib/outils/arrayOutils'
@@ -13,14 +13,14 @@ import { context } from '../../modules/context'
 import {
   gestionnaireFormulaireTexte,
   listeQuestionsToContenuSansNumero,
-  randint
+  randint,
 } from '../../modules/outils'
 import Exercice from '../Exercice'
 export const titre = 'Table de simple distributivité'
 export const dateDePublication = '29/12/2025'
 // export const dateDeModifImportante = '04/01/2025'
- export const interactifReady = true
- export const interactifType = 'mathLive'
+export const interactifReady = true
+export const interactifType = 'mathLive'
 /**
  * Développer des expressions de simple distributivité à l'aide d'un tableau de  multiplication
  * @author Olivier Mimeau
@@ -36,20 +36,43 @@ export default class TableSimpleDistributivite extends Exercice {
   constructor() {
     super()
     this.besoinFormulaireTexte = [
-            'Choix des questions',
+      'Choix des questions',
       `Nombres séparés par des tirets :\n
 1 : a(bx+c)\n
-2 : a(bx-c)\n
-3 : ax(bx+c)\n
-4 : ax(bx-c)\n`
+2 : ax(bx+c)\n`,
     ]
-this.besoinFormulaire2CaseACocher = ['a peut-être négatif']
-// this.besoinFormulaire3CaseACocher = ['b peut etre négatif']
+    this.besoinFormulaire2Texte = [
+      'Choix pour a',
+      `Nombres séparés par des tirets :\n
+1 : 1<a<10\n
+2 : a=1 si forme ax(bx+c)\n
+3 : a<0\n
+4: Mélange\n`,
+    ]
 
-    this.spacing = context.isHtml ? 3 : 2
-    this.spacingCorr = context.isHtml ? 3 : 2
+    this.besoinFormulaire3Texte = [
+      'Choix pour b',
+      `Nombres séparés par des tirets :\n
+1 : 1<b<10\n
+2 : b=1\n
+3 : b<0\n
+4: Mélange\n`,
+    ]
+    this.besoinFormulaire4Texte = [
+      'pour c',
+      `Nombres séparés par des tirets :\n
+1 : 0<c<10\n
+2 : c<0\n
+3: Mélange\n`,
+    ]
+
+    this.spacing = context.isHtml ? 1 : 2
+    this.spacingCorr = context.isHtml ? 1 : 2
     this.nbQuestions = 4
-    this.sup = '1-2-3-4'
+    this.sup = '1-2'
+    this.sup2 = '1'
+    this.sup3 = '1'
+    this.sup4 = '3'
     this.listeAvecNumerotation = false
     this.exoCustomResultat = true
   }
@@ -61,23 +84,41 @@ this.besoinFormulaire2CaseACocher = ['a peut-être négatif']
         ? 'Dans chaque cas, compléter les tables de multiplication puis écrire le développement obtenu.'
         : 'Compléter la table de multiplication puis écrire le développement obtenu.'
 
-    const typesDeQuestionsDisponibles = gestionnaireFormulaireTexte({
-      saisie: this.sup,
-      min: 1,
-      max: 4,
-      melange: 5,
-      defaut: 5,
-      nbQuestions: this.nbQuestions,
-    })
-    const listeTypeDeQuestions = combinaisonListes(
-      typesDeQuestionsDisponibles,
+    const listeTypeDeQuestions = gereFormulairesTextes(
+      this.sup,
+      1,
+      2,
+      5,
+      5,
       this.nbQuestions,
     )
-
+    const listeChoixa = gereFormulairesTextes(
+      this.sup2,
+      1,
+      3,
+      4,
+      4,
+      this.nbQuestions,
+    )
+    const listeChoixb = gereFormulairesTextes(
+      this.sup3,
+      1,
+      3,
+      4,
+      4,
+      this.nbQuestions,
+    )
+    const listeChoixc = gereFormulairesTextes(
+      this.sup4,
+      1,
+      2,
+      3,
+      3,
+      this.nbQuestions,
+    )
     for (
-      let i = 0, cpt = 0, a1, a0,b, c, typesDeQuestions;
+      let i = 0, cpt = 0, a1, a0, b, c, typesDeQuestions;
       i < this.nbQuestions && cpt < 50;
-
     ) {
       typesDeQuestions = listeTypeDeQuestions[i]
       let texte = ''
@@ -88,47 +129,48 @@ this.besoinFormulaire2CaseACocher = ['a peut-être négatif']
         entetesLgn: [],
         contenu: [],
         L1C1txt: '',
-        L1C2txt: '',
-
+        L1C2txt: [],
       }
-// a1x+a0
-      a1 = randint(2, 9)
+      // a1x+a0
+      a1 = listeChoixa[i] === 2 ? 1 : randint(2, 9)
       a0 = randint(2, 9)
-      b = randint(2, 9, [a1])
-      c = randint(2, 9, [a0])
-      this.autoCorrection[2 * i] = {}
-      this.autoCorrection[2 * i + 1] = {}
+      b =
+        listeChoixb[i] === 2
+          ? 1
+          : listeChoixb[i] === 3
+            ? -randint(1, 9, [a1, a0])
+            : randint(2, 9, [a1, a0])
+      c = randint(1, 9, [a0, a1])
+      if (listeChoixc[i] === 2) {
+        c = -c
+      }
       switch (typesDeQuestions) {
-
-        case 2: // a(bx-c)\n
-          a1=0
-          c = -c
-          break
-        case 3: // ax(bx+c)
-          a0=0
-          break
-        case 4:  // ax(bx-c)
-          a0=0
-          c = -c
+        case 2: // ax(bx+c)
+          a0 = 0
+          a1 = listeChoixa[i] === 3 ? -a1 : a1
           break
         case 1: // a(bx+c)
         default:
-          a1=0
+          a1 = 0
+          a0 = listeChoixa[i] === 3 ? -a0 : a0
           break
       }
+      this.autoCorrection[2 * i] = {}
+      this.autoCorrection[2 * i + 1] = {}
 
       texte = `$${lettreDepuisChiffre(i + 1)} = ${reduireAxPlusB(a1, a0)}(${reduireAxPlusB(b, c)})$`
-          texteCorr = texte
+      texte += '<br><br>'
+      texteCorr = texte
       // developpements = faitDeveloppement(a1, a0, b, c)
-      const dvpt=reduirePolynomeDegre3(0, a1*b, a1*c + a0*b, a0*c)
+      const dvpt = reduirePolynomeDegre3(0, a1 * b, a1 * c + a0 * b, a0 * c)
       tableau = faitTableau(a1, a0, b, c)
-      texte += context.isHtml ? '<br>' : '\\par\\medskip'
+      // texte += context.isHtml ? '<br>' : '\\par\\medskip'
 
       if (this.interactif) {
         const tableauVide = AddTabDbleEntryMathlive.convertTclToTableauMathlive(
           tableau.entetesCol,
           tableau.entetesLgn,
-          ['', '',],
+          ['', ''],
         )
         // const NumEx = this.numeroExercice ? this.numeroExercice : 1
         // OM:  fonctionne pas
@@ -136,7 +178,7 @@ this.besoinFormulaire2CaseACocher = ['a peut-être négatif']
           this.numeroExercice ?? 0,
           2 * i,
           tableauVide,
-          `tableauMathlive`,// ` ${KeyboardType.clavierDeBaseAvecVariable}`,
+          `tableauMathlive`, // ` ${KeyboardType.clavierDeBaseAvecVariable}`,
           true,
           { L0C0: 'red' },
         )
@@ -154,18 +196,16 @@ this.besoinFormulaire2CaseACocher = ['a peut-être négatif']
           { L0C0: 'red' },
         )
       }
-      texte += context.isHtml
-        ? '<br> Développement réduit : '
-        : '\\par\\medskip Développement réduit: '
+
+      texte += '<br><br> Développement réduit : '
       texte += ajouteChampTexteMathLive(this, 2 * i + 1, '')
-      texteCorr += context.isHtml ? '<br>' : '\\par\\medskip'
+      texte += `<br><br>`
       texteCorr += tableauColonneLigne(
         tableau.entetesCol,
         tableau.entetesLgn,
         [
-          tableau.L1C1txt,
-          tableau.L1C2txt,
-
+          reduirePolynomeDegre3(0, a1 * b, a0 * b, 0),
+          reduireAxPlusB(a1 * c, a0 * c),
         ],
         1,
         true,
@@ -175,17 +215,41 @@ this.besoinFormulaire2CaseACocher = ['a peut-être négatif']
         { L0C0: 'red' },
       )
 
-      texteCorr += `<br>Développement réduit : $${lettreDepuisChiffre(i + 1)} = ${dvpt}$`
+      texteCorr += `<br><br>Développement réduit : $${lettreDepuisChiffre(i + 1)} = ${dvpt}$`
 
       handleAnswers(this, 2 * i, {
         bareme: toutPourUnPoint,
-        L1C1: { value: tableau.L1C1txt },
-        L1C2: { value: tableau.L1C2txt },
-
+        L1C1: {
+          value: tableau.L1C1txt,
+          options: {
+            calculFormel: true,
+          },
+        },
+        L1C2: {
+          value: tableau.L1C2txt,
+          options: {
+            calculFormel: true,
+          },
+        },
       })
-      handleAnswers(this, 2 * i + 1, { reponse: { value: dvpt } })
+      /*  handleAnswers(this, i, {
+              reponse: {
+                value: reponse,
+                options: {
+                  expressionNumerique: !this.litteral,
+                },
+              },
+            }) */
+      handleAnswers(this, 2 * i + 1, {
+        reponse: {
+          value: dvpt,
+          options: {
+            avecSigneMultiplier: false,
+          },
+        },
+      })
 
-      if (this.questionJamaisPosee(i, a1,a0, b, c,  typesDeQuestions)) {
+      if (this.questionJamaisPosee(i, a1, a0, b, c, typesDeQuestions)) {
         // Si la question n'a jamais été posée, on en créé une autre
         this.listeQuestions[i] = texte
         this.listeCorrections[i] = texteCorr
@@ -197,28 +261,49 @@ this.besoinFormulaire2CaseACocher = ['a peut-être négatif']
   }
 }
 
-
-
 interface TableauData {
   entetesCol: string[]
   entetesLgn: string[]
   contenu: string[]
   L1C1txt: string
-  L1C2txt: string
+  L1C2txt: string[]
 }
-
 
 function faitTableau(a: number, b: number, c: number, d: number): TableauData {
   // ici (ax+b)(cx+d) avec soit a=0 soit b=0
   const tableau: TableauData = {
-    entetesCol: ['\\phantom{A.}\\times\\phantom{E.}', reduireAxPlusB(c, 0), `${d}`],
-    entetesLgn: [reduireAxPlusB(a, b)],
-    contenu: [
-      `\\phantom{A.xE.}`,
-      `\\phantom{A.xE.}`,
+    entetesCol: [
+      '\\phantom{A.}\\times\\phantom{E.}',
+      reduireAxPlusB(c, 0),
+      `${d}`,
     ],
-    L1C1txt: reduirePolynomeDegre3(0, a * c, b*c, 0), 
-    L1C2txt: reduireAxPlusB(a * d, b*d), 
+    entetesLgn: [reduireAxPlusB(a, b)],
+    contenu: [`\\phantom{A.xE.}`, `\\phantom{A.xE.}`],
+    L1C1txt: reduirePolynomeDegre3(0, a * c, b * c, 0), // `${a}x\\times${c}x+${b}\\times${c}x`, // reduirePolynomeDegre3(0, a * c, b * c, 0),
+    L1C2txt: [reduireAxPlusB(a * d, b * d), `${a}x\\times${d}+${b}\\times${d}`], // `${a}x\\times${d}+${b}\\times${d}`, // reduireAxPlusB(a * d, b * d),
   }
   return tableau
+}
+
+function gereFormulairesTextes(
+  gSaisie: string,
+  gMin: number,
+  gMx: number,
+  gMelange: number,
+  gDefaut: number,
+  gNbQuestions: number,
+): (string | number)[] {
+  const typesDidactiquesDisponibles = gestionnaireFormulaireTexte({
+    saisie: gSaisie,
+    min: gMin,
+    max: gMx,
+    melange: gMelange,
+    defaut: gDefaut,
+    nbQuestions: gNbQuestions,
+  })
+  const listeTypeDidactiques = combinaisonListes(
+    typesDidactiquesDisponibles,
+    gNbQuestions,
+  )
+  return listeTypeDidactiques
 }
