1de5a4780e5b0ea2e366b901a4a8d2e9f8cdf791
Guillaume Valmont <guillaume.valmont@ac-reunion.fr>
2025-12-31 14:39:01 +0400

    coquille: restore debug code

--- tests/e2e/tests/console_errors/console_errors.test.ts
+++ tests/e2e/tests/console_errors/console_errors.test.ts
@@ -294,6 +294,7 @@ if (process.env.NIV !== null && process.env.NIV !== undefined) {
   log(filter)
   testRunAllLots(filter)
 } else if (
+  process.env.CI &&
   process.env.CHANGED_FILES !== null &&
   process.env.CHANGED_FILES !== undefined
 ) {
