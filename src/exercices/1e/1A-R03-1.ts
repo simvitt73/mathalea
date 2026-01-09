d35e0d42538b356cb5b7b09511b7caaa59518979
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2026-01-09 10:51:37 +0000

    Jc fixe1 a r03 1 et le refactorise

--- src/exercices/1e/1A-R03-1.ts
+++ src/exercices/1e/1A-R03-1.ts
@@ -27,7 +27,41 @@ export const dateDePublication = '16/07/2025'
 export default class ProportionDeProportion extends ExerciceQcmA {
   // S'occupe de passser les données originales à la fonction appliquerLesValeurs
 
+  appliquerLesValeurs: (
+    jour: string,
+    prop1: number,
+    prop2: number,
+    prenom: string,
+    reponses: string[],
+  ) => void = (
+    jour: string,
+    prop1: number,
+    prop2: number,
+    prenom: string,
+    reponses: string[],
+  ) => {
+    this.enonce = `${prenom} consacre $${prop1}\\,\\%$ de sa journée de ${jour} à faire ses devoirs. <br>
+     $${prop2}\\,\\%$ du temps consacré aux devoirs est consacré à faire un exposé.  <br>
+    Le pourcentage du temps consacré à l’exposé par rapport à la journée de ${jour} est égal à :`
+    this.correction = `Le pourcentage du temps consacré à l’exposé par rapport à la journée de ${jour} est égal à $${prop1}\\,\\%$ de $${prop2}\\,\\%$, soit $${miseEnEvidence(reponses[0].slice(1, -1))}$.`
+    this.reponses = reponses
+  }
+
   versionOriginale: () => void = () => {
+    const prenom = 'Jean'
+    const jour = 'dimanche'
+    const prop1 = 25
+    const prop2 = 80
+    const reponses = [
+      '$\\dfrac{1}{4}\\times 80\\,\\%$',
+      '$80\\,\\%-25\\,\\%$',
+      '$0,08\\times 25\\,\\%$',
+      `Cela dépend de la durée de la journée de ${jour}. `,
+    ]
+    this.appliquerLesValeurs(jour, prop1, prop2, prenom, reponses)
+  }
+
+  versionAleatoire: () => void = () => {
     const table = [
       'lundi',
       'mardi',
@@ -38,107 +72,59 @@ export default class ProportionDeProportion extends ExerciceQcmA {
       'dimanche',
     ]
     const jour = randint(0, 6)
-    this.enonce = `Jean consacre $25\\,\\%$ de sa journée de ${table[jour]} à faire ses devoirs. <br>
-     $80\\,\\%$ du temps consacré aux devoirs est consacré à faire un exposé.  <br>
-    Le pourcentage du temps consacré à l’exposé par rapport à la journée de ${table[jour]} est égal à :`
-    this.correction = `Le pourcentage du temps consacré à l’exposé par rapport à la journée de ${table[jour]} est égal à $25\\,\\%$ de $80\\,\\%$, soit $${miseEnEvidence('\\dfrac{1}{4}\\times 80\\,\\%')}$.`
-    this.reponses = [
-      '$\\dfrac{1}{4}\\times 80\\,\\%$',
-      '$80\\,\\%-25\\,\\%$',
-      '$0,08\\times 25\\,\\%$',
-      `Cela dépend de la durée de la journée de ${table[jour]}. `,
-    ]
-  }
-
-  versionAleatoire: () => void = () => {
+    let proportions: [number, number] = [0, 0]
+    let bonnesReponses: string[] = []
+    let prop1: number
+    let prop2: number
+    let reponses: string[]
     switch (choice([1, 2])) {
       case 1:
-        {
-          const table = [
-            'lundi',
-            'mardi',
-            'mercredi',
-            'jeudi',
-            'vendredi',
-            'samedi',
-            'dimanche',
-          ]
-          const jour = randint(0, 6)
-          const proportions = choice([
-            [25, 4 * randint(7, 15)],
-            [20, 5 * randint(5, 15)],
-            [50, 2 * randint(26, 40)],
-          ])
-          const prop1 = proportions[0]
-          const prop2 = proportions[1]
-          const P = prenomM()
-          const bonneReponse1 = `${texNombre(prop1 / 100, 2)}\\times ${texNombre(prop2, 2)}\\,\\%`
-          const bonneReponse2 = `${texNombre(prop2 / 100, 2)}\\times ${texNombre(prop1, 2)}\\,\\%`
-          const bonneReponse3 = `${texNombre((prop2 * prop1) / 100, 2)}\\,\\%`
-          const bonneReponseRetenue = choice([
-            bonneReponse1,
-            bonneReponse2,
-            bonneReponse3,
-          ])
-          this.enonce = `${P} consacre $${prop1}\\,\\%$ de sa journée de ${table[jour]} à faire ses devoirs. <br>
-     $${prop2}\\,\\%$ du temps consacré aux devoirs est consacré à faire un exposé.  <br>
-    Le pourcentage du temps consacré à l’exposé par rapport à la journée de ${table[jour]} est égal à :`
-          this.correction = `Le temps consacré à l’exposé par rapport à la journée de ${table[jour]} est égal à $${prop1}\\,\\%$ de $${prop2}\\,\\%$,  soit $${prop1}\\,\\%\\times ${prop2}\\,\\%$, ou encore $${miseEnEvidence(`${bonneReponseRetenue}`)}$.`
-          this.reponses = [
-            `$${bonneReponseRetenue}$`,
-            `$${prop2}\\,\\%-${prop1}\\,\\%$`,
-            `$${prop2}\\,\\%\\times${texNombre(prop1 / 100, 2)}\\,\\%$`,
-            `$${texNombre(prop1 / 100, 2)}\\times${texNombre(prop2 / 100, 2)}\\,\\%$`,
-          ]
-        }
+        proportions = choice([
+          [25, 4 * randint(7, 15)],
+          [20, 5 * randint(5, 15)],
+          [50, 2 * randint(26, 40)],
+        ])
+        prop1 = proportions[0]
+        prop2 = proportions[1]
+        bonnesReponses = [
+          `$${texNombre(prop1 / 100, 2)}\\times ${texNombre(prop2, 2)}\\,\\%$`,
+          `$${texNombre(prop2 / 100, 2)}\\times ${texNombre(prop1, 2)}\\,\\%$`,
+          `$${texNombre((prop2 * prop1) / 100, 2)}\\,\\%$`,
+        ]
+        reponses = [
+          choice(bonnesReponses),
+          `$${prop2}\\,\\%-${prop1}\\,\\%$`,
+          `$${prop2}\\,\\%\\times${texNombre(prop1 / 100, 2)}\\,\\%$`,
+          `$${texNombre(prop1 / 100, 2)}\\times${texNombre(prop2 / 100, 2)}\\,\\%$`,
+        ]
         break
 
       case 2:
       default:
-        {
-          const table = [
-            'lundi',
-            'mardi',
-            'mercredi',
-            'jeudi',
-            'vendredi',
-            'samedi',
-            'dimanche',
-          ]
-          const jour = randint(0, 6)
-          const proportions = choice([
-            [25, 4 * randint(5, 15)],
-            [20, 5 * randint(5, 15)],
-            [50, 2 * randint(28, 40)],
-          ])
-          const prop1 = proportions[0]
-          const prop2 = proportions[1]
-          const P = prenomM()
-          const bonneReponse1 = `${texNombre(prop1 / 100, 2)}\\times ${texNombre(prop2, 2)}\\,\\%`
-          const bonneReponse2 = `${texNombre(prop1 / 100, 2)}\\times ${texNombre(prop2 / 100, 2)}`
-          const bonneReponse3 = `${texNombre(prop1 / 100, 2)}\\times ${new FractionEtendue(prop2, 100).texFractionSimplifiee}`
-          const bonneReponse4 = `${new FractionEtendue(prop1, 100).texFractionSimplifiee}\\times ${texNombre(prop2 / 100, 2)}`
+        proportions = choice([
+          [25, 4 * randint(5, 15)],
+          [20, 5 * randint(5, 15)],
+          [50, 2 * randint(28, 40)],
+        ])
+        prop1 = proportions[0]
+        prop2 = proportions[1]
+        bonnesReponses = [
+          `$${texNombre(prop1 / 100, 2)}\\times ${texNombre(prop2, 2)}\\,\\%$`,
+          `$${texNombre(prop1 / 100, 2)}\\times ${texNombre(prop2 / 100, 2)}$`,
+          `$${texNombre(prop1 / 100, 2)}\\times ${new FractionEtendue(prop2, 100).texFractionSimplifiee}$`,
+          `$${new FractionEtendue(prop1, 100).texFractionSimplifiee}\\times ${texNombre(prop2 / 100, 2)}$`,
+        ]
 
-          const bonneReponseRetenue = choice([
-            bonneReponse1,
-            bonneReponse2,
-            bonneReponse3,
-            bonneReponse4,
-          ])
-
-          this.enonce = `${P} consacre $${prop1}\\,\\%$ de sa journée de ${table[jour]} à faire ses devoirs. <br>
-     $${prop2}\\,\\%$ du temps consacré aux devoirs est consacré à faire un exposé.  <br>
-    La proportion du temps consacré à l’exposé par rapport à la journée de ${table[jour]} est égale à :`
-          this.correction = `Le temps consacré à l’exposé par rapport à la journée de ${table[jour]} est égal à $${prop1}\\,\\%$ de $${prop2}\\,\\%$,  soit $${prop1}\\,\\%\\times ${prop2}\\,\\%$, ou encore $${miseEnEvidence(`${bonneReponseRetenue}`)}$.`
-          this.reponses = [
-            `$${bonneReponseRetenue}$`,
-            `$${prop2}\\,\\%-${prop1}\\,\\%$`,
-            `$${new FractionEtendue(prop1, 100).texFractionSimplifiee}\\times ${texNombre(prop2, 2)}$`,
-            `$${prop1}\\times ${texNombre(prop2 / 100, 2)}$`,
-          ]
-        }
+        reponses = [
+          choice(bonnesReponses),
+          `$${prop2}\\,\\%-${prop1}\\,\\%$`,
+          `$${new FractionEtendue(prop1, 100).texFractionSimplifiee}\\times ${texNombre(prop2, 2)}$`,
+          `$${prop1}\\times ${texNombre(prop2 / 100, 2)}$`,
+        ]
         break
     }
+    const P = prenomM()
+    this.appliquerLesValeurs(table[jour], prop1, prop2, String(P), reponses)
   }
 
   // Ici il n'y a rien à faire, on appelle juste la version aleatoire (pour un qcm aleatoirisé, c'est le fonctionnement par défaut)
