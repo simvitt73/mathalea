366867237e4c678b7ee8ef4268bc3b23645b5a10
Rémi Angot <remi.angot@gmail.com>
2026-01-10 16:23:30 +0100

    fix: clavier pour can2G13 avec racine

--- src/exercices/can/2e/can2G13.ts
+++ src/exercices/can/2e/can2G13.ts
@@ -32,6 +32,7 @@ export default class NormeVecteur extends ExerciceSimple {
     this.formatChampTexte = KeyboardType.clavierFullOperations
     this.typeExercice = 'simple'
     this.nbQuestions = 1
+    this.formatChampTexte = KeyboardType.clavierFullOperations
   }
 
   nouvelleVersion() {
