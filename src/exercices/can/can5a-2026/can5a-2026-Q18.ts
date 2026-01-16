a169c33f22a5400a6f0aae2bd7bbf117c8c1ed55
gillesMora <gilles.mora@ac-normandie.fr>
2026-01-16 17:23:48 +0100

    Q19

--- src/exercices/can/can5a-2026/can5a-2026-Q18.ts
+++ src/exercices/can/can5a-2026/can5a-2026-Q18.ts
@@ -1,10 +1,11 @@
 import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
+import { choice } from '../../../lib/outils/arrayOutils'
 import { miseEnEvidence } from '../../../lib/outils/embellissements'
+import { sp } from '../../../lib/outils/outilString'
 import { texNombre } from '../../../lib/outils/texNombre'
-import { randint } from '../../../modules/outils'
 import ExerciceCan from '../../ExerciceCan'
 
-export const titre = 'Calculer le périmètre d\'un rectangle'
+export const titre = 'Calculer une moyenne'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 export const uuid = 'kv0x8'
@@ -18,32 +19,48 @@ export const refs = {
 
 */
 export default class Can52026Q18 extends ExerciceCan {
-  enonce(longueur?: number, largeur?: number) {
-    if (longueur == null || largeur == null) {
-      longueur = randint(5, 9)
-      largeur = randint(3, longueur - 1)
+ enonce(a?: number, b?: number, c?: number, d?: number) {
+    if (a == null || b == null || c == null || d == null) {
+      // Configurations avec moyenne entière
+      // [a, b, c, d] où la somme est divisible par 4
+      const listeCas = [
+        [4, 9, 11, 12],   // Somme = 36, Moyenne = 9
+        [3, 7, 10, 12],   // Somme = 32, Moyenne = 8
+        [5, 8, 9, 14],    // Somme = 36, Moyenne = 9
+        [2, 6, 11, 13],   // Somme = 32, Moyenne = 8
+        [4, 7, 12, 13],   // Somme = 36, Moyenne = 9
+        [3, 9, 10, 14],   // Somme = 36, Moyenne = 9
+        [5, 7, 8, 12],    // Somme = 32, Moyenne = 8
+        [2, 8, 11, 15],   // Somme = 36, Moyenne = 9
+        [6, 7, 9, 14],    // Somme = 36, Moyenne = 9
+        [4, 6, 10, 12],   // Somme = 32, Moyenne = 8
+      ]
+      const cas = choice(listeCas)
+      a = cas[0]
+      b = cas[1]
+      c = cas[2]
+      d = cas[3]
     }
 
-    this.reponse = (longueur + largeur) * 2
-     this.formatChampTexte = KeyboardType.clavierDeBase
-    this.question = `Un rectangle a une longueur de $${longueur}\\text{ cm}$ et une largeur de $${largeur}\\text{ cm}$.<br>
-Son périmètre est égal à :`
-    
-    this.correction = `Le périmètre d'un rectangle de longueur $${longueur}\\text{ cm}$ et de largeur $${largeur}\\text{ cm}$ est :<br>
-$2\\times (${longueur}+${largeur})=2\\times ${longueur + largeur}=${miseEnEvidence(texNombre((longueur + largeur) * 2, 0))}\\text{ cm}$.`
-    
+    const somme = a + b + c + d
+    const moyenne = somme / 4
+this.formatChampTexte = KeyboardType.clavierDeBase
+    this.question = `$${a}$ ${sp(2)} ; ${sp(2)} $${b}$ ${sp(2)} ; ${sp(2)} $${c}$ ${sp(2)} ; ${sp(2)} $${d}$<br>
+La moyenne de ces nombres est :`
+
+    this.correction = `La somme des $4$ nombres est : $${a}+${b}+${c}+${d}=${somme}$.<br>
+La moyenne est donc : $\\dfrac{${somme}}{4}=${miseEnEvidence(texNombre(moyenne, 0))}$.`
+
+    this.reponse = moyenne
     this.canEnonce = this.question
-    this.canReponseACompleter = '$\\ldots\\text{ cm}$'
-    this.optionsChampTexte = { texteApres: ' $\\text{cm}$' }
+    this.canReponseACompleter = '$\\ldots$'
     
     if (this.interactif) {
-      this.question += '<br>'
-    } else {
-      this.question += '<br>$\\ldots$ cm'
+      this.optionsChampTexte = { texteApres: '' }
     }
   }
 
   nouvelleVersion() {
-    this.canOfficielle ? this.enonce(7, 5) : this.enonce()
+    this.canOfficielle ? this.enonce(4, 9, 11, 12) : this.enonce()
   }
 }
--- src/exercices/can/can5a-2026/can5a-2026-Q19.ts
+++ src/exercices/can/can5a-2026/can5a-2026-Q19.ts
@@ -1,10 +1,17 @@
+import { afficheMesureAngle } from '../../../lib/2d/AfficheMesureAngle'
+import { pointAbstrait } from '../../../lib/2d/PointAbstrait'
+import { polygoneAvecNom } from '../../../lib/2d/polygones'
 import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
+import { degTan } from '../../../lib/mathFonctions/radToDeg'
+import { choice } from '../../../lib/outils/arrayOutils'
 import { miseEnEvidence } from '../../../lib/outils/embellissements'
 import { texNombre } from '../../../lib/outils/texNombre'
+import { context } from '../../../modules/context'
+import { mathalea2d } from '../../../modules/mathalea2d'
 import { randint } from '../../../modules/outils'
 import ExerciceCan from '../../ExerciceCan'
 
-export const titre = 'Calculer le périmètre d\'un rectangle'
+export const titre = 'Calculer un angle dans un triangle connaissant les deux autres'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 export const uuid = 'e9ly4'
@@ -18,32 +25,91 @@ export const refs = {
 
 */
 export default class Can52026Q19 extends ExerciceCan {
-  enonce(longueur?: number, largeur?: number) {
-    if (longueur == null || largeur == null) {
-      longueur = randint(5, 9)
-      largeur = randint(3, longueur - 1)
+  enonce(angleD?: number, angleF?: number) {
+    if (angleD == null || angleF == null) {
+      // Configurations [angleD, angleF] avec somme < 180°
+      const listeCas = [
+        [75, 50],   // angleE = 55°
+        [65, 45],   // angleE = 70°
+        [70, 60],   // angleE = 50°
+       
+        [60, 50],   // angleE = 70°
+        [75, 45],   // angleE = 60°
+       
+        [70, 55],   // angleE = 55°
+        [65, 60],   // angleE = 55°
+       
+      ]
+      const cas = choice(listeCas)
+      angleD = cas[0]
+      angleF = cas[1]
     }
 
-    this.reponse = (longueur + largeur) * 2
-     this.formatChampTexte = KeyboardType.clavierDeBase
-    this.question = `Un rectangle a une longueur de $${longueur}\\text{ cm}$ et une largeur de $${largeur}\\text{ cm}$.<br>
-Son périmètre est égal à :`
+    const angleE = 180 - angleD - angleF
     
-    this.correction = `Le périmètre d'un rectangle de longueur $${longueur}\\text{ cm}$ et de largeur $${largeur}\\text{ cm}$ est :<br>
-$2\\times (${longueur}+${largeur})=2\\times ${longueur + largeur}=${miseEnEvidence(texNombre((longueur + largeur) * 2, 0))}\\text{ cm}$.`
+    // Construction du triangle
+    // D en bas à gauche, F en bas à droite, E en haut
+    const D = pointAbstrait(0, 0, 'D')
+    const F = pointAbstrait(5, 0, 'F')
+    // Position de E basée sur l'angle en D
+    const E = pointAbstrait(2.5, 2.5 * degTan(angleD), 'E')
     
-    this.canEnonce = this.question
-    this.canReponseACompleter = '$\\ldots\\text{ cm}$'
-    this.optionsChampTexte = { texteApres: ' $\\text{cm}$' }
+    const pol = polygoneAvecNom(D, F, E)
+    const triangle = pol[0] // Le tracé du triangle
+    const labels = pol[1]   // Les labels des points
     
+    // Affichage de l'angle en D (connu)
+    const affAngleD = afficheMesureAngle(F, D, E, 'black', 0.8, `${angleD}°`)
+    
+    // Affichage de l'angle en F (connu)
+    const affAngleF = afficheMesureAngle(E, F, D, 'black', 0.8, `${angleF}°`)
+    
+    // Affichage de l'angle cherché en E
+    const affAngleE = afficheMesureAngle(D, E, F, 'black', 0.5, '?')
+
+    this.formatChampTexte = KeyboardType.clavierDeBase
+    this.reponse = angleE
+    
+    const xmin = Math.min(D.x, F.x, E.x) - 1
+    const ymin = Math.min(D.y, F.y, E.y) - 1
+    const xmax = Math.max(D.x, F.x, E.x) + 1
+    const ymax = Math.max(D.y, F.y, E.y) + 1.5
+    
+    this.canEnonce = mathalea2d(
+      {
+        xmin,
+        ymin,
+        xmax,
+        ymax,
+        pixelsParCm: 20,
+        mainlevee: false,
+        amplitude: 0.3,
+        scale: 0.7,
+        style: 'margin: auto',
+      },
+      [triangle, labels, affAngleD, affAngleF, affAngleE]
+    )
+    
+    this.question = this.canEnonce
+    
+    this.correction = `La somme des angles d'un triangle est égale à $180\\circ$.<br>
+Dans le triangle $DEF$, on a :<br>
+$\\widehat{EDF}+\\widehat{DFE}+\\widehat{FED}=180\\circ$<br>
+$${angleD}\\circ+${angleF}\\circ+\\widehat{FED}=180\\circ$<br>
+$${angleD + angleF}\\circ+\\widehat{FED}=180\\circ$<br>
+$\\widehat{FED}=180\\circ-${angleD + angleF}\\circ=${miseEnEvidence(angleE + '$\\circ$')}$`
+    
+    this.canReponseACompleter = '$?=\\ldots\\circ$'
+    this.optionsChampTexte = { texteApres: '$\\circ$' }
+    this.formatChampTexte = KeyboardType.clavierDeBase
     if (this.interactif) {
-      this.question += '<br>'
-    } else {
-      this.question += '<br>$\\ldots$ cm'
+      this.question += '<br>$?=$'
+    } else if (context.isHtml) {
+      this.question += '<br>$?=\\ldots\\circ$'
     }
   }
 
   nouvelleVersion() {
-    this.canOfficielle ? this.enonce(7, 5) : this.enonce()
+    this.canOfficielle ? this.enonce(75, 50) : this.enonce()
   }
 }
