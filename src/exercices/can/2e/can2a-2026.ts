efe7c23ae5170479b1450c4a5f71f3632ef63186
Eric Elter <eric.elter@ac-normandie.fr>
2026-01-20 21:50:38 +0100

    Nx fichiers

--- src/exercices/can/2e/can2a-2026.ts
+++ src/exercices/can/2e/can2a-2026.ts
@@ -2,37 +2,37 @@
  * ⚠️ Cet exercice est utilisé dans le test : tests/e2e/tests/view/view.eleve.can.2025.2e.test.ts ⚠️
  */
 
-import Question1 from '../can2a-2026/can2a-2026-Q1'
-import Question2 from '../can2a-2025/can2a-2025-Q2'
-import Question3 from '../can2a-2025/can2a-2025-Q3'
-import Question4 from '../can2a-2025/can2a-2025-Q4'
-import Question5 from '../can2a-2025/can2a-2025-Q5'
-import Question6 from '../can2a-2025/can2a-2025-Q6'
-import Question7 from '../can2a-2025/can2a-2025-Q7'
-import Question8 from '../can2a-2025/can2a-2025-Q8'
-import Question9 from '../can2a-2025/can2a-2025-Q9'
-import Question10 from '../can2a-2025/can2a-2025-Q10'
-import Question11 from '../can2a-2025/can2a-2025-Q11'
-import Question12 from '../can2a-2025/can2a-2025-Q12'
-import Question13 from '../can2a-2025/can2a-2025-Q13'
-import Question14 from '../can2a-2025/can2a-2025-Q14'
-import Question15 from '../can2a-2025/can2a-2025-Q15'
-import Question16 from '../can2a-2025/can2a-2025-Q16'
-import Question17 from '../can2a-2025/can2a-2025-Q17'
-import Question18 from '../can2a-2025/can2a-2025-Q18'
-import Question19 from '../can2a-2025/can2a-2025-Q19'
-import Question20 from '../can2a-2025/can2a-2025-Q20'
-import Question21 from '../can2a-2025/can2a-2025-Q21'
-import Question22 from '../can2a-2025/can2a-2025-Q22'
-import Question23 from '../can2a-2025/can2a-2025-Q23'
-import Question24 from '../can2a-2025/can2a-2025-Q24'
-import Question25 from '../can2a-2025/can2a-2025-Q25'
-import Question26 from '../can2a-2025/can2a-2025-Q26'
-import Question27 from '../can2a-2025/can2a-2025-Q27'
-import Question28 from '../can2a-2025/can2a-2025-Q28'
-import Question29 from '../can2a-2025/can2a-2025-Q29'
-import Question30 from '../can2a-2025/can2a-2025-Q30'
 import MetaExercice from '../../MetaExerciceCan'
+import Question1 from '../can2a-2026/can2a-2026-Q1'
+import Question10 from '../can2a-2026/can2a-2026-Q10'
+import Question11 from '../can2a-2026/can2a-2026-Q11'
+import Question12 from '../can2a-2026/can2a-2026-Q12'
+import Question13 from '../can2a-2026/can2a-2026-Q13'
+import Question14 from '../can2a-2026/can2a-2026-Q14'
+import Question15 from '../can2a-2026/can2a-2026-Q15'
+import Question16 from '../can2a-2026/can2a-2026-Q16'
+import Question17 from '../can2a-2026/can2a-2026-Q17'
+import Question18 from '../can2a-2026/can2a-2026-Q18'
+import Question19 from '../can2a-2026/can2a-2026-Q19'
+import Question2 from '../can2a-2026/can2a-2026-Q2'
+import Question20 from '../can2a-2026/can2a-2026-Q20'
+import Question21 from '../can2a-2026/can2a-2026-Q21'
+import Question22 from '../can2a-2026/can2a-2026-Q22'
+import Question23 from '../can2a-2026/can2a-2026-Q23'
+import Question24 from '../can2a-2026/can2a-2026-Q24'
+import Question25 from '../can2a-2026/can2a-2026-Q25'
+import Question26 from '../can2a-2026/can2a-2026-Q26'
+import Question27 from '../can2a-2026/can2a-2026-Q27'
+import Question28 from '../can2a-2026/can2a-2026-Q28'
+import Question29 from '../can2a-2026/can2a-2026-Q29'
+import Question3 from '../can2a-2026/can2a-2026-Q3'
+import Question30 from '../can2a-2026/can2a-2026-Q30'
+import Question4 from '../can2a-2026/can2a-2026-Q4'
+import Question5 from '../can2a-2026/can2a-2026-Q5'
+import Question6 from '../can2a-2026/can2a-2026-Q6'
+import Question7 from '../can2a-2026/can2a-2026-Q7'
+import Question8 from '../can2a-2026/can2a-2026-Q8'
+import Question9 from '../can2a-2026/can2a-2026-Q9'
 
 export const titre = 'CAN Seconde sujet 2026'
 export const interactifReady = true
@@ -82,7 +82,7 @@ const questions = [
   Question30,
 ]
 
-export default class Can2a2025 extends MetaExercice {
+export default class Can2a2026 extends MetaExercice {
   constructor() {
     super(questions)
   }
---
+++ src/exercices/can/can2a-2026/can2a-2026-Q14.ts
@@ -0,0 +1,41 @@
+import Decimal from 'decimal.js'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
+import { miseEnEvidence } from '../../../lib/outils/embellissements'
+import { texNombre } from '../../../lib/outils/texNombre'
+import { randint } from '../../../modules/outils'
+import ExerciceCan from '../../ExerciceCan'
+export const titre = 'Multiplier un entier avec un décimal'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const uuid = 'zoga9'
+export const refs = {
+  'fr-fr': [],
+  'fr-ch': ['NR'],
+}
+/**
+ * Modèle d'exercice très simple pour la course aux nombres
+ * @author Gilles Mora
+
+*/
+export default class Can2a2026Q14 extends ExerciceCan {
+  enonce(a?: Decimal, b?: number) {
+    if (a == null || b == null) {
+      a = new Decimal(randint(2, 9)).div(10)
+      b = randint(5, 9)
+    }
+
+    this.formatChampTexte = KeyboardType.clavierDeBase
+    this.reponse = texNombre(a.mul(b), 1)
+    this.question = `$${texNombre(a, 1)} \\times ${b}$ `
+    this.correction = `$${texNombre(a, 1)} \\times ${b}=${miseEnEvidence(this.reponse)}$`
+    this.canEnonce = this.question
+    this.canReponseACompleter = ''
+    if (this.interactif) {
+      this.question += '$=$'
+    }
+  }
+
+  nouvelleVersion() {
+    this.canOfficielle ? this.enonce(new Decimal(0.6), 9) : this.enonce()
+  }
+}
