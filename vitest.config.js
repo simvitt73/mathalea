40833ba125afd95cdcd3bce695a43be526b95c00
svitt <97439749+simvitt73@users.noreply.github.com>
2026-02-01 13:25:52 +0100

    Update vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 
 
 export default defineConfig({
+  base : 'alea',
   plugins: [svelte({ hot: !process.env.VITEST })],
   resolve: {
     conditions: ['browser'],
